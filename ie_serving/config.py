8c75db3bd0546622b42874bb96d81be5f8d8bc20
Dariusz Trawinski <dariusz.trawinski@intel.com>
2018-07-10 18:28:39 +0200

    fix path to cpu ext lib

--- ie_serving/config.py
+++ ie_serving/config.py
@@ -1,6 +1,6 @@
 import os
 
 DEVICE = os.environ.get('DEVICE', "CPU")
-CPU_EXTENSION = os.environ.get('CPU_EXTENSION', "/opt/intel/deeplearning_deploymenttoolkit/deployment_tools/"
+CPU_EXTENSION = os.environ.get('CPU_EXTENSION', "/opt/intel/computer_vision_sdk/deployment_tools/"
                                                 "inference_engine/lib/ubuntu_16.04/intel64/libcpu_extension_avx2.so")
 PLUGIN_DIR = os.environ.get('PLUGIN_DIR', None)
