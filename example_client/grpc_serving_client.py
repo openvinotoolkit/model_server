8e8a540d9887442d70b2105759f48a3ad78dd3c3
mareklew-Intel <marek.lewandowski@intel.com>
2018-08-21 15:01:19 +0200

    Add license file and headers (#16)

--- example_client/grpc_serving_client.py
+++ example_client/grpc_serving_client.py
@@ -1,3 +1,19 @@
+#
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 from grpc.beta import implementations
 import numpy as np
 import tensorflow.contrib.util as tf_contrib_util
@@ -42,7 +58,7 @@ for x in range(100):
     nu = np.array(output)
     ma = np.argmax(nu)
     # for object classification models show imagenet class
-    print("Best classification", classes.imagenet_classess[ma])
+    print("Best classification", classes.imagenet_classes[ma])
 
 print('processing time for all iterations')
 for x in processing_times:
