93ccb252f05ebe6f1b4912457ec7b9f34995350b
Miłosz Żeglarski <milosz.zeglarski@intel.com>
2018-11-27 13:34:05 +0100

    after review v3

--- tests/unit/models/test_gs_model.py
+++ tests/unit/models/test_gs_model.py
@@ -13,46 +13,54 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import pytest
+
 from ie_serving.models.gs_model import GSModel
 
 
-def test_get_versions(mocker):
+@pytest.mark.parametrize("content_list, versions", [
+    (['model/3/', 'model/3.txt', 'model/sub/2/', 'model/one'],
+     ['gs://bucket/model/3/']),
+    (['model/3/model.xml', 'model/3.txt', 'model/sub/2/', 'model/one'],
+     ['gs://bucket/model/3/']),
+    (['model/3/something.xml', 'model/3/something.bin', 'model/2/4/3/file',
+      'model/1/'],
+     ['gs://bucket/model/3/', 'gs://bucket/model/2/', 'gs://bucket/model/1/']),
+    (['model/dir1/', 'model/3.txt', 'model/dir2/2/', 'model/one/file'], []),
+])
+def test_get_versions(mocker, content_list, versions):
     list_content_mocker = mocker.patch('ie_serving.models.gs_model.GSModel.'
                                        'gs_list_content')
-    list_content_mocker.return_value = ['model/3/',
-                                        'model/3.txt',
-                                        'model/sub/2/',
-                                        'model/one']
+    list_content_mocker.return_value = content_list
 
     output = GSModel.get_versions('gs://bucket/model')
-    assert 1 == len(output)
-    assert 'gs://bucket/model/3/' == output[0]
+    assert set(output) == set(versions)
 
 
-def test_not_get_versions_files(mocker):
+@pytest.mark.parametrize("content_list, version_files", [
+    (['model/3/', 'model/3.txt', 'model/3/2/'],
+     (None, None, None)),
+    (['model/3/model.xml', 'model/3/model.bin', 'model/3/sub/file'],
+     ('gs://bucket/model/3/model.xml', 'gs://bucket/model/3/model.bin', None)),
+    (['model/3/something.xml', 'model/3/something.bin',
+      'model/3/mapping_config.json'],
+     ('gs://bucket/model/3/something.xml',
+      'gs://bucket/model/3/something.bin',
+      'gs://bucket/model/3/mapping_config.json')),
+    (['model/3//file.bin', 'model/3//file.xml'],
+     (None, None, None)),
+    (['model/3/some file.bin', 'model/3/some file.xml'],
+     (None, None, None)),
+    (['model/3/somefile.bin', 'model/3/otherfile.xml'],
+     (None, None, None)),
+])
+def test_get_versions_files(mocker, content_list, version_files):
     list_content_mocker = mocker.patch('ie_serving.models.gs_model.GSModel.'
                                        'gs_list_content')
-    list_content_mocker.return_value = ['model/3/doc.doc',
-                                        'model/3/subdir',
-                                        'model/3/something.xml',
-                                        'model/3/model.bin']
+    list_content_mocker.return_value = content_list
 
     xml, bin, mapping = GSModel.get_version_files('gs://bucket/model/3/')
-    assert xml is None and bin is None and mapping is None
-
-
-def test_get_versions_files(mocker):
-    list_content_mocker = mocker.patch('ie_serving.models.gs_model.GSModel.'
-                                       'gs_list_content')
-    list_content_mocker.return_value = ['model/3/doc.doc',
-                                        'model/3/subdir',
-                                        'model/3/model.xml',
-                                        'model/3/model.bin']
-
-    xml, bin, mapping = GSModel.get_version_files('gs://bucket/model/3/')
-    assert xml == 'gs://bucket/model/3/model.xml' and \
-        bin == 'gs://bucket/model/3/model.bin' and \
-        mapping is None
+    assert (xml, bin, mapping) == version_files
 
 
 def test_get_mapping_config(mocker):
