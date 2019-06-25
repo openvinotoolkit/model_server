b69372d1b9610ed7766b532824b39c4ab2bef34b
Dariusz Trawinski <dariusz.trawinski@intel.com>
2019-06-25 11:37:34 +0200

    style fixes

--- example_client/rest_serving_client.py
+++ example_client/rest_serving_client.py
@@ -14,50 +14,51 @@
 # limitations under the License.
 #
 
-import grpc
+
 import numpy as np
-import tensorflow.contrib.util as tf_contrib_util
 import classes
 import datetime
 import argparse
 import json
 import requests
-from tensorflow_serving.apis import predict_pb2
-from tensorflow_serving.apis import prediction_service_pb2_grpc
+
 
 def create_request(img, request_format):
+    signature = "serving_default"
     if request_format == "row_name":
         instances = []
         instance = {}
-        for i in range(0,img.shape[0]-1,1):
+        for i in range(0, img.shape[0]-1, 1):
             instance[args['input_name']] = img[i].tolist()
             instances.append(instance)
-        data_obj = {"signature_name": "serving_default","instances": instances }
+        data_obj = {"signature_name": signature, "instances": instances}
     elif request_format == "row_noname":
-        data_obj = {"signature_name": "serving_default",'instances': img.tolist() }
+        data_obj = {"signature_name": signature, 'instances': img.tolist()}
     elif request_format == "column_name":
-        data_obj = {"signature_name": "serving_default",'inputs': {args['input_name']: img.tolist()}}
+        data_obj = {"signature_name": signature,
+                    'inputs': {args['input_name']: img.tolist()}}
     elif request_format == "column_noname":
-        data_obj = {"signature_name": "serving_default",'inputs':  img.tolist()}
+        data_obj = {"signature_name": signature, 'inputs':  img.tolist()}
     else:
         print("invalid request format defined")
         exit(1)
     data_json = json.dumps(data_obj)
     return data_json
 
+
 parser = argparse.ArgumentParser(description='Sends requests via TFS gRPC API using images in numpy format. '
                                              'It displays performance statistics and optionally the model accuracy')
 parser.add_argument('--images_numpy_path', required=True, help='numpy in shape [n,w,h,c] or [n,c,h,w]')
 parser.add_argument('--labels_numpy_path', required=False, help='numpy in shape [n,1] - can be used to check model accuracy')
-parser.add_argument('--rest_url',required=False, default='http://localhost',  help='Specify url to REST API service. default: http://localhost')
-parser.add_argument('--rest_port',required=False, default=5555, help='Specify port to grpc service. default: 5555')
-parser.add_argument('--input_name',required=False, default='input', help='Specify input tensor name. default: input')
-parser.add_argument('--output_name',required=False, default='resnet_v1_50/predictions/Reshape_1',
+parser.add_argument('--rest_url', required=False, default='http://localhost',  help='Specify url to REST API service. default: http://localhost')
+parser.add_argument('--rest_port', required=False, default=5555, help='Specify port to grpc service. default: 5555')
+parser.add_argument('--input_name', required=False, default='input', help='Specify input tensor name. default: input')
+parser.add_argument('--output_name', required=False, default='resnet_v1_50/predictions/Reshape_1',
                     help='Specify output name. default: resnet_v1_50/predictions/Reshape_1')
 parser.add_argument('--transpose_input', choices=["False", "True"], default="True",
                     help='Set to False to skip NHWC>NCHW or NCHW>NHWC input transposing. default: True',
                     dest="transpose_input")
-parser.add_argument('--transpose_method', choices=["nchw2nhwc","nhwc2nchw"], default="nhwc2nchw",
+parser.add_argument('--transpose_method', choices=["nchw2nhwc", "nhwc2nchw"], default="nhwc2nchw",
                     help="How the input transposition should be executed: nhwc2nchw or nhwc2nchw",
                     dest="transpose_method")
 parser.add_argument('--iterations', default=0,
@@ -71,18 +72,18 @@ parser.add_argument('--batchsize', default=1,
 parser.add_argument('--model_name', default='resnet', help='Define model name, must be same as is in service. default: resnet',
                     dest='model_name')
 parser.add_argument('--request_format', default='row_noname', help='Request format according to TF Serving API: row_noname,row_name,column_noname,column_name',
-                    choices=["row_noname","row_name","column_noname","column_name"],dest='request_format')
+                    choices=["row_noname", "row_name", "column_noname", "column_name"], dest='request_format')
 parser.add_argument('--model_version', help='Model version to be used. Default: LATEST',
-                    type=int,dest='model_version')
+                    type=int, dest='model_version')
 args = vars(parser.parse_args())
 
-processing_times = np.zeros((0),int)
+processing_times = np.zeros((0), int)
 
 # optional preprocessing depending on the model
 imgs = np.load(args['images_numpy_path'], mmap_mode='r', allow_pickle=False)
 imgs = imgs - np.min(imgs)  # Normalization 0-255
 imgs = imgs / np.ptp(imgs) * 255  # Normalization 0-255
-#imgs = imgs[:,:,:,::-1] # RGB to BGR
+# imgs = imgs[:,:,:,::-1] # RGB to BGR
 print('Image data range:', np.amin(imgs), ':', np.amax(imgs))
 # optional preprocessing depending on the model
 
@@ -107,9 +108,9 @@ print('\tImages numpy path: {}'.format(args.get('images_numpy_path')))
 
 if args.get('transpose_input') == "True":
     if args.get('transpose_method') == "nhwc2nchw":
-        imgs = imgs.transpose((0,3,1,2))
+        imgs = imgs.transpose((0, 3, 1, 2))
     if args.get('transpose_method') == "nchw2nhwc":
-        imgs = imgs.transpose((0,2,3,1))
+        imgs = imgs.transpose((0, 2, 3, 1))
 print('\tImages in shape: {}\n'.format(imgs.shape))
 
 iteration = 0
@@ -117,7 +118,8 @@ iteration = 0
 while iteration <= iterations:
     for x in range(0, imgs.shape[0] - batch_size + 1, batch_size):
         iteration += 1
-        if iteration > iterations: break
+        if iteration > iterations:
+            break
 
         img = imgs[x:(x + batch_size)]
         if args.get('labels_numpy_path') is not None:
@@ -127,8 +129,8 @@ while iteration <= iterations:
         start_time = datetime.datetime.now()
         version = ""
         if args.get('model_version') is not None:
-            version =  "/versions/{}".format(args.get('model_version'))
-        result = requests.post("{}:{}/v1/models/{}{}:predict".format(args['rest_url'], args['rest_port'],args['model_name'],version), data=data_json)
+            version = "/versions/{}".format(args.get('model_version'))
+        result = requests.post("{}:{}/v1/models/{}{}:predict".format(args['rest_url'], args['rest_port'], args['model_name'], version), data=data_json)
         end_time = datetime.datetime.now()
 
         try:
@@ -149,7 +151,7 @@ while iteration <= iterations:
 
         if type(result_dic[keyname]) is list:  # no named output
             output = result_dic[keyname]
-        else: # dictionary with named outputs
+        else:  # dictionary with named outputs
             if args['output_name'] not in result_dic[keyname]:
                 print("Invalid output name", args['output_name'])
                 print("Available outputs:")
@@ -158,19 +160,17 @@ while iteration <= iterations:
                 exit(1)
             output = result_dic[keyname][args['output_name']]
         duration = (end_time - start_time).total_seconds() * 1000
-        processing_times = np.append(processing_times,np.array([int(duration)]))
+        processing_times = np.append(processing_times, np.array([int(duration)]))
 
-
-        nu = np.array(output) # numpy array with inference results
+        nu = np.array(output)  # numpy array with inference results
 
         # for object classification models show imagenet class
-        print('Iteration {}; Processing time: {:.2f} ms; speed {:.2f} fps'.format(iteration,round(np.average(duration), 2),
-                                                                round(1000 * batch_size / np.average(duration), 2)
-                                                                ))
+        print('Iteration {}; Processing time: {:.2f} ms; speed {:.2f} fps'.format(iteration, round(np.average(duration), 2),
+                                                                                  round(1000 * batch_size / np.average(duration), 2)))
         # Comment out this section for non imagenet datasets
         print("imagenet top results in a single batch:")
         for i in range(nu.shape[0]):
-            single_result = nu[[i],...]
+            single_result = nu[[i], ...]
             ma = np.argmax(single_result)
             mark_message = ""
             if args.get('labels_numpy_path') is not None:
@@ -179,22 +179,22 @@ while iteration <= iterations:
                     matched_count += 1
                     mark_message = "; Correct match."
                 else:
-                    mark_message = "; Incorrect match. Should be {} {}".format(lb[i], classes.imagenet_classes[lb[i]] )
-            print("\t",i, classes.imagenet_classes[ma],ma, mark_message)
+                    mark_message = "; Incorrect match. Should be {} {}".format(lb[i], classes.imagenet_classes[lb[i]])
+            print("\t", i, classes.imagenet_classes[ma], ma, mark_message)
         # Comment out this section for non imagenet datasets
 
 print('\nprocessing time for all iterations')
 print('average time: {:.2f} ms; average speed: {:.2f} fps'.format(round(np.average(processing_times), 2),
-                                                                    round(1000 * batch_size / np.average(processing_times), 2)))
+                                                                  round(1000 * batch_size / np.average(processing_times), 2)))
 
 print('median time: {:.2f} ms; median speed: {:.2f} fps'.format(round(np.median(processing_times), 2),
-                                                                  round(1000 * batch_size / np.median(processing_times), 2)))
+                                                                round(1000 * batch_size / np.median(processing_times), 2)))
 
 print('max time: {:.2f} ms; max speed: {:.2f} fps'.format(round(np.max(processing_times), 2),
-                                                            round(1000 * batch_size / np.max(processing_times), 2)))
+                                                          round(1000 * batch_size / np.max(processing_times), 2)))
 
 print('min time: {:.2f} ms; min speed: {:.2f} fps'.format(round(np.min(processing_times), 2),
-                                                            round(1000 * batch_size / np.min(processing_times), 2)))
+                                                          round(1000 * batch_size / np.min(processing_times), 2)))
 
 print('time percentile 90: {:.2f} ms; speed percentile 90: {:.2f} fps'.format(
     round(np.percentile(processing_times, 90), 2),
