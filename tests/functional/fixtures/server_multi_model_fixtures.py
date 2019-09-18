27b900a7f1732cd1d8500781f30522e2472d80a5
mzegla <milosz.zeglarski@intel.com>
2019-09-18 14:53:08 +0200

    fix style

--- tests/functional/fixtures/server_multi_model_fixtures.py
+++ tests/functional/fixtures/server_multi_model_fixtures.py
@@ -27,11 +27,21 @@ def start_server_multi_model(request, get_image, get_test_dir,
     shutil.copyfile('tests/functional/config.json',
                     get_test_dir + '/saved_models/config.json')
 
+    GOOGLE_APPLICATION_CREDENTIALS = \
+        os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_REGION = os.getenv('AWS_REGION')
 
     client = get_docker_context
-
+    envs = ['GOOGLE_APPLICATION_CREDENTIALS=/etc/gcp.json',
+            'AWS_ACCESS_KEY_ID=' + AWS_ACCESS_KEY_ID,
+            'AWS_SECRET_ACCESS_KEY=' + AWS_SECRET_ACCESS_KEY,
+            'AWS_REGION=' + AWS_REGION]
     volumes_dict = {'{}'.format(get_test_dir + '/saved_models/'):
-                    {'bind': '/opt/ml', 'mode': 'ro'}}
+                    {'bind': '/opt/ml', 'mode': 'ro'},
+                    GOOGLE_APPLICATION_CREDENTIALS:
+                        {'bind': '/etc/gcp.json', 'mode': 'ro'}}
     command = "/ie-serving-py/start_server.sh ie_serving config " \
               "--config_path /opt/ml/config.json --port 9001 " \
               "--rest_port 5561"
@@ -41,6 +51,7 @@ def start_server_multi_model(request, get_image, get_test_dir,
                                       ports={'9001/tcp': 9001,
                                              '5561/tcp': 5561},
                                       remove=True, volumes=volumes_dict,
+                                      environment=envs,
                                       command=command)
     request.addfinalizer(container.kill)
 
