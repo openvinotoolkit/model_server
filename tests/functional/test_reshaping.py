8e90ff9a0018b3e239664c84973d079de6a8aea8
mzegla <milosz.zeglarski@intel.com>
2019-09-18 13:36:52 +0200

    init

--- tests/functional/test_reshaping.py
+++ tests/functional/test_reshaping.py
@@ -29,18 +29,21 @@ shapes = [
     {'in': (1, 3, 1024, 1024), 'out': (1, 1, 200, 7)},
 ]
 
+fixed_input_shape = (1, 3, 600, 600)
+fixed_output_shape = (1, 1, 200, 7)
+
 
 class TestModelReshaping:
 
-    def test_single_local_model_reshaping(
+    def test_single_local_model_reshaping_auto(
             self, face_detection_model_downloader,
-            start_server_face_detection_model,
+            start_server_face_detection_model_auto_shape,
             create_grpc_channel):
 
         print("Downloaded model files:", face_detection_model_downloader)
 
         # Connect to grpc service
-        stub = create_grpc_channel('localhost:9000', PREDICTION_SERVICE)
+        stub = create_grpc_channel('localhost:9010', PREDICTION_SERVICE)
 
         out_name = 'detection_out'
         for shape in shapes:
@@ -53,18 +56,55 @@ class TestModelReshaping:
             print("output shape", output[out_name].shape)
             assert output[out_name].shape == shape['out'], ERROR_SHAPE
 
+    @pytest.mark.skip(reason="Fixed shape option not yet merged")
+    @pytest.mark.parametrize("shape, is_correct",
+                             [(fixed_input_shape, True), ((1, 3, 300, 300),
+                                                          False)])
+    def test_single_local_model_reshaping_fixed(
+            self, face_detection_model_downloader,
+            start_server_face_detection_model_named_shape,
+            start_server_face_detection_model_nonamed_shape,
+            create_grpc_channel, shape, is_correct):
+
+        print("Downloaded model files:", face_detection_model_downloader)
+
+        # Connect to grpc service
+        stubs = [create_grpc_channel('localhost:9011', PREDICTION_SERVICE),
+                 create_grpc_channel('localhost:9012', PREDICTION_SERVICE)]
+
+        out_name = 'detection_out'
+
+        for stub in stubs:
+            imgs = np.zeros(shape)
+            if is_correct:
+                output = infer_batch(batch_input=imgs,
+                                     input_tensor='data', grpc_stub=stub,
+                                     model_spec_name='face_detection',
+                                     model_spec_version=None,
+                                     output_tensors=[out_name])
+                print("output shape", output[out_name].shape)
+                assert output[out_name].shape == fixed_output_shape, \
+                    ERROR_SHAPE
+            else:
+                with pytest.raises(Exception):
+                    infer_batch(batch_input=imgs,
+                                input_tensor='data', grpc_stub=stub,
+                                model_spec_name='face_detection',
+                                model_spec_version=None,
+                                output_tensors=[out_name])
+
     @pytest.mark.parametrize("request_format",
                              [('row_name'), ('row_noname'),
                               ('column_name'), ('column_noname')])
-    def test_single_local_model_reshaping_rest(
+    def test_single_local_model_reshaping_auto_rest(
             self, face_detection_model_downloader,
-            start_server_face_detection_model, request_format):
+            start_server_face_detection_model_auto_shape, request_format):
 
         print("Downloaded model files:", face_detection_model_downloader)
         out_name = 'detection_out'
         for shape in shapes:
             imgs = np.zeros(shape['in'])
-            rest_url = 'http://localhost:5555/v1/models/face_detection' \
+            rest_url = 'http://localhost:5565/v1/models/face_detection' \
                        ':predict'
             output = infer_batch_rest(batch_input=imgs,
                                       input_tensor='data', rest_url=rest_url,
@@ -73,7 +113,7 @@ class TestModelReshaping:
             print("output shape", output[out_name].shape)
             assert output[out_name].shape == shape['out'], ERROR_SHAPE
 
-    def test_multi_local_model_reshaping(
+    def test_multi_local_model_reshaping_auto(
             self, face_detection_model_downloader,
             start_server_multi_model,
             create_grpc_channel):
@@ -88,16 +128,54 @@ class TestModelReshaping:
             imgs = np.zeros(shape['in'])
             output = infer_batch(batch_input=imgs,
                                  input_tensor='data', grpc_stub=stub,
-                                 model_spec_name='face_detection',
+                                 model_spec_name='face_detection_auto',
                                  model_spec_version=None,
                                  output_tensors=[out_name])
             print("output shape", output[out_name].shape)
             assert output[out_name].shape == shape['out'], ERROR_SHAPE
 
+    @pytest.mark.skip(reason="Fixed shape option not yet merged")
+    @pytest.mark.parametrize("shape, is_correct",
+                             [(fixed_input_shape, True), ((1, 3, 300, 300),
+                              False)])
+    def test_multi_local_model_reshaping_fixed(
+            self, face_detection_model_downloader,
+            start_server_multi_model,
+            create_grpc_channel, shape, is_correct):
+
+        print("Downloaded model files:", face_detection_model_downloader)
+
+        # Connect to grpc service
+        stub = create_grpc_channel('localhost:9001', PREDICTION_SERVICE)
+
+        models_names = ["face_detection_fixed_noname",
+                        "face_detection_fixed_name"]
+
+        out_name = 'detection_out'
+
+        imgs = np.zeros(shape)
+        for model_name in models_names:
+            if is_correct:
+                output = infer_batch(batch_input=imgs,
+                                     input_tensor='data', grpc_stub=stub,
+                                     model_spec_name=model_name,
+                                     model_spec_version=None,
+                                     output_tensors=[out_name])
+                print("output shape", output[out_name].shape)
+                assert output[out_name].shape == fixed_output_shape, \
+                    ERROR_SHAPE
+            else:
+                with pytest.raises(Exception):
+                    infer_batch(batch_input=imgs,
+                                input_tensor='data', grpc_stub=stub,
+                                model_spec_name=model_name,
+                                model_spec_version=None,
+                                output_tensors=[out_name])
+
     @pytest.mark.parametrize("request_format",
                              [('row_name'), ('row_noname'),
                               ('column_name'), ('column_noname')])
-    def test_mutli_local_model_reshaping_rest(
+    def test_mutli_local_model_reshaping_auto_rest(
             self, face_detection_model_downloader,
             start_server_multi_model, request_format):
 
