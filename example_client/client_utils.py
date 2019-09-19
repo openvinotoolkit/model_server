26eb869ecd54b4045dad6c79d9f0943e591c2856
Trawinski, Dariusz <dariusz.trawinski@intel.com>
2019-09-19 15:39:11 +0200

    face detection client (#95)
    face detection client

--- example_client/client_utils.py
+++ example_client/client_utils.py
@@ -10,9 +10,9 @@ def print_statistics(processing_times, batch_size):
           .format(round(np.median(processing_times), 2),
                  round(1000 * batch_size / np.median(processing_times), 2)))
 
-    print('max time: {:.2f} ms; max speed: {:.2f} fps'.format(round(np.max(processing_times), 2),round(1000 * batch_size / np.max(processing_times), 2)))
+    print('max time: {:.2f} ms; min speed: {:.2f} fps'.format(round(np.max(processing_times), 2),round(1000 * batch_size / np.max(processing_times), 2)))
 
-    print('min time: {:.2f} ms; min speed: {:.2f} fps'.format(round(np.min(processing_times), 2),
+    print('min time: {:.2f} ms; max speed: {:.2f} fps'.format(round(np.min(processing_times), 2),
                                                           round(1000 * batch_size / np.min(processing_times), 2)))
 
     print('time percentile 90: {:.2f} ms; speed percentile 90: {:.2f} fps'.format(
