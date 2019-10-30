6eb1b4d27d748970f15c4e64d2de786cb5e28b91
mzegla <milosz.zeglarski@intel.com>
2019-10-30 09:20:16 +0100

    fix2

--- tests/functional/fixtures/server_remote_models_fixtures.py
+++ tests/functional/fixtures/server_remote_models_fixtures.py
@@ -34,8 +34,8 @@ def start_server_single_model_from_gc(request, get_image, get_test_dir,
               "--model_name resnet " \
               "--model_path gs://inference-eu/ml-test " \
               "--port 9000 --target_device CPU --nireq 4 --plugin_config " \
-              "\"{\"CPU_THROUGHPUT_STREAMS\": \"2\", " \
-              "\"CPU_THREADS_NUM\": \"4\"}\""
+              "\"{\\\"CPU_THROUGHPUT_STREAMS\\\": \\\"2\\\", " \
+              "\\\"CPU_THREADS_NUM\\\": \\\"4\\\"}\""
 
     container = client.containers.run(image=get_image, detach=True,
                                       name='ie-serving-py-test-single-gs',
