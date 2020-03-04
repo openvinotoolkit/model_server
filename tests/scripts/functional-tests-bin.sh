9bab9e3843e038d2c733533e7ac9efff7db98a80
Krzysztof Romanowski <krzysztof.romanowski@intel.com>
2020-03-04 10:00:39 +0100

    Jenkins: functional for bin and apt ubuntu
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski@intel.com>

--- tests/scripts/functional-tests-bin.sh
+++ tests/scripts/functional-tests-bin.sh
@@ -5,6 +5,6 @@ TEST_DIRS=tests
 
 make docker_build_bin dldt_package_url=${OPENVINO_DOWNLOAD_LINK_2020_1}
 
-. .venv-jenkins/bin/activate
+. .venv/bin/activate
 
-py.test ${TEST_DIRS}/functional/ -v --test_dir=/var/jenkins_home/test_ovms_models
+py.test ${TEST_DIRS}/functional/ -v --test_dir=/var/jenkins_home/test_models
