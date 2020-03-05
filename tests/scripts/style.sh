6514df17132b10fc6621539d23656badb233689f
Krzysztof Romanowski <krzysztof.romanowski@intel.com>
2020-03-05 11:46:06 +0100

    Don't prepare virtualenv as separate step
    Signed-off-by: Krzysztof Romanowski <krzysztof.romanowski@intel.com>

--- tests/scripts/style.sh
+++ tests/scripts/style.sh
@@ -4,5 +4,12 @@ set -ex
 STYLE_CHECK_OPTS="${STYLE_CHECK_OPTS:---exclude=ie_serving/tensorflow_serving_api}" 
 STYLE_CHECK_DIRS="${STYLE_CHECK_DIRS:-tests ie_serving setup.py}"
 
-. .venv/bin/activate
+python3 -m pip install -U virtualenv
+python3 -m virtualenv .venv-style
+
+. .venv-style/bin/activate
+
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+
 flake8 ${STYLE_CHECK_OPTS} ${STYLE_CHECK_DIRS}
