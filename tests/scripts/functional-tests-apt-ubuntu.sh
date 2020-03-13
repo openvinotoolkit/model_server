5b3bb30fb25f73789c7b084d05b7e884b36b5cbb
Trawinski, Dariusz <dariusz.trawinski@intel.com>
2020-03-13 13:05:52 +0100

    review changes (#163)
    Reviewed and corrected readme with minor edits.
    
    
    Co-authored-by: Ryan Loney <ryan.loney@intel.com>
    Co-authored-by: rasapala <58549742+rasapala@users.noreply.github.com>

--- tests/scripts/functional-tests-apt-ubuntu.sh
+++ tests/scripts/functional-tests-apt-ubuntu.sh
@@ -7,4 +7,4 @@ make docker_build_apt_ubuntu
 
 . .venv-jenkins/bin/activate
 
-py.test ${TEST_DIRS}/functional/ -v --test_dir=/var/jenkins_home/test_models_mzeglars
+py.test ${TEST_DIRS}/functional/ -v --test_dir=/var/jenkins_home/test_ovms_models
