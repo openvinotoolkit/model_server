1c9fc67794e3652c0a2b88ea53670639580c3ea7
Patryk Matyjasek <patryk.matyjasek@intel.com>
2020-04-28 09:26:51 +0200

    grpc predict call implementation
    Signed-off-by: Patryk Matyjasek <patryk.matyjasek@intel.com>

--- extras/ams_wrapper/src/api/ovms_connector.py
+++ extras/ams_wrapper/src/api/ovms_connector.py
@@ -17,6 +17,12 @@
 import grpc
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2_grpc
+from tensorflow import make_tensor_proto, make_ndarray
+import numpy as np
+
+
+class ModelNotFoundError(ValueError):
+    pass
 
 class OvmsConnector():
     def __init__(self, ovms_port, ovms_model_info):
@@ -25,12 +31,35 @@ class OvmsConnector():
         self.model_version = ovms_model_info['model_version']
         self.input_name = ovms_model_info['input_name']
         self.input_shape = ovms_model_info['input_shape']
+        self.output_name = ovms_model_info["output_name"] 
 
         channel = grpc.insecure_channel("{}:{}".format("127.0.0.1", self.ovms_port))
         self.stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
-    
+
     def send(self, inference_input):
         # TODO: prepare request and handle response
         request = predict_pb2.PredictRequest()
-        result = self.stub.Predict(request, 10.0)
-        return 
+        request.model_spec.name = self.model_name
+        try:
+            tensor_proto = make_tensor_proto(inference_input, shape=(inference_input.shape))
+        except TypeError as e:
+            raise TypeError("Unsupported data type") from e
+        except ValueError as e:
+            raise ValueError("Invalid arguments") from e
+        request.inputs[self.input_name].CopyFrom(tensor_proto)
+        try:
+            result = self.stub.Predict(request, 10.0)
+            output = make_ndarray(result.outputs[self.output_name])
+            numpy_array = np.array(output)
+        except grpc.RpcError as e:
+            if e.code() == grpc.StatusCode.INVALID_ARGUMENT:
+                raise ValueError("Invalid argument: {}".format(e.details())) from e
+            elif e.code() == grpc.StatusCode.NOT_FOUND:
+                raise ModelNotFoundError("Requested model not found") from e
+            else:
+                raise Exception("GRPC error") from e
+        except TypeError as e:
+            raise TypeError("Output datatype error") from e
+
+        return numpy_array
+
