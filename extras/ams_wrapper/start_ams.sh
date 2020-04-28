4b9acfa0a8eedd9ac06798da467fb5a2ec156bdb
mzeglars <milosz.zeglarski@intel.com>
2020-04-28 14:25:28 +0200

    help message and -e option in launching script

--- extras/ams_wrapper/start_ams.sh
+++ extras/ams_wrapper/start_ams.sh
@@ -1,14 +1,54 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # TODO: 
-#   * Consider more user friendly arguments passing
 #   * Consider parametrizing paths
 #   * Might require providing mapping for neural network output
 
-export FILE_SYSTEM_POLL_WAIT_SECONDS=0
+help_message="
+    This script runs OpenVINO Model Server and AMS Service in the background.
+    OVMS will served models available under path /opt/models with configuration
+    defined in /opt/models/config.json file. 
+    
+    Options:
+    --help                          Show help message
+    --ams_port <ams_port>           Port for AMS Service to listen on
+                                    (default: 5000)
+    --ovms_port <ovms_port>         Port for OVMS to listen on
+                                    (default: 9000)
 
-AMS_PORT=${1:-5000}
-OVMS_PORT=${2:-9000}
+    Example:
+    ./start_ams.sh --ams_port=4000 --ovms_port=8080
+    
+    This command will start AMS service listening on port 4000 and OVMS service
+    listening on port 8080.
+    "
+
+
+export FILE_SYSTEM_POLL_WAIT_SECONDS=0
+AMS_PORT=5000
+OVMS_PORT=9000
+
+for i in "$@"
+do
+case $i in
+    --help=*)
+        echo "$help_message"
+        exit 0
+    ;;
+    --ams_port=*)
+        AMS_PORT="${i#*=}"
+        shift # past argument=value
+    ;;
+    --ovms_port=*)
+        OVMS_PORT="${i#*=}"
+        shift # past argument=value
+    ;;
+    *)
+        echo "$help_message"
+        exit 0
+    ;;
+esac
+done
 
 . /ie-serving-py/.venv/bin/activate
 /ie-serving-py/start_server.sh ie_serving config --config_path /opt/models/config.json --port $OVMS_PORT & 
