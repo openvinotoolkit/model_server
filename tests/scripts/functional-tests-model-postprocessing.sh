6362b96c0113d3b1f8e87d2e6fb13e365f4891cc
rasapala <rafal.a.sapala@intel.com>
2020-05-06 16:34:18 +0200

    Fix functional jenkins tests.

--- tests/scripts/functional-tests-model-postprocessing.sh
+++ tests/scripts/functional-tests-model-postprocessing.sh
@@ -2,22 +2,13 @@
 set -ex
 
 TEST_DIRS=tests
-DOCKER_OVMS_TAG="ie-serving-bin:latest"
-export TESTS_SUFFIX="bin"
-export PORTS_PREFIX="92 57"
+DOCKER_OVMS_TAG="ie-serving-apt-ubuntu:latest"
+export TESTS_SUFFIX="apt-ubuntu"
+export PORTS_PREFIX="91 56"
 
-mkdir -p extras/models
-make DOCKER_OVMS_TAG=${DOCKER_OVMS_TAG} docker_build_ams dldt_package_url=${OPENVINO_DOWNLOAD_LINK_2020_2}
+make DOCKER_OVMS_TAG=${DOCKER_OVMS_TAG} docker_build_apt_ubuntu
 
 . .venv-jenkins/bin/activate
 
-# Start mock server
-# TODO: remove this section and related changes after ams wrapper is ready for functional tests
-python ${TEST_DIRS}/functional/utils/ams_mock_server.py &
-mock_server_pid=$!
+py.test ${TEST_DIRS}/functional/test_single_model_vehicle.py ${TEST_DIRS}/functional/test_single_model_vehicle_attributes.py  -v --test_dir=/var/jenkins_home/test_ovms_models-${TESTS_SUFFIX} --image ${DOCKER_OVMS_TAG}
 
-set +e
-NO_PROXY=localhost py.test ${TEST_DIRS}/functional/test_single_model_vehicle* -v --test_dir=/var/jenkins_home/test_ovms_models-${TESTS_SUFFIX} --image ${DOCKER_OVMS_TAG}
-exit_code=$?
-kill ${mock_server_pid}
-exit ${exit_code}
