f81ac553f6a1c10d73547374ee3c6670e32eb741
Mateusz Ciesielski <mateusz.j.ciesielski@intel.com>
2020-05-07 14:11:53 +0200

    Explicit cast to float32 in image preprocessing (#220)

--- extras/ams_wrapper/src/preprocessing/preprocess_image.py
+++ extras/ams_wrapper/src/preprocessing/preprocess_image.py
@@ -41,7 +41,7 @@ class ImageTransformError(ValueError):
 def preprocess_binary_image(image: bytes, channels: int = None,
                             target_size: Tuple[int, int] = None,
                             channels_first=True,
-                            dtype=tf.dtypes.uint8, scale: float = None,
+                            dtype=tf.dtypes.float32, scale: float = None,
                             standardization=False,
                             reverse_input_channels=True) -> np.ndarray:
     """
@@ -88,7 +88,8 @@ def preprocess_binary_image(image: bytes, channels: int = None,
         raise TypeError('Invalid type {} for parameter dtype.'.format(type(dtype)))
 
     try:
-        decoded_image = tf.io.decode_image(image, channels=channels, dtype=dtype)
+        decoded_image = tf.io.decode_image(image, channels=channels)
+        tf.dtypes.cast(decoded_image, dtype)
     except Exception as e:
         raise ImageDecodeError('Provided image is invalid, unable to decode.') from e
 
