5cbf78e14958c597d9dce64bc972d53cee66ad81
Mateusz Ciesielski <mateusz.j.ciesielski@intel.com>
2020-05-11 11:13:57 +0200

    Review fixes

--- extras/ams_wrapper/src/api/models/model.py
+++ extras/ams_wrapper/src/api/models/model.py
@@ -40,7 +40,6 @@ class Model(ABC):
                  config_file_path: str = None):
         self.ovms_connector = ovms_connector
         self.model_name = model_name
-        #self.labels = self.load_labels(config_file_path)
         self.input_configs = self.load_input_configs(config_file_path)
         self.output_configs = self.load_output_configs(config_file_path) 
         self.labels = {output_name: {index: label for label, index in self.output_configs[output_name].classes.items()}
