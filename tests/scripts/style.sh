b09c2de2d48b2bb64adbdea57243f3094ea1a323
Mateusz Ciesielski <mateusz.j.ciesielski@intel.com>
2020-05-19 14:10:43 +0200

    Fix style.sh

--- tests/scripts/style.sh
+++ tests/scripts/style.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-STYLE_CHECK_OPTS="${STYLE_CHECK_OPTS:---exclude=ie_serving/tensorflow_serving_api}" 
-STYLE_CHECK_DIRS="${STYLE_CHECK_DIRS:-tests ie_serving setup.py}"
+STYLE_CHECK_OPTS="${STYLE_CHECK_OPTS:---exclude=ie_serving/tensorflow_serving_api --max-line-length 120}" 
+STYLE_CHECK_DIRS="${STYLE_CHECK_DIRS:-tests ie_serving setup.py extras}"
 
 . .venv-jenkins/bin/activate
 
