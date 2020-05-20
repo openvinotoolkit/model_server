e337e8d5937690075a6e7da503522a255d5af964
Dariusz Trawinski <dariusz.trawinski@intel.com>
2020-05-20 17:10:35 +0200

    run tests on both images

--- tests/scripts/functional-tests-ams.sh
+++ tests/scripts/functional-tests-ams.sh
@@ -6,10 +6,9 @@ DOCKER_OVMS_TAG="ams:latest"
 export TESTS_SUFFIX="bin"
 export PORTS_PREFIX="92 57"
 
-make DOCKER_OVMS_TAG=${DOCKER_OVMS_TAG} docker_build_ams dldt_package_url=${OPENVINO_DOWNLOAD_LINK_2020_2}
-make DOCKER_OVMS_TAG=${DOCKER_OVMS_TAG}-clearlinux docker_build_ams_clearlinux dldt_package_url=${OPENVINO_DOWNLOAD_LINK_2020_2}
-
 . .venv-jenkins/bin/activate
+
+make DOCKER_OVMS_TAG=${DOCKER_OVMS_TAG} docker_build_ams dldt_package_url=${OPENVINO_DOWNLOAD_LINK_2020_2}
 
 NO_PROXY=localhost py.test ${TEST_DIRS}/functional/test_single_model_vehicle_attributes.py \
                            ${TEST_DIRS}/functional/test_ams_inference.py \
@@ -21,3 +20,14 @@ NO_PROXY=localhost py.test ${TEST_DIRS}/functional/test_single_model_vehicle_att
                            -v --test_dir=/var/jenkins_home/test_ovms_models-${TESTS_SUFFIX} \
                            --image ${DOCKER_OVMS_TAG}-clearlinux
 
+make DOCKER_OVMS_TAG=${DOCKER_OVMS_TAG}-clearlinux docker_build_ams_clearlinux dldt_package_url=${OPENVINO_DOWNLOAD_LINK_2020_2}
+
+NO_PROXY=localhost py.test ${TEST_DIRS}/functional/test_single_model_vehicle_attributes.py \
+                           ${TEST_DIRS}/functional/test_ams_inference.py \
+                           -v --test_dir=/var/jenkins_home/test_ovms_models-${TESTS_SUFFIX} \
+                           --image ${DOCKER_OVMS_TAG}
+
+NO_PROXY=localhost py.test ${TEST_DIRS}/functional/test_single_model_vehicle_attributes.py \
+                           ${TEST_DIRS}/functional/test_ams_inference.py \
+                           -v --test_dir=/var/jenkins_home/test_ovms_models-${TESTS_SUFFIX} \
+                           --image ${DOCKER_OVMS_TAG}-clearlinux
