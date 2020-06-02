b5634a9a46a3a9fdeeaba16b4bff2584a498a6c9
rasapala <rafal.a.sapala@intel.com>
2020-06-02 17:40:47 +0200

    Return to Ignore.

--- tests/scripts/functional-tests-clearlinux.sh
+++ tests/scripts/functional-tests-clearlinux.sh
@@ -10,4 +10,4 @@ make DOCKER_OVMS_TAG=${DOCKER_OVMS_TAG} docker_build_clearlinux
 
 . .venv-jenkins/bin/activate
 
-py.test ${TEST_DIRS}/functional/ -v --test_dir=/var/jenkins_home/test_ovms_models-${TESTS_SUFFIX} --image ${DOCKER_OVMS_TAG} --ignore=${TEST_DIRS}/functional/test_ams_inference.py  --ignore=${TEST_DIRS}/functional/ams_schemas.py --ignore-glob=${TEST_DIRS}/functional/test_single_model_vehicle*
+py.test ${TEST_DIRS}/functional/ -v --test_dir=/var/jenkins_home/test_ovms_models-${TESTS_SUFFIX} --image ${DOCKER_OVMS_TAG} --ignore=${TEST_DIRS}/functional/test_ams_inference.py  --ignore=${TEST_DIRS}/functional/ams_schemas.py --ignore=${TEST_DIRS}/functional/test_single_model_vehicle_attributes.py --ignore=${TEST_DIRS}/functional/test_single_model_vehicle.py
