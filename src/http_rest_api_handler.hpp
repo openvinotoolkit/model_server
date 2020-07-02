e70b87764aa0f0cc7733974d03aa894ac3fb81a4
Gumowski, Mariusz <mariusz.gumowski@intel.com>
2020-07-02 01:34:15 -0700

    CVS-30317 REST server

---
+++ src/http_rest_api_handler.hpp
@@ -0,0 +1,116 @@
+//*****************************************************************************
+// Copyright 2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+#pragma once
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "status.hpp"
+
+namespace ovms {
+
+class HttpRestApiHandler {
+public:
+    static const std::string kPathRegex;
+    static const std::string predictionRegexExp;
+    static const std::string modelstatusRegexExp;
+
+    /**
+     * @brief Construct a new HttpRest Api Handler
+     * 
+     * @param timeout_in_ms 
+     */
+    HttpRestApiHandler(int timeout_in_ms) :
+        predictionRegex(predictionRegexExp),
+        modelstatusRegex(modelstatusRegexExp),
+        timeout_in_ms(timeout_in_ms) {}
+
+    /**
+     * @brief Process Request
+     * 
+     * @param http_method 
+     * @param request_path 
+     * @param request_body 
+     * @param headers 
+     * @param resposnse 
+     *
+     * @return StatusCode 
+     */
+    StatusCode processRequest(
+        const std::string_view http_method,
+        const std::string_view request_path,
+        const std::string_view request_body,
+        std::vector<std::pair<std::string, std::string>>* headers,
+        std::string* resposnse);
+
+    /**
+     * @brief Process predict request
+     *
+     * @param model_name 
+     * @param model_version 
+     * @param model_version_label 
+     * @param request 
+     * @param response 
+     *
+     * @return StatusCode 
+     */
+    StatusCode processPredictRequest(
+        const std::string_view model_name,
+        const std::optional<int64_t>& model_version,
+        const std::optional<std::string_view>& model_version_label,
+        const std::string_view request,
+        std::string* response);
+
+    /**
+     * @brief Process Model Metadata request
+     * 
+     * @param model_name 
+     * @param model_version 
+     * @param model_version_label 
+     * @param response
+     *
+     * @return StatusCode 
+     */
+    StatusCode processModelMetadataRequest(
+        const std::string_view model_name,
+        const std::optional<int64_t>& model_version,
+        const std::optional<std::string_view>& model_version_label,
+        std::string* response);
+
+    /**
+     * @brief Process Model Status request
+     * 
+     * @param model_name 
+     * @param model_version 
+     * @param model_version_label 
+     * @param response 
+     * @return StatusCode 
+     */
+    StatusCode processModelStatusRequest(
+        const std::string_view model_name,
+        const std::optional<int64_t>& model_version,
+        const std::optional<std::string_view>& model_version_label,
+        std::string* response);
+
+private:
+    const std::regex predictionRegex;
+    const std::regex modelstatusRegex;
+
+    int timeout_in_ms;
+};
+
+}  // namespace ovms
---
+++ src/http_server.cpp
@@ -0,0 +1,132 @@
+//*****************************************************************************
+// Copyright 2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+#include <memory>
+#include <regex>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <spdlog/spdlog.h>
+
+#include "tensorflow_serving/util/net_http/server/public/httpserver.h"
+#include "tensorflow_serving/util/net_http/server/public/response_code_enum.h"
+#include "tensorflow_serving/util/net_http/server/public/server_request_interface.h"
+#include "tensorflow_serving/util/threadpool_executor.h"
+
+#include "http_rest_api_handler.hpp"
+#include "http_server.hpp"
+
+namespace ovms {
+
+namespace net_http = tensorflow::serving::net_http;
+
+class RequestExecutor final : public net_http::EventExecutor {
+public:
+    explicit RequestExecutor(int num_threads)
+        : executor_(tensorflow::Env::Default(), "httprestserver", num_threads) {}
+
+    void Schedule(std::function<void()> fn) override { executor_.Schedule(fn); }
+
+private:
+    tensorflow::serving::ThreadPoolExecutor executor_;
+};
+
+class RestApiRequestDispatcher {
+public:
+    RestApiRequestDispatcher(int timeout_in_ms)
+                             : regex_(HttpRestApiHandler::kPathRegex) {
+        handler_.reset(new HttpRestApiHandler(timeout_in_ms));
+    }
+
+    net_http::RequestHandler dispatch(net_http::ServerRequestInterface* req) {
+        std::smatch sm;
+        auto strreq = std::string(req->uri_path());
+        if (std::regex_match(strreq, sm, regex_)) {
+            return [this](net_http::ServerRequestInterface* req) {
+                this->processRequest(req);
+            };
+        }
+        spdlog::warn("Ignoring HTTP request: {} {}", req->http_method(), req->uri_path());
+
+        return nullptr;
+    }
+
+private:
+    void processRequest(net_http::ServerRequestInterface* req) {
+        spdlog::info("REST request {}", req->uri_path());
+        std::string body;
+        int64_t num_bytes = 0;
+        auto request_chunk = req->ReadRequestBytes(&num_bytes);
+        while (request_chunk != nullptr) {
+            body.append(std::string_view(request_chunk.get(), num_bytes));
+            request_chunk = req->ReadRequestBytes(&num_bytes);
+        }
+
+        std::vector<std::pair<std::string, std::string>> headers;
+        std::string output;
+        spdlog::info("Processing HTTP request: {} {} body: {} bytes",
+                     req->http_method(),
+                     req->uri_path(),
+                     body.size());
+        const auto status = handler_->processRequest(req->http_method(), req->uri_path(), body, &headers, &output);
+        const auto http_status = Status::toHTTPStatusCode(status);
+        for (const auto& kv : headers) {
+            req->OverwriteResponseHeader(kv.first, kv.second);
+        }
+        req->WriteResponseString(output);
+        if (http_status != net_http::HTTPStatusCode::OK) {
+            spdlog::error("Error Processing HTTP/REST request: {} {} Error: {}",
+                          req->http_method(),
+                          req->uri_path(),
+                          status);
+        }
+        req->ReplyWithStatus(http_status);
+    }
+
+    const std::regex regex_;
+    std::unique_ptr<HttpRestApiHandler> handler_;
+};
+
+std::unique_ptr<http_server> createAndStartHttpServer(int port, int num_threads, int timeout_in_ms) {
+    auto options = std::make_unique<net_http::ServerOptions>();
+    options->AddPort(static_cast<uint32_t>(port));
+    options->SetExecutor(std::make_unique<RequestExecutor>(num_threads));
+
+    auto server = net_http::CreateEvHTTPServer(std::move(options));
+    if (server == nullptr) {
+        spdlog::error("Failed to create http server");
+        return nullptr;
+    }
+
+    std::shared_ptr<RestApiRequestDispatcher> dispatcher =
+        std::make_shared<RestApiRequestDispatcher>(timeout_in_ms);
+
+    net_http::RequestHandlerOptions handler_options;
+    server->RegisterRequestDispatcher(
+        [dispatcher](net_http::ServerRequestInterface* req) {
+            return dispatcher->dispatch(req);
+        },
+        handler_options);
+
+    if (server->StartAcceptingRequests()) {
+        spdlog::info("REST server listening on port {} with {} threads", port, num_threads);
+        return server;
+    }
+
+    return nullptr;
+}
+
+}  // namespace ovms
