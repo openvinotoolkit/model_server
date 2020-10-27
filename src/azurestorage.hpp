19023c3a4a34413689849a88f914553a05fc8ce0
rasapala <rafal.a.sapala@intel.com>
2020-10-27 14:51:40 +0100

    Fix ignore pragma.

--- src/azurestorage.hpp
+++ src/azurestorage.hpp
@@ -26,6 +26,7 @@
 
 #include "status.hpp"
 #pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wall"
 #pragma GCC diagnostic ignored "-Wreorder"
 #include "was/blob.h"
 #include "was/common.h"
