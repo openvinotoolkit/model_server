aeae5e024df7a1bf9265af6e8c5a8e04024ab7c8
rasapala <rafal.a.sapala@intel.com>
2020-11-17 15:30:11 +0100

    Cleanup.

--- example_client/grpc_stateful_client.py
+++ example_client/grpc_stateful_client.py
@@ -25,33 +25,17 @@ from tensorflow_serving.apis import prediction_service_pb2_grpc
 from client_utils import print_statistics, prepare_certs
 from kaldi_python_io import ArchiveReader
 
-parser = argparse.ArgumentParser(description='Sends requests via TFS gRPC API using images in numpy format. '
-                                             'It displays performance statistics and optionally the model accuracy')
-parser.add_argument('--images_numpy_path', required=True, help='numpy in shape [n,w,h,c] or [n,c,h,w]')
-parser.add_argument('--labels_numpy_path', required=False, help='numpy in shape [n,1] - can be used to check model accuracy')
+parser = argparse.ArgumentParser(description='Sends requests via TFS gRPC API using data in stateful model ark input file. '
+                                             'It displays performance statistics and optionally')
+parser.add_argument('--model_input_path', required=False, default='rm_lstm4f/test_feat_1_10.ark' help='path to input ark file')
 parser.add_argument('--grpc_address',required=False, default='localhost',  help='Specify url to grpc service. default:localhost')
 parser.add_argument('--grpc_port',required=False, default=9000, help='Specify port to grpc service. default: 9000')
-parser.add_argument('--input_name',required=False, default='input', help='Specify input tensor name. default: input')
-parser.add_argument('--output_name',required=False, default='resnet_v1_50/predictions/Reshape_1',
-                    help='Specify output name. default: resnet_v1_50/predictions/Reshape_1')
-parser.add_argument('--transpose_input', choices=["False", "True"], default="True",
-                    help='Set to False to skip NHWC>NCHW or NCHW>NHWC input transposing. default: True',
-                    dest="transpose_input")
-parser.add_argument('--transpose_method', choices=["nchw2nhwc","nhwc2nchw"], default="nhwc2nchw",
-                    help="How the input transposition should be executed: nhwc2nchw or nchw2nhwc",
-                    dest="transpose_method")
-parser.add_argument('--iterations', default=0,
-                    help='Number of requests iterations, as default use number of images in numpy memmap. default: 0 (consume all frames)',
-                    dest='iterations', type=int)
-# If input numpy file has too few frames according to the value of iterations and the batch size, it will be
-# duplicated to match requested number of frames
-parser.add_argument('--batchsize', default=1,
-                    help='Number of images in a single request. default: 1',
-                    dest='batchsize')
-parser.add_argument('--model_name', default='resnet', help='Define model name, must be same as is in service. default: resnet',
+parser.add_argument('--input_name',required=False, default='Parameter', help='Specify input tensor name. default: Parameter')
+parser.add_argument('--output_name',required=False, default='affinetransform/Fused_Add_',
+                    help='Specify output name. default: affinetransform/Fused_Add_')
+parser.add_argument('--model_name', default='rm_lstm4f', help='Define model name, must be same as is in service. default: rm_lstm4f',
                     dest='model_name')
-parser.add_argument('--pipeline_name', default='', help='Define pipeline name, must be same as is in service',
-                    dest='pipeline_name')
+
 args = vars(parser.parse_args())
 
 channel = grpc.insecure_channel("{}:{}".format(args['grpc_address'],args['grpc_port']))
@@ -59,17 +43,15 @@ stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
 
 processing_times = np.zeros((0),int)
 
-ark_reader = ArchiveReader("rm_lstm4f/test_feat_1_10.ark")
+model_input_path = args.get('model_input_path')
+print('Reading ark file {}'.format(model_input_path)
+ark_reader = ArchiveReader("model_input_path")
 
 for key, obj in ark_reader:
     print("Input ark file data range {0}: {1}".format(key, obj.shape))
 
 print('Start processing:')
 print('\tModel name: {}'.format(args.get('model_name')))
-print('\tImages numpy path: {}'.format(args.get('images_numpy_path')))
-
-
-is_pipeline_request = bool(args.get('pipeline_name'))
 
 SEQUENCE_START = 1
 SEQUENCE_END = 2
@@ -85,9 +67,7 @@ for key, obj in ark_reader:
 
         print('\tExecution: {}\n'.format(x))
         request = predict_pb2.PredictRequest()
-        #request.model_spec.name = args.get('pipeline_name') if is_pipeline_request else args.get('model_name')
         request.model_spec.name = args.get('model_name')
-        # Add 1 dimension
 
         print('\tTensor before input in shape: {}\n'.format(obj[x].shape))
         print('\tTensor input in shape: {}\n'.format(expand_dims(obj[x], axis=0).shape))
@@ -117,10 +97,6 @@ for key, obj in ark_reader:
             for Y in result.outputs:
                 print(Y)
             exit(1)
-        #sequence_id = make_ndarray(result.outputs['sequence_id'])
-        #print("Sequence id check: {}".format(sequence_id))
-        #if int(sequence_id) <= 0:
-        #    print("ERROR: Wrong sequence_id in model output {}".format(sequence_id))
 
         duration = (end_time - start_time).total_seconds() * 1000
         processing_times = np.append(processing_times,np.array([int(duration)]))
