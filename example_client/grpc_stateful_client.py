bcecc9a479b5b16ab2c8f1dd147ce579741f6029
rasapala <rafal.a.sapala@intel.com>
2020-11-19 13:07:34 +0000

    Accuracy - 30 avg err.

--- example_client/grpc_stateful_client.py
+++ example_client/grpc_stateful_client.py
@@ -25,9 +25,36 @@ from tensorflow_serving.apis import prediction_service_pb2_grpc
 from client_utils import print_statistics, prepare_certs
 from kaldi_python_io import ArchiveReader
 
+debug_mode = False
+def printDebug(msg):
+    if debug_mode:
+        print(msg)
+
+def CalculateUtteranceError(referenceArray, resultArray):
+        
+    printDebug("OUTPUT: {} \n".format(resultArray))
+    printDebug("OUTPUT SHAPE: {} \n".format(resultArray.shape))
+    printDebug("REF SHAPE: {} \n".format(referenceArray.shape))
+    printDebug("REF: {} \n".format(referenceArray))
+    errorSum = 0.0
+    # DEBUG PRINTS
+    #for i in range(resultArray.shape[0]):
+    #        single_result = resultArray[i]
+    #        print("SINGLE OUTPUT: {}\t".format(single_result))
+    #        single_ref = referenceArray[i]
+    #        print("SINGLE REFERENCE: {}\n".format(single_ref))
+    #        diffError = single_result - single_ref
+    #        errorSum += (diffError ** 2)
+
+    #meanErr = errorSum / float(resultArray.shape[0])
+    meanErr = (np.square(resultArray - referenceArray)).mean(axis=None)
+    return meanErr
+
+
 parser = argparse.ArgumentParser(description='Sends requests via TFS gRPC API using data in stateful model ark input file. '
                                              'It displays performance statistics and optionally')
 parser.add_argument('--model_input_path', required=False, default='rm_lstm4f/test_feat_1_10.ark', help='Path to input ark file')
+parser.add_argument('--model_score_path', required=False, default='rm_lstm4f/test_score_1_10.ark', help='Path to input ark file')
 parser.add_argument('--grpc_address',required=False, default='localhost',  help='Specify url to grpc service. default:localhost')
 parser.add_argument('--grpc_port',required=False, default=9000, help='Specify port to grpc service. default: 9000')
 parser.add_argument('--input_name',required=False, default='Parameter', help='Specify input tensor name. default: Parameter')
@@ -47,30 +74,46 @@ model_input_path = args.get('model_input_path')
 print('Reading ark file {}'.format(model_input_path))
 ark_reader = ArchiveReader(model_input_path)
 
+model_score_path = args.get('model_score_path')
+print('Reading scores ark file {}'.format(model_score_path))
+ark_score = ArchiveReader(model_score_path)
+numberOfKeys = 0
+
 for key, obj in ark_reader:
     print("Input ark file data range {0}: {1}".format(key, obj.shape))
+    numberOfKeys += 1
+
+for key, obj in ark_score:
+    print("Scores ark file data range {0}: {1}".format(key, obj.shape))
+
+
+scoreObjects = { k:m for k,m in ark_score }
+
 
 print('Start processing:')
 print('\tModel name: {}'.format(args.get('model_name')))
 
 SEQUENCE_START = 1
 SEQUENCE_END = 2
+sequence_id = 1005
 
-sequence_id = 1001
+meanErrGlobal = 0.0
 for key, obj in ark_reader:
     batch_size = obj.shape[0]
-    print('\tInput name: {}\n'.format(key))
-    print('\tInput in shape: {}\n'.format(obj.shape))
-    print('\tInput batch size: {}\n'.format(batch_size))
-    print('\tSequence id: {}\n'.format(sequence_id))
+    print('\n\tInput name: {}'.format(key))
+    printDebug('\tInput in shape: {}'.format(obj.shape))
+    printDebug('\tInput batch size: {}'.format(batch_size))
+    printDebug('\tSequence id: {}'.format(sequence_id))
+
+    meanErrSum = 0.0
     for x in range(0, batch_size):
 
-        print('\tExecution: {}\n'.format(x))
+        printDebug('\tExecution: {}\n'.format(x))
         request = predict_pb2.PredictRequest()
         request.model_spec.name = args.get('model_name')
 
-        print('\tTensor before input in shape: {}\n'.format(obj[x].shape))
-        print('\tTensor input in shape: {}\n'.format(expand_dims(obj[x], axis=0).shape))
+        printDebug('\tTensor before input in shape: {}\n'.format(obj[x].shape))
+        printDebug('\tTensor input in shape: {}\n'.format(expand_dims(obj[x], axis=0).shape))
 
         request.inputs[args['input_name']].CopyFrom(make_tensor_proto(obj[x], shape=(expand_dims(obj[x], axis=0).shape)))
         if x == 0:
@@ -102,11 +145,29 @@ for key, obj in ark_reader:
         processing_times = np.append(processing_times,np.array([int(duration)]))
         output = make_ndarray(result.outputs[args['output_name']])
 
-        nu = np.array(output)
-        # for object classification models show imagenet class
-        print('Iteration {}; Processing time: {:.2f} ms; speed {:.2f} fps'.format(x,round(np.average(duration), 2),
-                                                                                  round(1000 * batch_size / np.average(duration), 2)
-                                                                                  ))
-    sequence_id += 1
+        # Reset sequence end for testing purpose
+        #request.inputs['sequence_control_input'].CopyFrom(make_tensor_proto(SEQUENCE_END, dtype="uint32"))
+        #result = stub.Predict(request, 10.0) # result includes a dictionary with all model outputs
+        
 
+
+        resultsArray = np.array(output)
+        referenceArray = scoreObjects[key][x]
+        
+        meanErr = CalculateUtteranceError(referenceArray, resultsArray[0,:])
+
+        meanErrSum += meanErr
+        # Statistics
+        printDebug('Iteration {}; Mean error: {:.10f} Processing time: {:.2f} ms; speed {:.2f} fps\n'.format(x,meanErr,round(np.average(duration), 2), round(1000 * batch_size / np.average(duration), 2)))
+
+        #END utterance loop
+
+    meanErrAvg = meanErrSum/batch_size
+    print("\tSequence {} mean error: {:.10f}\n".format(sequence_id, meanErrAvg))
+    sequence_id += 1
+    meanErrGlobal += meanErrAvg
+    #END input name loop
+
+meanGlobalErrAvg = meanErrGlobal/numberOfKeys
+print("Global mean error: {:.10f}\n".format(meanGlobalErrAvg))
 print_statistics(processing_times, batch_size)
