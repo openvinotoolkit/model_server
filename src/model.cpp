b5f5aeeeebfc047aec1f7d75621e18b282de784c
Dariusz Trawinski <dariusz.trawinski@intel.com>
2020-12-02 17:50:12 +0100

    style fix

--- src/model.cpp
+++ src/model.cpp
@@ -28,7 +28,6 @@
 namespace ovms {
 
 StatusCode downloadModels(std::shared_ptr<FileSystem>& fs, ModelConfig& config, std::shared_ptr<model_versions_t> versions) {
-
     if (versions->size() == 0) {
         return StatusCode::OK;
     }
