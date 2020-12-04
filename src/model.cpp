4323bd1e39d38820c5cb265df954f6d1769dc153
Dariusz Trawinski <dariusz.trawinski@intel.com>
2020-12-04 09:25:07 +0100

    fix downloading model when basePath change in config

--- src/model.cpp
+++ src/model.cpp
@@ -206,7 +206,7 @@ Status Model::reloadVersions(std::shared_ptr<model_versions_t> versionsToReload,
             result = StatusCode::UNKNOWN_ERROR;
             continue;
         }
-        if (modelVersion->getStatus().getState() == ModelVersionState::END) {
+        if (modelVersion->getStatus().getState() == ModelVersionState::END || modelVersion->getModelConfig().getBasePath() != config.getBasePath()) {
             downloadModels(fs, config, versionsToReload);
         } else {
             config.setLocalPath(modelVersion->getModelConfig().getLocalPath());
