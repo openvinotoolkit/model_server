2aa54e211b97f51c98aa797b4ec3fb27cac64389
rasapala <rafal.a.sapala@intel.com>
2020-12-23 11:20:00 +0100

    Adding unit tests.

---
+++ src/test/stateful_config_test.cpp
@@ -0,0 +1,140 @@
+
+//*****************************************************************************
+// Copyright 2020 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "../modelconfig.hpp"
+#include "../modelinstance.hpp"
+#include "test_utils.hpp"
+
+using namespace ovms;
+
+using testing::_;
+using testing::Return;
+
+static const char* modelDefaultConfig = R"(
+{
+    "model_config_list": [
+        {
+            "config": {
+                "name": "dummy",
+                "base_path": "/ovms/src/test/dummy",
+                "target_device": "CPU",
+                "model_version_policy": {"latest": {"num_versions":1}},
+                "nireq": 100,
+                "shape": {"b": "(1,10) "}
+            }
+        }
+    ],
+    "pipeline_config_list": [
+        {
+            "name": "pipeline1Dummy",
+            "inputs": ["custom_dummy_input"],
+            "nodes": [
+                {
+                    "name": "dummyNode",
+                    "model_name": "dummy",
+                    "type": "DL model",
+                    "inputs": [
+                        {"b": {"node_name": "request",
+                               "data_item": "custom_dummy_input"}}
+                    ],
+                    "outputs": [
+                        {"data_item": "a",
+                         "alias": "new_dummy_output"}
+                    ]
+                }
+            ],
+            "outputs": [
+                {"custom_dummy_output": {"node_name": "dummyNode",
+                                         "data_item": "new_dummy_output"}
+                }
+            ]
+        }
+    ]
+})";
+
+static const char* modelStatefulChangedConfig = R"(
+{
+    "model_config_list": [
+        {
+            "config": {
+                "name": "dummy",
+                "base_path": "/ovms/src/test/dummy",
+                "target_device": "CPU",
+                "model_version_policy": {"latest": {"num_versions":1}},
+                "nireq": 100,
+                "shape": {"b": "(1,10) "}
+            }
+        }
+    ],
+    "pipeline_config_list": [
+        {
+            "name": "pipeline1Dummy",
+            "inputs": ["custom_dummy_input"],
+            "nodes": [
+                {
+                    "name": "dummyNode",
+                    "model_name": "dummy",
+                    "type": "DL model",
+                    "inputs": [
+                        {"b": {"node_name": "request",
+                               "data_item": "custom_dummy_input"}}
+                    ],
+                    "outputs": [
+                        {"data_item": "a",
+                         "alias": "new_dummy_output"}
+                    ]
+                }
+            ],
+            "outputs": [
+                {"custom_dummy_output": {"node_name": "dummyNode",
+                                         "data_item": "new_dummy_output"}
+                }
+            ]
+        }
+    ]
+})";
+
+class StatefulConfigTest : public TestWithTempDir {
+protected:
+    void SetUp() override {
+        TestWithTempDir::SetUp();
+        // Prepare manager
+        config = modelDefaultConfig;
+    }
+
+    ModelConfig config;
+};
+
+TEST_P(StatefulConfigTest, DefaultValues) {
+    ConstructorEnabledModelManager managerWithDummyModel;
+    managerWithDummyModel.reloadModelWithVersions(config);
+
+    auto modelInstance = managerWithDummyModel.findModelInstance(dummyModelName);
+
+    auto modelConfig = modelInstance->getModelConfig();
+
+    ASSERT_EQ(modelConfig.isLowLatencyTransformationUsed(), false);
+    ASSERT_EQ(modelConfig.isStateful(), false);
+    ASSERT_EQ(modelConfig.getMaxSequenceNumber(), 500);
+    ASSERT_EQ(modelConfig.getSequenceTimeout(), 60);
+}
+
+
+
+
