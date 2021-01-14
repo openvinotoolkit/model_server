cf38b9ef637d63fb65b6448c80fa2aca20328f26
rasapala <rafal.a.sapala@intel.com>
2021-01-14 10:28:01 +0100

    1

--- src/test/stateful_modelinstance_test.cpp
+++ src/test/stateful_modelinstance_test.cpp
@@ -101,7 +101,7 @@ TEST_F(StatefulModelInstance, positiveValidate) {
 
     auto modelInstance = manager.findModelInstance(dummyModelName);
 
-    std::vector<uint32_t> seqId{ 0 };
+    std::vector<uint32_t> seqId{ 1 };
     std::vector<uint32_t> seqControl{ SEQUENCE_START };
     std::vector<uint32_t> seqData{ 90,91,92,93,94,96,97,98,99,100 };
     std::map<std::string, std::vector<std::uint32_t>> requestData = { {SEQUENCE_ID_INPUT, seqId}, {SEQUENCE_CONTROL_INPUT, seqControl}, {DUMMY_MODEL_INPUT_NAME, seqData} };
