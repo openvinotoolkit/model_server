3728c56c922546251aaad2c3fbddbde98e628fa3
Dariusz Trawinski <dariusz.trawinski@intel.com>
2021-01-22 15:14:43 +0100

    update sample and build command

---
+++ extras/ovms-operator/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# operator-sdk init --plugins=helm --domain=com --group=intel --version=v1alpha1 --kind=Ovms --helm-chart ../../deploy/ovms/ --helm-chart-version 3.0
+export IMG=quay.io/openvino/ovms-operator:v1.0
+make docker-build docker-push IMG=$IMG
+make install
+make deploy IMG=$IMG
+# kubectl apply -f config/samples/intel_v1alpha1_ovms.yaml
+
+
