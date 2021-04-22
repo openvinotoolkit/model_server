003a02ae9756c96d1f6bc4bbc67a4a0f9e7917ff
mzeglars <milosz.zeglarski@intel.com>
2021-04-22 11:49:36 +0200

    download model with HCLG compiled

--- example_client/stateful/asr_demo/prepare_model.sh
+++ example_client/stateful/asr_demo/prepare_model.sh
@@ -22,10 +22,5 @@ export DATA_PATH=/opt/data
 
 # Model download and unpack 
 cd $ASPIRE_PATH
-wget https://kaldi-asr.org/models/1/0001_aspire_chain_model.tar.gz
-tar -xvf 0001_aspire_chain_model.tar.gz
-
-# Make language graph for decoding
-cd $ASPIRE_PATH
-steps/online/nnet3/prepare_online_decoding.sh --mfcc-config conf/mfcc_hires.conf data/lang_chain exp/nnet3/extractor exp/chain/tdnn_7b exp/tdnn_7b_chain_online
-utils/mkgraph.sh --self-loop-scale 1.0 data/lang_pp_test exp/tdnn_7b_chain_online exp/tdnn_7b_chain_online/graph_pp
+wget https://kaldi-asr.org/models/1/0001_aspire_chain_model_with_hclg.tar.bz2
+tar -xvf 0001_aspire_chain_model_with_hclg.tar.bz2
