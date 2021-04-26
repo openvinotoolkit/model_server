fc0e3d4cc50549241061fc6371e92c13d9084b22
Dariusz Trawinski <dariusz.trawinski@intel.com>
2021-04-26 11:27:59 +0200

    jpeg client fix

--- example_client/jpeg_classification.py
+++ example_client/jpeg_classification.py
@@ -88,9 +88,6 @@ for line in lines:
     path, label = line.strip().split(" ")
     img = getJpeg(path, size, rgb_image)
 
-    # lbs = np.append(lbs,[int(label)],0)
-    # imgs = np.append(imgs, img,0)
-
     request = predict_pb2.PredictRequest()
     request.model_spec.name = args.get('model_name')
     request.inputs[args['input_name']].CopyFrom(make_tensor_proto(img, shape=(img.shape)))
@@ -115,7 +112,7 @@ for line in lines:
         matched += 1
         mark_message = "; Correct match."
     else:
-        mark_message = "; Incorrect match. Should be {} {}".format(lb[i], classes.imagenet_classes[lb[i]] )
+        mark_message = "; Incorrect match. Should be {} {}".format(label, classes.imagenet_classes[int(label)])
     i += 1
     print("\t",i, classes.imagenet_classes[ma],ma, mark_message)
 
@@ -125,5 +122,3 @@ accuracy = matched/i
 print("Overall accuracy=",accuracy*100,"%")
 print("Average latency=",latency,"ms")
 
-# np.save('imgs.npy', imgs)
-# np.save('lbs.npy', lbs)
