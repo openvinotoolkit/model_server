5a1ff7b1c92d85625c853a24ed7f90d29b8ff4fe
Jakub Ficek <jakub.ficek@intel.com>
2021-04-27 09:57:14 +0000

    batchsize added

--- example_client/grpc_binary_input_client.py
+++ example_client/grpc_binary_input_client.py
@@ -22,38 +22,6 @@ import datetime
 import argparse
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2_grpc
-import cv2
-
-def crop_resize(img,cropx,cropy):
-    y,x,c = img.shape
-    if y < cropy:
-        img = cv2.resize(img, (x, cropy))
-        y = cropy
-    if x < cropx:
-        img = cv2.resize(img, (cropx,y))
-        x = cropx
-    startx = x//2-(cropx//2)
-    starty = y//2-(cropy//2)
-    return img[starty:starty+cropy,startx:startx+cropx,:]
-
-def getBinaryJpeg(path, size, rgb_image=0):
-    with open(path, mode='rb') as file:
-        content = file.read()
-
-    img = np.frombuffer(content, dtype=np.uint8)
-    img = cv2.imdecode(img, cv2.IMREAD_COLOR)  # BGR format
-    # retrived array has BGR format and 0-255 normalization
-    # format of data is HWC
-    # add image preprocessing if needed by the model
-    img = crop_resize(img, size, size)
-    img = img.astype('float32')
-    #convert to RGB instead of BGR if required by model
-    if rgb_image:
-        img = img[:, :, [2, 1, 0]]
-    # switch from HWC to CHW and reshape to 1,3,size,size for model blob input requirements
-    img = img.transpose(2,0,1).reshape(1,3,size,size)
-    print(path, img.shape, "; data range:",np.amin(img),":",np.amax(img))
-    return img.tobytes()
 
 parser = argparse.ArgumentParser(description='Do requests to ie_serving and tf_serving using images in binary format')
 parser.add_argument('--images_list', required=False, default='input_images.txt', help='path to a file with a list of labeled images')
@@ -63,8 +31,8 @@ parser.add_argument('--input_name',required=False, default='input', help='Specif
 parser.add_argument('--output_name',required=False, default='resnet_v1_50/predictions/Reshape_1', help='Specify output name. default: resnet_v1_50/predictions/Reshape_1')
 parser.add_argument('--model_name', default='resnet', help='Define model name, must be same as is in service. default: resnet',
                     dest='model_name')
-parser.add_argument('--size',required=False, default=224, type=int, help='The size of the image in the model')
-parser.add_argument('--rgb_image',required=False, default=0, type=int, help='Convert BGR channels to RGB channels in the input image')
+parser.add_argument('--batchsize', default=1, help='Number of images in a single request. default: 1',
+                    dest='batchsize')
 args = vars(parser.parse_args())
 
 channel = grpc.insecure_channel("{}:{}".format(args['grpc_address'],args['grpc_port']))
@@ -73,6 +41,7 @@ input_images = args.get('images_list')
 size = args.get('size')
 with open(input_images) as f:
     lines = f.readlines()
+batch_size = min(int(args.get('batchsize')), len(lines))
 print('Start processing:')
 print('\tModel name: {}'.format(args.get('model_name')))
 print('\tImages list file: {}'.format(args.get('images_list')))
@@ -80,16 +49,20 @@ print('\tImages list file: {}'.format(args.get('images_list')))
 i = 0
 matched = 0
 processing_times = np.zeros((0),int)
-imgs = np.zeros((0,3,size, size), np.dtype('<f'))
-lbs = np.zeros((0), int)
 
-rgb_image = args.get('rgb_image')
+batch_i = 0
+image_data = []
 for line in lines:
+    batch_i += 1
     path, label = line.strip().split(" ")
-    img = getBinaryJpeg(path, size, rgb_image)
+    with open(path, 'rb') as f:
+        image_data.append(f.read())
+    if batch_i < batch_size:
+        continue
     request = predict_pb2.PredictRequest()
     request.model_spec.name = args.get('model_name')
-    request.inputs[args['input_name']].CopyFrom(tf.make_tensor_proto(img, shape=[1]))
+    request.inputs[args['input_name']].CopyFrom(tf.make_tensor_proto(img, shape=[len(image_data)]))
+    image_data = []
     start_time = datetime.datetime.now()
     result = stub.Predict(request, 10.0) # result includes a dictionary with all model outputs
     end_time = datetime.datetime.now()
