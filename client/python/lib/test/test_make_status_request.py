1e5fda4d74822342f87b5883e9b8e5e0ca4a9681
mkuczyns11 <martin.kuczynski@intel.com>
2021-07-12 15:09:21 +0200

    assertions added in make_status_request_valid test

--- client/python/lib/test/test_make_status_request.py
+++ client/python/lib/test/test_make_status_request.py
@@ -1,11 +1,16 @@
 import pytest
 
-from ovmsclient.tfs_compat.grpc.requests import make_status_request
+from ovmsclient.tfs_compat.grpc.requests import GrpcModelStatusRequest, make_status_request
 from config import MODEL_REQUEST_INVALID, MODEL_REQUEST_VALID
+from tensorflow_serving.apis.get_model_status_pb2 import GetModelStatusRequest
 
 @pytest.mark.parametrize("name, version", MODEL_REQUEST_VALID)
 def test_make_status_request_valid(name, version):
-    model_status_request = make_status_request(name, version)
+    request = model_status_request = make_status_request(name, version)
+    assert isinstance(request, GrpcModelStatusRequest)
+    assert request.model_version == version
+    assert request.model_name == name
+    assert isinstance(request.raw_request, GetModelStatusRequest)
 
 @pytest.mark.parametrize("name, version, exception, message", MODEL_REQUEST_INVALID)
 def test_make_status_request_invalid(name, version, exception, message):
