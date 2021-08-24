c17a18794187712d6034c53370e991e49f62f764
Rafal Jasinski <rafal.jasinski@intel.com>
2021-08-24 09:21:56 +0200

    Fix regressions.

--- tests/functional/utils/rest.py
+++ tests/functional/utils/rest.py
@@ -108,8 +108,11 @@ def process_json_output(result_dict, output_tensors):
 
     return output
 
-def _get_output_json(img, input_tensor, rest_url, request_format, method_to_call, prepare_body_format=False):
-    data_json = prepare_body_format(img, request_format, input_tensor) if prepare_body_format else None
+def _get_output_json(img, input_tensor, rest_url, request_format, method_to_call):
+    if img and input_tensor and request_format:
+        data_json = prepare_body_format(img, request_format, input_tensor)
+    else:
+        data_json = None
     result = method_to_call(rest_url, data=data_json)
     if not result.ok or result.status_code != 200:
         msg = f"REST {method_to_call} failed {result}"
@@ -120,20 +123,20 @@ def _get_output_json(img, input_tensor, rest_url, request_format, method_to_call
 
 def infer_rest(img, input_tensor, rest_url,
                output_tensors, request_format):
-    output_json = _get_output_json(img, input_tensor, rest_url, requests.post, request_format, True)
+    output_json = _get_output_json(img, input_tensor, rest_url, requests.post, request_format)
     data = process_json_output(output_json, output_tensors)
     return data
 
 
 def get_model_metadata_response_rest(rest_url):
-    output_json = _get_output_json(None, None, rest_url, requests.get, None, True)
+    output_json = _get_output_json(None, None, rest_url, requests.get, None)
     metadata_pb = get_model_metadata_pb2.GetModelMetadataResponse()
     response = Parse(output_json, metadata_pb, ignore_unknown_fields=False)
     return response
 
 
 def get_model_status_response_rest(rest_url):
-    output_json = _get_output_json(None, None, rest_url, requests.get, None, True)
+    output_json = _get_output_json(None, None, rest_url, requests.get, None)
     status_pb = get_model_status_pb2.GetModelStatusResponse()
     response = Parse(output_json, status_pb, ignore_unknown_fields=False)
     return response
