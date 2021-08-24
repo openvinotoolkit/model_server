7f20eed9b5ebc026d525fd189db3244e4abcde9d
mkuczyns11 <martin.kuczynski@intel.com>
2021-08-24 14:43:42 +0200

    last tests added

--- client/python/lib/tests/performance.py
+++ client/python/lib/tests/performance.py
@@ -1,5 +1,5 @@
 import ovmsclient
-# import tensorflow
+import tensorflow
 import datetime
 import numpy as np
 from ovmsclient.tfs_compat.grpc.requests import make_predict_request
@@ -13,7 +13,7 @@ np.random.seed(0)
 
 # parameters
 # iterations determine how many times tested method should be called
-iterations = 100_000
+iterations = 1_000_000
 # lower_iterations determine how many times tested method should be called for
 # time-consuming tests
 lower_iterations = 10_000
@@ -23,14 +23,21 @@ lower_iterations = 10_000
 make_tensor_proto_testing = False
 make_ndarray_testing = False
 # for predict_request creation testing there are 2 different cases
-# for make_predict_request testing import ovmsclient and do not import tensorflow and enable the test below
+# for make_predict_request testing import ovmsclient and do not
+# import tensorflow and enable the test below
 make_predict_request_testing = False
 # for predict request creation testing via TF import tensorflow instead of ovmsclient
 predict_request_TF_testing = False
-# for predict testing on ovmsclient import ovmsclient and do not import tensorflow and enable the test below
+# for predict testing on ovmsclient import ovmsclient and do not
+# import tensorflow and enable the test below
 predict_ovmsclient = False
 # for predict testing via TF import tensorflow instead of ovmsclient
-predict_TF = True
+predict_TF = False
+# for ovmsclient flow testing on ovmsclient import ovmsclient and do not
+# import tensorflow and enable the test below
+ovmsclient_testing = False
+# for tf flow testing import tensorflow instead of ovmsclient
+tf_testing = True
 # testing protos apply to make_tensor_proto, make_ndarray and make_predict_request tests
 proto_inputs_dict = {
     "float_shape_dtype_no_reshape": {
@@ -62,7 +69,7 @@ proto_inputs_dict = {
     },
     "2Dlist_shape_dtype_no_reshape": {
         "values": [[1, 2, 3], [1, 2, 3]],
-        "shape": [2,3],
+        "shape": [2, 3],
         "dtype": DataType.DT_INT64
     },
     "2Dlist_no_shape_dtype_no_reshape": {
@@ -98,7 +105,7 @@ proto_inputs_dict = {
     },
     "2Dndarray_shape_dtype_no_reshape": {
         "values": np.array([[1, 2, 3], [1, 2, 3]]),
-        "shape": [2,3],
+        "shape": [2, 3],
         "dtype": DataType.DT_FLOAT
     },
     "2Dndarray_no_shape_dtype_no_reshape": {
@@ -128,7 +135,7 @@ proto_inputs_dict = {
     },
     "2Dlist_shape_no_dtype_no_reshape": {
         "values": [[1, 2, 3], [1, 2, 3]],
-        "shape": [2,3],
+        "shape": [2, 3],
     },
     "2Dlist_no_shape_no_dtype_no_reshape": {
         "values": [[1, 2, 3], [1, 2, 3]],
@@ -153,7 +160,7 @@ proto_inputs_dict = {
     },
     "2Dndarray_shape_no_dtype_no_reshape": {
         "values": np.array([[1, 2, 3], [1, 2, 3]]),
-        "shape": [2,3],
+        "shape": [2, 3],
     },
     "2Dndarray_no_shape_no_dtype_no_reshape": {
         "values": np.array([[1, 2, 3], [1, 2, 3]]),
@@ -225,56 +232,56 @@ proto_inputs_dict = {
     # "2Dndarray_no_shape_no_dtype_reshape": {    #impossible
     # },
     "ndarray_zeros_resnet_npdtype_dtype_no_reshape": {
-        "values": np.zeros((1,3,244,244), dtype=np.float32),
+        "values": np.zeros((1, 3, 244, 244), dtype=np.float32),
         "dtype": DataType.DT_FLOAT
     },
     "ndarray_zeros_resnet_no_npdtype_dtype_no_reshape": {
-        "values": np.zeros((1,3,244,244)),
+        "values": np.zeros((1, 3, 244, 244)),
         "dtype": DataType.DT_INT16
     },
     "ndarray_zeros_resnet_npdtype_no_dtype_no_reshape": {
-        "values": np.zeros((1,3,244,244), dtype=np.float32),
+        "values": np.zeros((1, 3, 244, 244), dtype=np.float32),
     },
     "ndarray_zeros_resnet_no_npdtype_no_dtype_no_reshape": {
-        "values": np.zeros((1,3,244,244)),
+        "values": np.zeros((1, 3, 244, 244)),
     },
     "ndarray_zeros_resnet_npdtype_dtype_reshape": {
-        "values": np.zeros((1,3,244,244), dtype=np.float32),
+        "values": np.zeros((1, 3, 244, 244), dtype=np.float32),
         "dtype": DataType.DT_FLOAT,
-        "shape": [1,2,24,3721],
+        "shape": [1, 2, 24, 3721],
     },
     "ndarray_zeros_resnet_no_npdtype_dtype_reshape": {
-        "values": np.zeros((1,3,244,244)),
+        "values": np.zeros((1, 3, 244, 244)),
         "dtype": DataType.DT_INT16,
-        "shape": [1,2,24,3721],
+        "shape": [1, 2, 24, 3721],
     },
     "ndarray_zeros_resnet_npdtype_no_dtype_reshape": {
-        "values": np.zeros((1,3,244,244), dtype=np.float32),
-        "shape": [1,2,24,3721],
+        "values": np.zeros((1, 3, 244, 244), dtype=np.float32),
+        "shape": [1, 2, 24, 3721],
     },
     "ndarray_zeros_resnet_no_npdtype_no_dtype_reshape": {
-        "values": np.zeros((1,3,244,244)),
-        "shape": [1,2,24,3721],
+        "values": np.zeros((1, 3, 244, 244)),
+        "shape": [1, 2, 24, 3721],
     },
     "ndarray_rand_resnet_npdtype_dtype": {
-        "values": np.random.rand(1,3,244,244).astype(np.float32),
+        "values": np.random.rand(1, 3, 244, 244).astype(np.float32),
         "dtype": DataType.DT_FLOAT,
     },
     "ndarray_rand_resnet_no_npdtype_dtype": {
-        "values": np.random.rand(1,3,244,244),
+        "values": np.random.rand(1, 3, 244, 244),
         "dtype": DataType.DT_FLOAT,
     },
     "ndarray_rand_resnet_npdtype_no_dtype": {
-        "values": np.random.rand(1,3,244,244).astype(np.float32),
+        "values": np.random.rand(1, 3, 244, 244).astype(np.float32),
     },
     "ndarray_rand_resnet_no_npdtype_no_dtype": {
-        "values": np.random.rand(1,3,244,244),
+        "values": np.random.rand(1, 3, 244, 244),
     },
     "Big_2Dlist_shape_dtype_no_reshape": {
         "values": [[[i for i in range(5000)], [i for i in range(5000)]],
                    [[i for i in range(5000)], [i for i in range(5000)]],
                    [[i for i in range(5000)], [i for i in range(5000)]]],
-        "shape": [3,2,5000],
+        "shape": [3, 2, 5000],
         "dtype": DataType.DT_INT64
     },
     "Big_2Dlist_no_shape_dtype_no_reshape": {
@@ -287,7 +294,7 @@ proto_inputs_dict = {
         "values": [[[i for i in range(5000)], [i for i in range(5000)]],
                    [[i for i in range(5000)], [i for i in range(5000)]],
                    [[i for i in range(5000)], [i for i in range(5000)]]],
-        "shape": [3,2,5000],
+        "shape": [3, 2, 5000],
     },
     "Big_2Dlist_no_shape_no_dtype_no_reshape": {
         "values": [[[i for i in range(5000)], [i for i in range(5000)]],
@@ -298,7 +305,7 @@ proto_inputs_dict = {
         "values": [[[i for i in range(5000)], [i for i in range(5000)]],
                    [[i for i in range(5000)], [i for i in range(5000)]],
                    [[i for i in range(5000)], [i for i in range(5000)]]],
-        "shape": [2,6,2500],
+        "shape": [2, 6, 2500],
         "dtype": DataType.DT_INT64
     },
     # "Big_2Dlist_no_shape_dtype_reshape": {    #impossible
@@ -307,33 +314,33 @@ proto_inputs_dict = {
         "values": [[[i for i in range(5000)], [i for i in range(5000)]],
                    [[i for i in range(5000)], [i for i in range(5000)]],
                    [[i for i in range(5000)], [i for i in range(5000)]]],
-        "shape": [2,6,2500],
+        "shape": [2, 6, 2500],
     },
     # "Big_2Dlist_no_shape_no_dtype_reshape": {   #impossible
     # },
     "Big_binary_shape_dtype_no_reshape": {
-        "values": [bytes([i%256 for i in range(10000)]),
-                   bytes([(3*i)%256 for i in range(10000)]),
-                   bytes([(7*i)%256 for i in range(10000)])],
+        "values": [bytes([i % 256 for i in range(10000)]),
+                   bytes([(3*i) % 256 for i in range(10000)]),
+                   bytes([(7*i) % 256 for i in range(10000)])],
         "shape": [3],
         "dtype": DataType.DT_STRING
     },
     "Big_binary_no_shape_dtype_no_reshape": {
-        "values": [bytes([i%256 for i in range(10000)]),
-                   bytes([(3*i)%256 for i in range(10000)]),
-                   bytes([(7*i)%256 for i in range(10000)])],
+        "values": [bytes([i % 256 for i in range(10000)]),
+                   bytes([(3*i) % 256 for i in range(10000)]),
+                   bytes([(7*i) % 256 for i in range(10000)])],
         "dtype": DataType.DT_STRING
     },
     "Big_binary_shape_no_dtype_no_reshape": {
-        "values": [bytes([i%256 for i in range(10000)]),
-                   bytes([(3*i)%256 for i in range(10000)]),
-                   bytes([(7*i)%256 for i in range(10000)])],
+        "values": [bytes([i % 256 for i in range(10000)]),
+                   bytes([(3*i) % 256 for i in range(10000)]),
+                   bytes([(7*i) % 256 for i in range(10000)])],
         "shape": [3],
     },
     "Big_binary_no_shape_no_dtype_no_reshape": {
-        "values": [bytes([i%256 for i in range(10000)]),
-                   bytes([(3*i)%256 for i in range(10000)]),
-                   bytes([(7*i)%256 for i in range(10000)])],
+        "values": [bytes([i % 256 for i in range(10000)]),
+                   bytes([(3*i) % 256 for i in range(10000)]),
+                   bytes([(7*i) % 256 for i in range(10000)])],
     },
 }
 
@@ -398,7 +405,7 @@ if make_predict_request_testing:
 
     for key, value in proto_inputs_dict.items():
         final_time = 0
-        iteration = iterations if key not in lower_iteration_cases else lower_iterations
+        iteration = iterations
         inputs = {
             "proto": ovmsclient.make_tensor_proto(**value)
         }
@@ -424,7 +431,7 @@ if predict_request_TF_testing:
 
     for key, value in proto_inputs_dict.items():
         final_time = 0
-        iteration = iterations if key not in lower_iteration_cases else lower_iterations
+        iteration = iterations
         proto = ovmsclient.make_tensor_proto(**value)
         for i in range(iteration):
             start_time = datetime.datetime.now()
@@ -448,7 +455,7 @@ if predict_request_TF_testing:
 
 
 # predict testing configuration
-predict_proto = make_tensor_proto(np.zeros((1,3,224,224), dtype=np.float32))
+predict_proto = make_tensor_proto(np.zeros((1, 3, 224, 224), dtype=np.float32))
 predict_iterations = 10_000
 key = 'resnet'
 inputs = {
@@ -476,12 +483,12 @@ if predict_ovmsclient:
     print(f'{key} finished with time {final_time}s')
     print(f'Time spend per predict: {time_per_predict}ms')
 
-    # print(predict_ovmsclient_performance)
-    # proto_df = pd.DataFrame(data=predict_ovmsclient_performance, index=[0])
-    # proto_df.astype('float32')
-    # proto_df = proto_df.T
-    # proto_df.columns = ['time_per_predict[ms]']
-    # proto_df.to_excel('performance.xlsx', sheet_name='predict_ovmsclient_perf')
+    print(predict_ovmsclient_performance)
+    proto_df = pd.DataFrame(data=predict_ovmsclient_performance, index=[0])
+    proto_df.astype('float32')
+    proto_df = proto_df.T
+    proto_df.columns = ['time_per_predict[ms]']
+    proto_df.to_excel('performance.xlsx', sheet_name='predict_ovmsclient_perf')
 
 if predict_TF:
     predict_TF_performance = {}
@@ -497,11 +504,72 @@ if predict_TF:
     time_per_predict = final_time/predict_iterations * 1000
     predict_TF_performance[key] = time_per_predict
     print(f'{key} finished with time {final_time}s')
-    print(f'Time spend per predict: {time_per_predict}ms')
+    print(f'Time spend per iteration: {time_per_predict}ms')
 
-    # print(predict_TF_performance)
-    # proto_df = pd.DataFrame(data=predict_TF_performance, index=[0])
-    # proto_df.astype('float32')
-    # proto_df = proto_df.T
-    # proto_df.columns = ['time_per_predict[ms]']
-    # proto_df.to_excel('performance.xlsx', sheet_name='predict_TF_perf')
+    print(predict_TF_performance)
+    proto_df = pd.DataFrame(data=predict_TF_performance, index=[0])
+    proto_df.astype('float32')
+    proto_df = proto_df.T
+    proto_df.columns = ['time_per_predict[ms]']
+    proto_df.to_excel('performance.xlsx', sheet_name='predict_TF_perf')
+
+# whole flow testing
+flow_iterations = 10_000
+
+if ovmsclient_testing:
+    ovmsclient_performance = {}
+
+    final_time = 0
+    for i in range(flow_iterations):
+        start_time = datetime.datetime.now()
+        proto = ovmsclient.make_tensor_proto(np.zeros((1, 3, 224, 224), dtype=np.float32))
+        inputs = {
+            "0": proto
+        }
+        request = ovmsclient.make_grpc_predict_request(inputs, 'resnet')
+        response = client.predict(request)
+        response_dict = response.to_dict()
+        end_time = datetime.datetime.now()
+        final_time += (end_time - start_time).total_seconds()
+    time_per_iteration = final_time/flow_iterations * 1000
+    ovmsclient_performance[key] = time_per_iteration
+    print(f'{key} finished with time {final_time}s')
+    print(f'Time spend per predict: {time_per_iteration}ms')
+
+    print(ovmsclient_performance)
+    proto_df = pd.DataFrame(data=ovmsclient_performance, index=[0])
+    proto_df.astype('float32')
+    proto_df = proto_df.T
+    proto_df.columns = ['time_per_iteration[ms]']
+    proto_df.to_excel('performance.xlsx', sheet_name='ovmsclient_perf')
+
+if tf_testing:
+    tf_performance = {}
+
+    stub = client.prediction_service_stub
+    final_time = 0
+    for i in range(flow_iterations):
+        start_time = datetime.datetime.now()
+        proto = tensorflow.make_tensor_proto(np.zeros((1, 3, 224, 224), dtype=np.float32))
+
+        request = predict_pb2.PredictRequest()
+        request.model_spec.name = 'resnet'
+        request.model_spec.version.value = 0
+        request.inputs['0'].CopyFrom(proto)
+
+        response = stub.Predict(request, 10.0)
+
+        response_ndarray = tensorflow.make_ndarray(response.outputs['1463'])
+        end_time = datetime.datetime.now()
+        final_time += (end_time - start_time).total_seconds()
+    time_per_iteration = final_time/flow_iterations * 1000
+    tf_performance[key] = time_per_iteration
+    print(f'{key} finished with time {final_time}s')
+    print(f'Time spend per iteration: {time_per_iteration}ms')
+
+    print(tf_performance)
+    proto_df = pd.DataFrame(data=tf_performance, index=[0])
+    proto_df.astype('float32')
+    proto_df = proto_df.T
+    proto_df.columns = ['time_per_iteration[ms]']
+    proto_df.to_excel('performance.xlsx', sheet_name='tf_perf')
