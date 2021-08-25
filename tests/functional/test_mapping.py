2d5560d9f06fe4c063f41c6c91b82753af6a86f5
Rafal Jasinski <rafal.jasinski@intel.com>
2021-08-25 14:07:45 +0200

    * Skip all tests for NGINX * Add new configs * Add get_bool helper

--- tests/functional/test_mapping.py
+++ tests/functional/test_mapping.py
@@ -17,7 +17,7 @@
 import numpy as np
 import pytest
 from constants import ERROR_SHAPE
-from config import target_device
+from config import target_device, skip_nginx_test
 from model.models_information import AgeGender
 from utils.grpc import create_channel, infer, get_model_metadata, model_metadata_response
 import logging
@@ -26,6 +26,7 @@ from utils.rest import get_predict_url, get_metadata_url, infer_rest, get_model_
 logger = logging.getLogger(__name__)
 
 
+@pytest.mark.skipif(skip_nginx_test, reason="not implemented yet")
 @pytest.mark.skipif(target_device == "MYRIAD",
                     reason="error: Cannot load network into target device")
 class TestSingleModelMappingInference:
