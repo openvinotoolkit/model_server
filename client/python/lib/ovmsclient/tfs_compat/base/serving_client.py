29eaf0627b353ad8475600dbb25269459fe708cd
Miłosz Żeglarski <milosz.zeglarski@intel.com>
2021-09-07 13:54:28 +0200

    Client library - make_http_predict_request (#910)

--- client/python/lib/ovmsclient/tfs_compat/base/serving_client.py
+++ client/python/lib/ovmsclient/tfs_compat/base/serving_client.py
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+import os
+from validators import ipv4, domain
 from abc import ABC, abstractmethod
 
 
@@ -77,3 +79,81 @@ class ServingClient(ABC):
     @abstractmethod
     def _build(cls, config):
         raise NotImplementedError
+
+    @classmethod
+    def _prepare_certs(cls, server_cert_path, client_cert_path, client_key_path):
+
+        client_cert, client_key = None, None
+
+        server_cert = cls._open_certificate(server_cert_path)
+
+        if client_cert_path is not None:
+            client_cert = cls._open_certificate(client_cert_path)
+
+        if client_key_path is not None:
+            client_key = cls._open_private_key(client_key_path)
+
+        return server_cert, client_cert, client_key
+
+    @classmethod
+    def _open_certificate(cls, certificate_path):
+        with open(certificate_path, 'rb') as f:
+            certificate = f.read()
+            return certificate
+
+    @classmethod
+    def _open_private_key(cls, key_path):
+        with open(key_path, 'rb') as f:
+            key = f.read()
+            return key
+
+    @classmethod
+    def _check_config(cls, config):
+
+        if 'address' not in config or 'port' not in config:
+            raise ValueError('The minimal config must contain address and port')
+
+        cls._check_address(config['address'])
+
+        cls._check_port(config['port'])
+
+        if 'tls_config' in config:
+            cls._check_tls_config(config['tls_config'])
+
+    @classmethod
+    def _check_address(cls, address):
+
+        if not isinstance(address, str):
+            raise TypeError(f'address type should be string, but is {type(address).__name__}')
+
+        if address != "localhost" and not ipv4(address) and not domain(address):
+            raise ValueError('address is not valid')
+
+    @classmethod
+    def _check_port(cls, port):
+
+        if not isinstance(port, int):
+            raise TypeError(f'port type should be int, but is type {type(port).__name__}')
+
+        if port.bit_length() > 16 or port < 0:
+            raise ValueError(f'port should be in range <0, {2**16-1}>')
+
+    @classmethod
+    def _check_tls_config(cls, tls_config):
+
+        if 'server_cert_path' not in tls_config:
+            raise ValueError('server_cert_path is not defined in tls_config')
+
+        if ('client_key_path' in tls_config) != ('client_cert_path' in tls_config):
+            raise ValueError('none or both client_key_path and client_cert_path '
+                             'are required in tls_config')
+
+        valid_keys = ['server_cert_path', 'client_key_path', 'client_cert_path']
+        for key in tls_config:
+            if key not in valid_keys:
+                raise ValueError(f'{key} is not valid tls_config key')
+            if not isinstance(tls_config[key], str):
+                raise TypeError(f'{key} type should be string but is type '
+                                f'{type(tls_config[key]).__name__}')
+            if not os.path.isfile(tls_config[key]):
+                raise ValueError(f'{tls_config[key]} is not valid path to file')
---
+++ client/python/lib/tests/tfs_compat_http/config.py
@@ -0,0 +1,139 @@
+#
+# Copyright (c) 2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import json
+from numpy import array, int32, float32, float128
+
+from config import * # noqa
+
+# (inputs_dict,
+# model_name, model_version, expected_exception, expected_message)
+PREDICT_REQUEST_INVALID_INPUTS = [
+    ([],
+     'model_name', 0, TypeError, "inputs type should be dict, but is list"),
+    (('input1', [1, 2, 3]),
+     'model_name', 0, TypeError, "inputs type should be dict, but is tuple"),
+    ({
+        1: [1, 2, 3],
+        "input2": [1, 2]
+    }, 'model_name', 0, TypeError, "inputs keys should be type str, but found int"),
+    ({
+        "input1": [[1.0, 2.0], [1.0, 2.0, 3.0]]
+    }, 'model_name', 0, ValueError,
+     ("argument must be a dense tensor: [[1.0, 2.0], [1.0, 2.0, 3.0]] - "
+      "got shape [2], but wanted [2, 2]")),
+    ({
+        "input1": [[(1, 2, 3)], [(1, 2)], [(1, 2, 3)]]
+    }, 'model_name', 0, TypeError, "provided values type is not valid"),
+    ({
+        "input1": float128(2.5)
+    }, 'model_name', 0, TypeError, "provided values type is not valid"),
+    ({
+        "input1": (1, 2, 3)
+    }, 'model_name', 0, TypeError,
+     "values type should be (list, np.ndarray, scalar), but is tuple"),
+    ({
+        "input1": [
+            [bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00]),
+             bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00])],
+            [bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00]),
+             bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00])]
+        ]
+    }, 'model_name', 0, ValueError, "bytes values with dtype DT_STRING must be in shape [N]"),
+]
+
+# (inputs_dict,
+# expected_parsed_input,
+# model_name, model_version)
+PREDICT_REQUEST_VALID = [
+    ({
+        "input1": [1, 2, 3],
+        "input2": array([1.0, 2.0, 3.0]),
+        "input3": [[int32(3), int32(1)], [int32(4), int32(16)]],
+    }, json.dumps({
+        "inputs": {
+            "input1": [1, 2, 3],
+            "input2": [1.0, 2.0, 3.0],
+            "input3": [[3, 1], [4, 16]]
+        }
+    }), 'model_name', 0),
+
+    ({
+        "input1": 5.0,
+        "input2": bytes([1, 2, 3])
+    }, json.dumps({
+        "inputs": {
+            "input1": [5.0],
+            "input2": {"b64": ["AQID"]}
+        }
+    }), 'model_name', 0),
+
+    ({
+
+    }, json.dumps({
+        "inputs": {}
+    }), 'model_name', 0)
+]
+
+# (inputs_dict,
+# expected_parsed_input)
+PARSE_INPUT_DATA_VALID = [
+    (array([[[1, 2, 3]]], dtype=float32), [[[1.0, 2.0, 3.0]]]),
+
+    (array([-1.0, -2.0, -3.0]), [-1.0, -2.0, -3.0]),
+
+    ([[int32(3), int32(1)], [int32(4), int32(16)]], [[3, 1], [4, 16]]),
+
+    (array([[0.012, -0.0002, 0.31, 0.0000014, -0.00054]]),
+     [[0.012, -0.0002, 0.31, 0.0000014, -0.00054]]),
+
+    (1, [1]),
+
+    (0, [0]),
+
+    ([1, 2, 3.0], [1.0, 2.0, 3.0]),
+
+    ([bytes([1, 2, 3]), bytes([4, 5, 6]), bytes([7, 8, 9])], {"b64": ["AQID", "BAUG", "BwgJ"]})
+]
+
+# (inputs_dict,
+# expected_exception, expected_message)
+PARSE_INPUT_DATA_INVALID = [
+    ([[1.0, 2.0], [1.0, 2.0, 3.0]], ValueError,
+     ("argument must be a dense tensor: [[1.0, 2.0], [1.0, 2.0, 3.0]] - "
+      "got shape [2], but wanted [2, 2]")),
+
+    ([[(1, 2, 3)], [(1, 2)], [(1, 2, 3)]],
+     TypeError, "provided values type is not valid"),
+
+    ([1, 2, 3, "str"],
+     TypeError, "provided values type is not valid"),
+
+    ([[1, 2], [3, 4], ["five", 6]],
+     TypeError, "provided values type is not valid"),
+
+    (float128(2.5), TypeError, "provided values type is not valid"),
+
+    ((1, 2, 3), TypeError,
+     "values type should be (list, np.ndarray, scalar), but is tuple"),
+
+    ([
+            [bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00]),
+             bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00])],
+            [bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00]),
+             bytes([0x13, 0x00, 0x00, 0x00, 0x08, 0x00])]
+     ], ValueError, "bytes values with dtype DT_STRING must be in shape [N]"),
+]
