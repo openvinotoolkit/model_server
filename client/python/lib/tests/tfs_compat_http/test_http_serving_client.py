60994b21105c5b5c2d08b3bb01e1b2532df3df50
mkuczyns11 <martin.kuczynski@intel.com>
2021-09-14 09:48:05 +0200

    http serving client test file

---
+++ client/python/lib/tests/tfs_compat_http/test_http_serving_client.py
@@ -0,0 +1,36 @@
+import pytest
+
+from ovmsclient.tfs_compat.http.serving_client import make_http_client
+
+from tfs_compat_http.config import (BUILD_VALID, BUILD_INVALID_CONFIG)
+
+
+@pytest.mark.parametrize("config, method_call_count, expected_client_key, expected_server_cert",
+                         BUILD_VALID)
+def test_make_grpc_client_valid(mocker, config, method_call_count, expected_client_key,
+                                expected_server_cert):
+    mock_check_config = mocker.patch('ovmsclient.tfs_compat.base.serving_client'
+                                     '.ServingClient._check_config')
+
+    client = make_http_client(config)
+
+    assert mock_check_config.call_count == method_call_count['check_config']
+    assert client.address == config['address']
+    assert client.port == config['port']
+    assert client.client_key == expected_client_key
+    assert client.server_cert == expected_server_cert
+
+
+@pytest.mark.parametrize("config, expected_exception, expected_message,"
+                         "method_call_count", BUILD_INVALID_CONFIG)
+def test_make_grpc_client_invalid_config(mocker, config, expected_exception,
+                                         expected_message, method_call_count):
+    mock_check_config = mocker.patch('ovmsclient.tfs_compat.base.serving_client'
+                                     '.ServingClient._check_config',
+                                     side_effect=expected_exception(expected_message))
+
+    with pytest.raises(expected_exception) as e_info:
+        make_http_client(config)
+
+    assert str(e_info.value) == expected_message
+    assert mock_check_config.call_count == method_call_count['check_config']
