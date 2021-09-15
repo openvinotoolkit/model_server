98cae51f6bd2a5f7285464ca21721b69c58ca3c2
mkuczyns11 <martin.kuczynski@intel.com>
2021-09-15 14:57:12 +0200

    session added to serving client

--- client/python/lib/tests/tfs_compat_http/test_http_serving_client.py
+++ client/python/lib/tests/tfs_compat_http/test_http_serving_client.py
@@ -15,6 +15,7 @@
 #
 
 import pytest
+import requests
 
 from ovmsclient.tfs_compat.http.serving_client import make_http_client
 
@@ -23,7 +24,7 @@ from tfs_compat_http.config import (BUILD_VALID, BUILD_INVALID_CONFIG)
 
 @pytest.mark.parametrize("config, method_call_count, expected_client_key, expected_server_cert",
                          BUILD_VALID)
-def test_make_grpc_client_valid(mocker, config, method_call_count, expected_client_key,
+def test_make_http_client_valid(mocker, config, method_call_count, expected_client_key,
                                 expected_server_cert):
     mock_check_config = mocker.patch('ovmsclient.tfs_compat.base.serving_client'
                                      '.ServingClient._check_config')
@@ -35,11 +36,12 @@ def test_make_grpc_client_valid(mocker, config, method_call_count, expected_clie
     assert client.port == config['port']
     assert client.client_key == expected_client_key
     assert client.server_cert == expected_server_cert
+    assert isinstance(client.session, requests.Session)
 
 
 @pytest.mark.parametrize("config, expected_exception, expected_message,"
                          "method_call_count", BUILD_INVALID_CONFIG)
-def test_make_grpc_client_invalid_config(mocker, config, expected_exception,
+def test_make_http_client_invalid_config(mocker, config, expected_exception,
                                          expected_message, method_call_count):
     mock_check_config = mocker.patch('ovmsclient.tfs_compat.base.serving_client'
                                      '.ServingClient._check_config',
