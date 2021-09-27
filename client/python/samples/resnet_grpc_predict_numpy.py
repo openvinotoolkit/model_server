a17747ce31e15eff3b56421fe6dd096dfd57af0c
Miłosz Żeglarski <milosz.zeglarski@intel.com>
2021-09-27 15:34:06 +0200

    Dockerfiles with ovmsclient environment (#932)

--- client/python/samples/resnet_grpc_predict_numpy.py
+++ client/python/samples/resnet_grpc_predict_numpy.py
@@ -14,15 +14,16 @@
 # limitations under the License.
 #
 
+import numpy as np
 import argparse
 from ovmsclient import make_grpc_client, make_grpc_predict_request
-from utils.common import get_model_io_names, read_image_paths, read_imgs_as_ndarray, get_model_input_shape
+from utils.common import get_model_io_names
 from utils.resnet_utils import resnet_postprocess
 
 
 parser = argparse.ArgumentParser(description='Make prediction using images in numerical format')
-parser.add_argument('--images_dir', required=True,
-                    help='Path to a directory with images in JPG or PNG format')
+parser.add_argument('--images_numpy', required=True,
+                    help='Path to a .npy file with data to infer')
 parser.add_argument('--grpc_address', required=False, default='localhost',
                     help='Specify url to grpc service. default:localhost')
 parser.add_argument('--grpc_port', required=False, default=9000, type=int,
@@ -31,14 +32,17 @@ parser.add_argument('--model_name', default='resnet', help='Model name to query.
                     dest='model_name')
 parser.add_argument('--model_version', default=0, type=int, help='Model version to query. default: latest available',
                     dest='model_version')
+parser.add_argument('--iterations', default=0, type=int, help='Total number of requests to be sent. default: 0 - all elements in numpy',
+                    dest='iterations')
 args = vars(parser.parse_args())
 
 # configuration
-images_dir = args.get('images_dir')
+images_numpy_path = args.get('images_numpy')
 address = args.get('grpc_address')
 port = args.get('grpc_port')
 model_name = args.get('model_name')
 model_version = args.get('model_version')
+iterations = args.get('iterations')
 
 # creating grpc client
 config = {
@@ -49,17 +53,17 @@ client = make_grpc_client(config)
 
 # receiving metadata from model
 input_name, output_name = get_model_io_names(client, model_name, model_version)
-input_shape = get_model_input_shape(client, model_name, model_version)
-input_layout = "NCHW"
 
 # preparing images
-imgs = read_imgs_as_ndarray(images_dir, input_shape, input_layout)
-imgs_paths = read_image_paths(images_dir)
+imgs = np.load(images_numpy_path, mmap_mode='r', allow_pickle=False)
 
-for i, img in enumerate(imgs):
+if iterations == 0:
+    iterations = imgs.shape[0]
+
+for i in range (iterations):
     # preparing predict request
     inputs = {
-        input_name: [img]
+        input_name: [imgs[i%imgs.shape[0]]]
     }
     request = make_grpc_predict_request(inputs, model_name, model_version)
 
@@ -68,4 +72,4 @@ for i, img in enumerate(imgs):
 
     # response post processing
     label, confidence_score = resnet_postprocess(response, output_name)
-    print(f"Image {imgs_paths[i]} has been classified as {label} with {confidence_score*100}% confidence")
+    print(f"Image #{i%imgs.shape[0]} has been classified as {label} with {confidence_score*100}% confidence")
