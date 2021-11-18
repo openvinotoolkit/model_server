e8667983edfb6685530496b02329ddee5e3fb58c
Karolina Sankiewicz <karolina.sankiewicz@intel.com>
2021-11-18 14:32:52 +0100

    CVS-67840 build policy (#1028)
    * CVS-67840 build policy

---
+++ ci/buildOnDevelop.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent {
+      label 'ovmscheck'
+    }
+    stages {
+        stage('Configure') {
+          steps {
+            script {
+              checkout scm
+              shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+              echo shortCommit
+            }
+          }
+        }
+
+        stage("Run rebuild on develop branch") {
+          steps {
+              sh """
+              env
+              """
+              echo shortCommit
+              build job: "ovmsc/ubuntu/ubuntu-ovms-recompile-develop", parameters: [[$class: 'StringParameterValue', name: 'OVMSCCOMMIT', value: shortCommit]]
+              build job: "ovmsc/redhat/redhat-ovms-recompile-develop", parameters: [[$class: 'StringParameterValue', name: 'OVMSCCOMMIT', value: shortCommit]]
+          }
+        }
+    }
+}
+
