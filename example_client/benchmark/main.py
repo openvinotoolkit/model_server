72f2361b1fec34eadecb5c4a179e9f4722fdfa7b
Krzysztof Czarnecki <krzysztof.czarnecki@intel.com>
2021-11-24 15:58:40 +0100

    change license

--- example_client/benchmark/main.py
+++ example_client/benchmark/main.py
@@ -1,22 +1,17 @@
 #
-# INTEL CONFIDENTIAL
 # Copyright (c) 2021 Intel Corporation
 #
-# The source code contained or described herein and all documents related to
-# the source code ("Material") are owned by Intel Corporation or its suppliers
-# or licensors. Title to the Material remains with Intel Corporation or its
-# suppliers and licensors. The Material contains trade secrets and proprietary
-# and confidential information of Intel or its suppliers and licensors. The
-# Material is protected by worldwide copyright and trade secret laws and treaty
-# provisions. No part of the Material may be used, copied, reproduced, modified,
-# published, uploaded, posted, transmitted, distributed, or disclosed in any way
-# without Intel's prior express written permission.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# No license under any patent, copyright, trade secret or other intellectual
-# property right is granted to or conferred upon you by disclosure or delivery
-# of the Materials, either expressly, by implication, inducement, estoppel or
-# otherwise. Any license under such intellectual property rights must be express
-# and approved by Intel in writing.
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 ###
@@ -41,6 +36,7 @@ from ovms_benchmark_client.client_nvtrt import NvTrtClient
 from ovms_benchmark_client.client_ovms import OVmsClient
 from ovms_benchmark_client.client import BaseClient
 
+INTERNAL_VERSION="1.9"
 
 # client engine - used for single and multiple client configuration
 def run_single_client(xargs, worker_name_or_client, index, json_flag=None):
@@ -249,7 +245,7 @@ if __name__ == "__main__":
                         help="flag to print internal version")
     xargs = vars(parser.parse_args())
     if xargs["internal_version"]:
-        os.system("cat /ovms_benchmark_client/VERSION")
+        print(INTERNAL_VERSION)
         sys.exit(0)
 
     # check address is specified
