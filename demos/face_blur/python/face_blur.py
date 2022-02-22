48eb34a60bcaf63f7b102db361d16250f56fccf9
mkuczyns11 <martin.kuczynski@intel.com>
2022-02-22 10:14:43 +0100

    cn without blur effect and basic demo created

---
+++ demos/face_blur/python/face_blur.py
@@ -0,0 +1,115 @@
+#
+# Copyright (c) 2022 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import grpc
+import cv2
+import os
+import numpy as np
+from tensorflow import make_tensor_proto, make_ndarray
+import argparse
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+
+parser = argparse.ArgumentParser(description='Client for OCR pipeline')
+parser.add_argument('--grpc_address', required=False, default='localhost',  help='Specify url to grpc service. default:localhost')
+parser.add_argument('--grpc_port', required=False, default=9178, help='Specify port to grpc service. default: 9178')
+parser.add_argument('--pipeline_name', required=False, default='face_blur_pipeline', help='Pipeline name to request. default: face_blur_pipeline')
+parser.add_argument('--image_input_name', required=False, default='image', help='Pipeline input name for input with image. default: image')
+parser.add_argument('--image_input_path', required=True, help='Input image path.')
+parser.add_argument('--people_images_output_name', required=False, default='people_images', help='Pipeline output name for cropped images with people. default: people_images')
+parser.add_argument('--people_images_save_path', required=False, default='', help='If specified, people images will be saved to disk.')
+parser.add_argument('--image_width', required=False, default=1920, help='Original image width. default: 1920')
+parser.add_argument('--image_height', required=False, default=1024, help='Original image height. default: 1024')
+parser.add_argument('--image_layout', required=False, default='NHWC', choices=['NCHW', 'NHWC', 'BINARY'], help='Pipeline input image layout. default: NCHW')
+
+args = vars(parser.parse_args())
+
+def prepare_img_input_in_nchw_format(request, name, path, resize_to_shape):
+    img = cv2.imread(path).astype(np.float32)  # BGR color format, shape HWC
+    img = cv2.resize(img, (resize_to_shape[1], resize_to_shape[0]))
+    target_shape = (img.shape[0], img.shape[1])
+    img = img.transpose(2,0,1).reshape(1,3,target_shape[0],target_shape[1])
+    request.inputs[name].CopyFrom(make_tensor_proto(img, shape=img.shape))
+
+def prepare_img_input_in_nhwc_format(request, name, path, resize_to_shape):
+    img = cv2.imread(path).astype(np.float32)  # BGR color format, shape HWC
+    img = cv2.resize(img, (resize_to_shape[1], resize_to_shape[0]))
+    target_shape = (img.shape[0], img.shape[1])
+    img = img.reshape(1,target_shape[0],target_shape[1],3)
+    request.inputs[name].CopyFrom(make_tensor_proto(img, shape=img.shape))
+
+def prepare_img_input_in_binary_format(request, name, path):
+    with open(path, 'rb') as f:
+        data = f.read()
+        request.inputs[name].CopyFrom(make_tensor_proto(data, shape=[1]))
+
+def draw_boxes(image, detections):
+    width = image.shape[2] if image.shape[0] == 3 else image.shape[1]
+    height = image.shape[1] if image.shape[0] == 3 else image.shape[0]
+    params = detections[0][0]
+    for i in range(params.shape[0]):
+        if params[i][2] > 0.7 and params[i][0] == 0:
+            x_min = int(params[i][3] * width)
+            y_min = int(params[i][4] * height)
+            x_max = int(params[i][5] * width)
+            y_max = int(params[i][6] * height)
+            image = cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0,0,255), 1)
+    return image
+
+def save_image(image, location):
+    if len(image.shape) == 3 and image.shape[0] == 3:  # NCHW
+        image = image.transpose(1,2,0)
+    cv2.imwrite(os.path.join(location, 'image.jpg'), image)
+
+address = "{}:{}".format(args['grpc_address'],args['grpc_port'])
+MAX_MESSAGE_LENGTH = 1024 * 1024 * 1024
+channel = grpc.insecure_channel(address,
+    options=[
+        ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),
+        ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH),
+    ])
+
+stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+request = predict_pb2.PredictRequest()
+request.model_spec.name = args['pipeline_name']
+
+if args['image_layout'] == 'NCHW':
+    prepare_img_input_in_nchw_format(request, args['image_input_name'], args['image_input_path'], (int(args['image_height']), int(args['image_width'])))
+elif args['image_layout'] == 'NHWC':
+    prepare_img_input_in_nhwc_format(request, args['image_input_name'], args['image_input_path'], (int(args['image_height']), int(args['image_width'])))
+else:
+    prepare_img_input_in_binary_format(request, args['image_input_name'], args['image_input_path'])
+
+try:
+    response = stub.Predict(request, 30.0)
+except grpc.RpcError as err:
+    if err.code() == grpc.StatusCode.ABORTED:
+        print('No face has been found in the image')
+        exit(1)
+    else:
+        raise err
+
+blurred_image = make_ndarray(response.outputs['image'])[0]
+
+for name in response.outputs:
+    print(f"Output: name[{name}]")
+    tensor_proto = response.outputs[name]
+    output_nd = make_ndarray(tensor_proto)
+    print(f"    numpy => shape[{output_nd.shape}] data[{output_nd.dtype}]")
+    
+    if name == 'detection':
+        blurred_image = draw_boxes(blurred_image, output_nd)
+        save_image(blurred_image, args['people_images_save_path'])
---
+++ src/custom_nodes/face_blur/face_blur.cpp
@@ -0,0 +1,253 @@
+//*****************************************************************************
+// Copyright 2021 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+#include <iostream>
+
+#include "../../custom_node_interface.h"
+#include "../common/opencv_utils.hpp"
+#include "../common/utils.hpp"
+#include "opencv2/opencv.hpp"
+
+static constexpr const char* TENSOR_NAME = "image";
+static constexpr const char* DETECTION_TENSOR_NAME = "detection";
+
+int initialize(void** customNodeLibraryInternalManager, const struct CustomNodeParam* params, int paramsCount) {
+    return 0;
+}
+
+int deinitialize(void* customNodeLibraryInternalManager) {
+    return 0;
+}
+
+int execute(const struct CustomNodeTensor* inputs, int inputsCount, struct CustomNodeTensor** outputs, int* outputsCount, const struct CustomNodeParam* params, int paramsCount, void* customNodeLibraryInternalManager) {
+    // Parameters reading
+    int originalImageHeight = get_int_parameter("original_image_height", params, paramsCount, -1);
+    int originalImageWidth = get_int_parameter("original_image_width", params, paramsCount, -1);
+    NODE_ASSERT(originalImageHeight > 0, "original image height must be larger than 0");
+    NODE_ASSERT(originalImageWidth > 0, "original image width must be larger than 0");
+    int targetImageHeight = get_int_parameter("target_image_height", params, paramsCount, -1);
+    int targetImageWidth = get_int_parameter("target_image_width", params, paramsCount, -1);
+    NODE_ASSERT(targetImageHeight > 0, "target image height must be larger than 0");
+    NODE_ASSERT(targetImageWidth > 0, "target image width must be larger than 0");
+    std::string originalImageLayout = get_string_parameter("original_image_layout", params, paramsCount, "NCHW");
+    NODE_ASSERT(originalImageLayout == "NCHW" || originalImageLayout == "NHWC", "original image layout must be NCHW or NHWC");
+    std::string targetImageLayout = get_string_parameter("target_image_layout", params, paramsCount, "NCHW");
+    NODE_ASSERT(targetImageLayout == "NCHW" || targetImageLayout == "NHWC", "target image layout must be NCHW or NHWC");
+    bool convertToGrayScale = get_string_parameter("convert_to_gray_scale", params, paramsCount) == "true";
+    uint64_t targetImageColorChannels = convertToGrayScale ? 1 : 3;
+    float confidenceThreshold = get_float_parameter("confidence_threshold", params, paramsCount, -1.0);
+    NODE_ASSERT(confidenceThreshold >= 0 && confidenceThreshold <= 1.0, "confidence threshold must be in 0-1 range");
+    // uint64_t maxOutputBatch = get_int_parameter("max_output_batch", params, paramsCount, 100);
+    bool debugMode = get_string_parameter("debug", params, paramsCount) == "true";
+
+    const CustomNodeTensor* imageTensor = nullptr;
+    const CustomNodeTensor* detectionTensor = nullptr;
+
+    for (int i = 0; i < inputsCount; i++) {
+        if (std::strcmp(inputs[i].name, TENSOR_NAME) == 0) {
+            imageTensor = &(inputs[i]);
+        } else if (std::strcmp(inputs[i].name, DETECTION_TENSOR_NAME) == 0) {
+            detectionTensor = &(inputs[i]);
+        } else {
+            std::cout << "Unrecognized input: " << inputs[i].name << std::endl;
+            return 1;
+        }
+    }
+
+    NODE_ASSERT(imageTensor != nullptr, "Missing input image");
+    NODE_ASSERT(detectionTensor != nullptr, "Missing input scores");
+    NODE_ASSERT(imageTensor->precision == FP32, "image input is not FP32");
+    NODE_ASSERT(detectionTensor->precision == FP32, "image input is not FP32");
+
+    NODE_ASSERT(imageTensor->dimsCount == 4, "input image shape must have 4 dimensions");
+    NODE_ASSERT(imageTensor->dims[0] == 1, "input image batch must be 1");
+    NODE_ASSERT(imageTensor->dims[originalImageLayout == "NCHW" ? 1 : 3] == 3, "input image needs to have 3 color channels");
+
+    NODE_ASSERT(detectionTensor->dimsCount == 4, "input detection shape must have 4 dimensions");
+    NODE_ASSERT(detectionTensor->dims[0] == 1, "input detection dim[0] must be 1");
+    NODE_ASSERT(detectionTensor->dims[1] == 1, "input detection dim[1] must be 1");
+    NODE_ASSERT(detectionTensor->dims[2] == 200, "input detection dim[2] must be 200");
+    NODE_ASSERT(detectionTensor->dims[3] == 7, "input detection dim[3] must be 7");
+
+    uint64_t _imageHeight = imageTensor->dims[originalImageLayout == "NCHW" ? 2 : 1];
+    uint64_t _imageWidth = imageTensor->dims[originalImageLayout == "NCHW" ? 3 : 2];
+    NODE_ASSERT(_imageHeight <= std::numeric_limits<int>::max(), "image height is too large");
+    NODE_ASSERT(_imageWidth <= std::numeric_limits<int>::max(), "image width is too large");
+    int imageHeight = static_cast<int>(_imageHeight);
+    int imageWidth = static_cast<int>(_imageWidth);
+
+    if (debugMode) {
+        std::cout << "Processing input tensor image resolution: " << cv::Size(imageHeight, imageWidth) << "; expected resolution: " << cv::Size(originalImageHeight, originalImageWidth) << std::endl;
+    }
+
+    NODE_ASSERT(imageHeight == originalImageHeight, "original image size parameter differs from original image tensor size");
+    NODE_ASSERT(imageWidth == originalImageWidth, "original image size parameter differs from original image tensor size");
+
+    cv::Mat image;
+    if (originalImageLayout == "NHWC") {
+        image = nhwc_to_mat(imageTensor);
+    } else {
+        image = nchw_to_mat(imageTensor);
+    }
+
+    NODE_ASSERT(image.cols == imageWidth, "Mat generation failed");
+    NODE_ASSERT(image.rows == imageHeight, "Mat generation failed");
+
+    // uint64_t detectionsCount = detectionTensor->dims[2];
+    // uint64_t featuresCount = detectionTensor->dims[3];
+
+    // std::vector<cv::Rect> boxes;
+
+    // for (uint64_t i = 0; i < detectionsCount; i++) {
+    //     float* detection = (float*)(detectionTensor->data + (i * featuresCount * sizeof(float)));
+    //     int imageId = static_cast<int>(detection[0]);
+    //     int labelId = static_cast<int>(detection[1]);
+    //     float confidence = detection[2];
+    //     int xMin = static_cast<int>(detection[3] * imageWidth);
+    //     int yMin = static_cast<int>(detection[4] * imageHeight);
+    //     int xMax = static_cast<int>(detection[5] * imageWidth);
+    //     int yMax = static_cast<int>(detection[6] * imageHeight);
+    //     if (imageId == 0 && confidence >= confidenceThreshold) {
+    //         auto box = cv::Rect(cv::Point(xMin, yMin), cv::Point(xMax, yMax));
+    //         boxes.emplace_back(box);
+    //         if (debugMode) {
+    //             std::cout << "Detection:\nImageID: " << imageId << "; LabelID:" << labelId << "; Confidence:" << confidence << "; Box:" << box << std::endl;
+    //         }
+    //     }
+    // }
+
+    // if (boxes.size() > maxOutputBatch) {
+    //     boxes.resize(maxOutputBatch);
+    // }
+
+    // Prepare output tensor
+    uint64_t byteSize = sizeof(float) * targetImageHeight * targetImageWidth * targetImageColorChannels;
+    NODE_ASSERT(image.total() * image.elemSize() == byteSize, "buffer size differs");
+    float* buffer = (float*)malloc(byteSize);
+    NODE_ASSERT(buffer != nullptr, "malloc has failed");
+
+    if (targetImageLayout == "NCHW") {
+        reorder_to_nchw_2<float>((float*)image.data, (float*)buffer, image.rows, image.cols, image.channels());
+    } else {
+        std::memcpy((uint8_t*)buffer, image.data, byteSize);
+    }
+
+    *outputsCount = 1;
+    *outputs = (struct CustomNodeTensor*)malloc(*outputsCount * sizeof(CustomNodeTensor));
+    if ((*outputs) == nullptr) {
+        std::cout << "malloc has failed" << std::endl;
+        return 1;
+    }
+
+    CustomNodeTensor& output = (*outputs)[0];
+    output.name = TENSOR_NAME;
+    output.data = reinterpret_cast<uint8_t*>(buffer);
+    output.dataBytes = byteSize;
+    output.dimsCount = 4;
+    output.dims = (uint64_t*)malloc(output.dimsCount * sizeof(uint64_t));
+    NODE_ASSERT(output.dims != nullptr, "malloc has failed");
+    output.dims[0] = 1;
+    output.dims[0] = 1;
+    if (targetImageLayout == "NCHW") {
+        output.dims[1] = targetImageColorChannels;
+        output.dims[2] = targetImageHeight;
+        output.dims[3] = targetImageWidth;
+    } else {
+        output.dims[1] = targetImageHeight;
+        output.dims[2] = targetImageWidth;
+        output.dims[3] = targetImageColorChannels;
+    }
+    output.precision = FP32;
+    return 0;
+}
+
+int getInputsInfo(struct CustomNodeTensorInfo** info, int* infoCount, const struct CustomNodeParam* params, int paramsCount, void* customNodeLibraryInternalManager) {
+    int originalImageHeight = get_int_parameter("original_image_height", params, paramsCount, -1);
+    int originalImageWidth = get_int_parameter("original_image_width", params, paramsCount, -1);
+    NODE_ASSERT(originalImageHeight > 0, "original image height must be larger than 0");
+    NODE_ASSERT(originalImageWidth > 0, "original image width must be larger than 0");
+    std::string originalImageLayout = get_string_parameter("original_image_layout", params, paramsCount, "NCHW");
+    NODE_ASSERT(originalImageLayout == "NCHW" || originalImageLayout == "NHWC", "original image layout must be NCHW or NHWC");
+
+    *infoCount = 2;
+    *info = (struct CustomNodeTensorInfo*)malloc(*infoCount * sizeof(struct CustomNodeTensorInfo));
+    NODE_ASSERT((*info) != nullptr, "malloc has failed");
+
+    (*info)[0].name = TENSOR_NAME;
+    (*info)[0].dimsCount = 4;
+    (*info)[0].dims = (uint64_t*)malloc((*info)[0].dimsCount * sizeof(uint64_t));
+    NODE_ASSERT(((*info)[0].dims) != nullptr, "malloc has failed");
+    (*info)[0].dims[0] = 1;
+    if (originalImageLayout == "NCHW") {
+        (*info)[0].dims[1] = 3;
+        (*info)[0].dims[2] = originalImageHeight;
+        (*info)[0].dims[3] = originalImageWidth;
+    } else {
+        (*info)[0].dims[1] = originalImageHeight;
+        (*info)[0].dims[2] = originalImageWidth;
+        (*info)[0].dims[3] = 3;
+    }
+    (*info)[0].precision = FP32;
+
+    (*info)[1].name = DETECTION_TENSOR_NAME;
+    (*info)[1].dimsCount = 4;
+    (*info)[1].dims = (uint64_t*)malloc((*info)[1].dimsCount * sizeof(uint64_t));
+    NODE_ASSERT(((*info)[1].dims) != nullptr, "malloc has failed");
+    (*info)[1].dims[0] = 1;
+    (*info)[1].dims[1] = 1;
+    (*info)[1].dims[2] = 200;
+    (*info)[1].dims[3] = 7;
+    (*info)[1].precision = FP32;
+    return 0;
+}
+
+int getOutputsInfo(struct CustomNodeTensorInfo** info, int* infoCount, const struct CustomNodeParam* params, int paramsCount, void* customNodeLibraryInternalManager) {
+    int targetImageHeight = get_int_parameter("target_image_height", params, paramsCount, -1);
+    int targetImageWidth = get_int_parameter("target_image_width", params, paramsCount, -1);
+    NODE_ASSERT(targetImageHeight > 0, "target image height must be larger than 0");
+    NODE_ASSERT(targetImageWidth > 0, "target image width must be larger than 0");
+    std::string targetImageLayout = get_string_parameter("target_image_layout", params, paramsCount, "NCHW");
+    NODE_ASSERT(targetImageLayout == "NCHW" || targetImageLayout == "NHWC", "target image layout must be NCHW or NHWC");
+    bool convertToGrayScale = get_string_parameter("convert_to_gray_scale", params, paramsCount) == "true";
+
+    *infoCount = 1;
+    *info = (struct CustomNodeTensorInfo*)malloc(*infoCount * sizeof(struct CustomNodeTensorInfo));
+    NODE_ASSERT((*info) != nullptr, "malloc has failed");
+
+    (*info)[0].name = TENSOR_NAME;
+    (*info)[0].dimsCount = 4;
+    (*info)[0].dims = (uint64_t*)malloc((*info)->dimsCount * sizeof(uint64_t));
+    NODE_ASSERT(((*info)[0].dims) != nullptr, "malloc has failed");
+    (*info)[0].dims[0] = 1;
+
+    if (targetImageLayout == "NHWC") {
+        (*info)[0].dims[1] = targetImageHeight ;
+        (*info)[0].dims[2] = targetImageWidth;
+        (*info)[0].dims[3] = convertToGrayScale ? 1 : 3;
+    } else {
+        (*info)[0].dims[1] = convertToGrayScale ? 1 : 3;
+        (*info)[0].dims[2] = targetImageHeight;
+        (*info)[0].dims[3] = targetImageWidth;
+    }
+
+    (*info)[0].precision = FP32;
+
+    return 0;
+}
+
+int release(void* ptr, void* customNodeLibraryInternalManager) {
+    free(ptr);
+    return 0;
+}
