d917289edf89ba9fcf9bd5e7330067e28dfa9a4d
Jasinski, Rafal <rafal.jasinski@intel.com>
2022-03-25 11:42:03 +0100

    .

--- tests/functional/utils/port_manager.py
+++ tests/functional/utils/port_manager.py
@@ -1,29 +1,25 @@
 #
-# INTEL CONFIDENTIAL
 # Copyright (c) 2022 Intel Corporation
 #
-# The source code contained or described herein and all documents related to
-# the source code ("Material") are owned by Intel Corporation or its suppliers
-# or licensors. Title to the Material remains with Intel Corporation or its
-# suppliers and licensors. The Material contains trade secrets and proprietary
-# and confidential information of Intel or its suppliers and licensors. The
-# Material is protected by worldwide copyright and trade secret laws and treaty
-# provisions. No part of the Material may be used, copied, reproduced, modified,
-# published, uploaded, posted, transmitted, distributed, or disclosed in any way
-# without Intel's prior express written permission.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# No license under any patent, copyright, trade secret or other intellectual
-# property right is granted to or conferred upon you by disclosure or delivery
-# of the Materials, either expressly, by implication, inducement, estoppel or
-# otherwise. Any license under such intellectual property rights must be express
-# and approved by Intel in writing.
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import logging
 import socket
 import errno
 
-from common_libs.logger import get_logger
+from utils.helpers import get_xdist_worker_count, get_xdist_worker_nr
 
-logger = get_logger(__name__)
+logger = logging.getLogger(__name__)
 
 class SimplePortManager:
     def __init__(self, name: str):
