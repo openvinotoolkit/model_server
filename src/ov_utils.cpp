c0944d9b6914bf7813ea411412068f6efe70a32e
michalkulakowski <michal.kulakowski@intel.com>
2022-04-21 16:44:06 +0200

    Add plugin config validation for multi and auto

--- src/ov_utils.cpp
+++ src/ov_utils.cpp
@@ -84,36 +84,41 @@ Status validatePluginConfiguration(const plugin_config_t& pluginConfig, const st
     auto availableDevices = ieCore.get_available_devices();
     auto availablePlugins = availableDevices;
     const std::string supportedConfigKey = METRIC_KEY(SUPPORTED_CONFIG_KEYS);
+    std::vector<std::string> allSupportedConfigKeys;
     for (const auto& plugin : availablePlugins) {
-        std::vector<std::string> supportedConfigKeys;
+        std::vector<std::string> pluginSupportedConfigKeys;
         try {
             SPDLOG_LOGGER_DEBUG(modelmanager_logger, "Validating plugin: {}; configuration", plugin);
-            std::vector<std::string> supportedConfigKeys2 = ieCore.get_property(plugin, supportedConfigKey).as<std::vector<std::string>>();
-            supportedConfigKeys = std::move(supportedConfigKeys2);
+            std::vector<std::string> pluginSupportedConfigKeys2 = ieCore.get_property(plugin, supportedConfigKey).as<std::vector<std::string>>();
+            pluginSupportedConfigKeys = std::move(pluginSupportedConfigKeys2);
         } catch (std::exception& e) {
             SPDLOG_LOGGER_DEBUG(modelmanager_logger, "Exception thrown from IE when requesting plugin: {}; key: {}; value. Error: {}", plugin, supportedConfigKey, e.what());
         } catch (...) {
             SPDLOG_LOGGER_DEBUG(modelmanager_logger, "Exception thrown from IE when requesting plugin: {}; key: {}; value.", plugin, supportedConfigKey);
         }
-        if(plugin == targetDevice){
-            for (auto& config : pluginConfig) {
-                if (std::find(supportedConfigKeys.begin(), supportedConfigKeys.end(), config.first) == supportedConfigKeys.end())
-                {
-                    std::ostringstream supportedConfigKeysStr;
-                    if (!supportedConfigKeys.empty())
-                    {
-                        std::copy(supportedConfigKeys.begin(), supportedConfigKeys.end()-1,
-                            std::ostream_iterator<std::string>(supportedConfigKeysStr, ","));
-                    
-                        supportedConfigKeysStr << supportedConfigKeys.back();
-                    }
 
-                    SPDLOG_LOGGER_ERROR(modelmanager_logger, "Plugin config key: {} not found in supported config keys for {} device. List of supported keys for this device: {}", config.first, plugin, supportedConfigKeysStr.str());
-                    return StatusCode::MODEL_CONFIG_INVALID;
-                }
-            }
+        if(targetDevice.find(plugin) != std::string::npos || targetDevice == "AUTO"){
+            allSupportedConfigKeys.insert(allSupportedConfigKeys.end(), pluginSupportedConfigKeys.begin(), pluginSupportedConfigKeys.end());
         }
     }
+
+    for (auto& config : pluginConfig) {
+        if (std::find(allSupportedConfigKeys.begin(), allSupportedConfigKeys.end(), config.first) == allSupportedConfigKeys.end())
+        {
+            std::ostringstream allSupportedConfigKeysStr;
+            if (!allSupportedConfigKeys.empty())
+            {
+                std::copy(allSupportedConfigKeys.begin(), allSupportedConfigKeys.end()-1,
+                    std::ostream_iterator<std::string>(allSupportedConfigKeysStr, ","));
+            
+                allSupportedConfigKeysStr << allSupportedConfigKeys.back();
+            }
+
+            SPDLOG_LOGGER_ERROR(modelmanager_logger, "Plugin config key: {} not found in supported config keys for device: {}. List of supported keys for this device: {}.", config.first, targetDevice, allSupportedConfigKeysStr.str());
+            return StatusCode::MODEL_CONFIG_INVALID;
+        }
+    }
+
     return StatusCode::OK;
 }
 }  // namespace ovms
