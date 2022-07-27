d2179eb9752f54f7fa4425f93ef0150834473d65
michalkulakowski <Michal.Kulakowski@intel.com>
2022-07-27 08:31:19 +0200

    Fix make perf_test (#1332)
    * Fix ONNX functional tests after change in ONNX repo
    
    This change makes models support dynamic batch size:
    https://github.com/onnx/models/commit/652f4e4af7975c8e7a505c4b6e0f8ac72d8260ea
    
    We need to hardcode ONNX Model version. We can bump model version later
    with tests refactor. This would require fix on OV/ONNX side as model
    reports that dynamic bs model have dynamic shape only on input not
    output.
    
    JIRA:CVS-87169
    
    * Specify protobuf version in tests/requirements
    
    * Remove unnecessary input shape change in mo.
    
    * Correct Resnet models information.
    
    Co-authored-by: atobisze <adrian.tobiszewski@intel.com>

--- tests/functional/model/models_information.py
+++ tests/functional/model/models_information.py
@@ -104,8 +104,8 @@ class Resnet:
     name = "resnet"
     dtype = np.float32
     input_name = "map/TensorArrayStack/TensorArrayGatherV3"
-    input_shape = (1, 3, 224, 224)
-    output_name = "softmax_tensor"
+    input_shape = (1, 224, 224, 3)
+    output_name = "softmax_tensor:0"
     output_shape = (1, 1001)
     rest_request_format = 'column_name'
     model_path = os.path.join(config.models_path, name)
@@ -120,8 +120,8 @@ class ResnetBS4:
     name = "resnet_bs4"
     dtype = np.float32
     input_name = "map/TensorArrayStack/TensorArrayGatherV3"
-    input_shape = (4, 3, 224, 224)
-    output_name = "softmax_tensor"
+    input_shape = (4, 224, 224, 3)
+    output_name = "softmax_tensor:0"
     output_shape = (4, 1001)
     rest_request_format = 'row_noname'
     version = 1
@@ -131,8 +131,8 @@ class ResnetBS8:
     name = "resnet_bs8"
     dtype = np.float32
     input_name = "map/TensorArrayStack/TensorArrayGatherV3"
-    input_shape = (8, 3, 224, 224)
-    output_name = "softmax_tensor"
+    input_shape = (8, 224, 224, 3)
+    output_name = "softmax_tensor:0"
     output_shape = (8, 1001)
     rest_request_format = 'row_noname'
     model_path = os.path.join(config.models_path, name)
@@ -143,8 +143,8 @@ class ResnetS3:
     name = "resnet_s3"
     dtype = np.float32
     input_name = "map/TensorArrayStack/TensorArrayGatherV3"
-    input_shape = (1, 3, 224, 224)
-    output_name = "softmax_tensor"
+    input_shape = (1, 224, 224, 3)
+    output_name = "softmax_tensor:0"
     output_shape = (1, 1001)
     rest_request_format = 'row_name'
     model_path = "s3://inference/resnet"
@@ -168,7 +168,7 @@ class ResnetONNX:
     output_name = "gpu_0/softmax_1"
     output_shape = (1, 1000)
     rest_request_format = 'row_name'
-    url = "https://github.com/onnx/models/raw/main/vision/classification/resnet/model/resnet50-caffe2-v1-9"
+    url = "https://github.com/onnx/models/raw/cf382db7781fc8193249386d6b50a4753659d058/vision/classification/resnet/model/resnet50-caffe2-v1-9"
     download_extensions = [".onnx"]
     version = 1
     model_path = os.path.join(config.models_path, name)
