4612e7441c4e72f9e73180cfe0a025fb4ad71151
bstrzele <bartosz.strzelecki@intel.com>
2022-09-23 14:44:52 +0200

    added documentation for grpc infer dummy

--- client/cpp/kserve-api/README.md
+++ client/cpp/kserve-api/README.md
@@ -14,3 +14,53 @@ cd model_server/client/cpp/kserve-api
 ```Bash
 cmake . && make
 ```
+
+## GRPC dummy examples
+
+### Start the Model Server Container with dummy Model
+```Bash
+docker run --rm -d -v $(pwd)/models:/models -p 9000:9000 openvino/model_server:latest --model_name dummy --model_path /models/dummy --port 9000 
+```
+
+Once you finish above steps, you are ready to run the samples.
+
+### Run the Client to perform inference
+
+```Bash
+./grpc_infer_dummy --help
+Sends requests via KServe gRPC API.
+Usage:
+  grpc_infer_dummy [OPTION...]
+
+  -h, --help              Show this help message and exit
+      --grpc_address arg  Specify url to grpc service.  (default: 
+                          localhost)
+      --grpc_port arg     Specify port to grpc service.  (default: 9000)
+      --input_name arg    Specify input tensor name.  (default: b)
+      --output_name arg   Specify input tensor name.  (default: a)
+      --model_name arg    Define model name, must be same as is in service. 
+                           (default: dummy)
+      --model_version     Define model version.
+      --timeout arg       Request timeout. (default: 0)
+```
+
+- Usage Example
+
+```Bash
+./grpc_infer_dummy --grpc_port 9000 --model_name dummy
+0 => 1
+1 => 2
+2 => 3
+3 => 4
+4 => 5
+5 => 6
+6 => 7
+7 => 8
+8 => 9
+9 => 10
+======Client Statistics======
+completed_request_count 1
+cumulative_total_request_time_ns 5207356
+cumulative_send_time_ns 46654
+cumulative_receive_time_ns 5312
+```
---
+++ client/cpp/kserve-api/samples/grpc_server_live.cc
@@ -0,0 +1,73 @@
+//*****************************************************************************
+// Copyright 2022 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+#include <iostream>
+#include <string>
+
+#include <cxxopts.hpp>
+
+#include "grpc_client.h"
+
+namespace tc = triton::client;
+
+#define FAIL_IF_ERR(X, MSG)                                              \
+    {                                                                    \
+        tc::Error err = (X);                                             \
+        if (!err.IsOk()) {                                               \
+            std::cerr << "error: " << (MSG) << ": " << err << std::endl; \
+            exit(1);                                                     \
+        }                                                                \
+    }
+
+
+int main(int argc, char** argv) {
+    cxxopts::Options opt("grpc_infer_dummy", "Sends requests via KServe gRPC API.");
+
+    // clang-format off
+    opt.add_options()
+    ("h,help", "Show this help message and exit")
+    ("grpc_address", "Specify url to grpc service. ", cxxopts::value<std::string>()->default_value("localhost"))
+    ("grpc_port", "Specify port to grpc service. ", cxxopts::value<std::string>()->default_value("9000"))
+    ("timeout", "Request timeout.", cxxopts::value<int>()->default_value("0"))
+    ;
+    // clang-format on
+
+    auto args = opt.parse(argc, argv);
+
+    if (args.count("help")) {
+        std::cout << opt.help() << std::endl;
+        exit(0);
+    }
+
+
+    std::string url(args["grpc_address"].as<std::string>() + ":" + args["grpc_port"].as<std::string>());
+
+    // Create a InferenceServerGrpcClient instance to communicate with the
+    // server using gRPC protocol.
+    std::unique_ptr<tc::InferenceServerGrpcClient> client;
+
+    FAIL_IF_ERR(
+        tc::InferenceServerGrpcClient::Create(&client, url),
+        err);
+
+    bool live;
+    FAIL_IF_ERR(
+        client->IsServerLive(&live),
+        "unable to get server liveness");
+    std::cout<<"Server Live: "<<(live?"True":"False") << std::endl;
+
+
+    return 0;
+}
