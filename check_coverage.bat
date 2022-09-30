50ed668db41f630223ecc616eff6c6aa09609220
rasapala <rafal.a.sapala@intel.com>
2022-09-30 11:55:13 +0200

    Cov fix

--- check_coverage.bat
+++ check_coverage.bat
@@ -9,15 +9,15 @@ MIN_LINES_COV=73.3
 MIN_FUNCTION_COV=73.6
 
 
-LINES_COV=`cat genhtml/index.html | grep "headerCovTableEntry.*%" | grep -oP  ">\K(\d*.\d*)" | head -n 1`
-FUNC_COV=`cat genhtml/index.html | grep "headerCovTableEntry.*%" | grep -oP  ">\K(\d*.\d*)" | tail -n 1`
+LINES_COV=`cat genhtml/index.html | grep "headerCovTableEntry.*%" | grep -oP  ">\K(\d*.\d*) " | head -n 1`
+FUNC_COV=`cat genhtml/index.html | grep "headerCovTableEntry.*%" | grep -oP  ">\K(\d*.\d*) " | tail -n 1`
 
 if (( $(echo "$MIN_LINES_COV > $LINES_COV" | bc -l) )); then
     echo "Error: $LINES_COV % Lines coverage is lower than minimal $MIN_LINES_COV %"
     exit 1
 fi
 
-if (( $(echo "$MIN_FUNCTION_COV > $FUNCTION_COV" | bc -l) )); then
+if (( $(echo "$MIN_FUNCTION_COV > $FUNC_COV" | bc -l) )); then
     echo "Error: $FUNCTION_COV % Functions coverage is lower than minimal $MIN_FUNCTION_COV %"
     exit 1
 fi
