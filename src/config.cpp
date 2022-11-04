2ac36f9e2e11dc0c39696cda8e0136c7a8971efb
Damian Kalinowski <damian.kalinowski@intel.com>
2022-11-04 09:46:02 +0100

    move to cpp file

--- src/config.cpp
+++ src/config.cpp
@@ -374,4 +374,230 @@ void Config::validate() {
     return;
 }
 
+const std::string& Config::__configPath() const {
+    if (result->count("config_path"))
+        return result->operator[]("config_path").as<std::string>();
+    return empty;
+}
+
+const std::string& Config::configPath() const { return _configPath; }
+
+uint64_t Config::__port() const {
+    return result->operator[]("port").as<uint64_t>();
+}
+
+uint64_t Config::port() const { return _port; }
+
+const std::string Config::__cpuExtensionLibraryPath() const {
+    if (result != nullptr && result->count("cpu_extension")) {
+        return result->operator[]("cpu_extension").as<std::string>();
+    }
+    return "";
+}
+
+const std::string Config::cpuExtensionLibraryPath() const { return _cpuExtensionLibraryPath; }
+
+const std::string Config::__grpcBindAddress() const {
+    if (result->count("grpc_bind_address"))
+        return result->operator[]("grpc_bind_address").as<std::string>();
+    return "0.0.0.0";
+}
+
+const std::string Config::grpcBindAddress() const { return _grpcBindAddress; }
+
+uint64_t Config::__restPort() const {
+    return result->operator[]("rest_port").as<uint64_t>();
+}
+
+uint64_t Config::restPort() const { return _restPort; }
+
+const std::string Config::__restBindAddress() const {
+    if (result->count("rest_bind_address"))
+        return result->operator[]("rest_bind_address").as<std::string>();
+    return "0.0.0.0";
+}
+
+const std::string Config::restBindAddress() const { return _restBindAddress; }
+
+uint Config::__grpcWorkers() const {
+    return result->operator[]("grpc_workers").as<uint>();
+}
+
+uint Config::grpcWorkers() const { return _grpcWorkers; }
+
+uint Config::__restWorkers() const {
+    return result->operator[]("rest_workers").as<uint>();
+}
+
+uint Config::restWorkers() const { return _restWorkers; }
+
+const std::string& Config::__modelName() const {
+    if (result->count("model_name"))
+        return result->operator[]("model_name").as<std::string>();
+    return empty;
+}
+
+const std::string& Config::modelName() const { return _modelName; }
+
+const std::string& Config::__modelPath() const {
+    if (result->count("model_path"))
+        return result->operator[]("model_path").as<std::string>();
+    return empty;
+}
+
+const std::string& Config::modelPath() const { return _modelPath; }
+
+const std::string& Config::__batchSize() const {
+    if (!result->count("batch_size")) {
+        static const std::string d = "0";
+        return d;
+    }
+    return result->operator[]("batch_size").as<std::string>();
+}
+
+const std::string& Config::batchSize() const { return _batchSize; }
+
+const std::string& Config::__shape() const {
+    if (result->count("shape"))
+        return result->operator[]("shape").as<std::string>();
+    return empty;
+}
+
+const std::string& Config::Config::shape() const { return _shape; }
+
+const std::string& Config::__layout() const {
+    if (result->count("layout"))
+        return result->operator[]("layout").as<std::string>();
+    return empty;
+}
+
+const std::string& Config::layout() const { return _layout; }
+
+const std::string& Config::__modelVersionPolicy() const {
+    if (result->count("model_version_policy"))
+        return result->operator[]("model_version_policy").as<std::string>();
+    return empty;
+}
+
+const std::string& Config::modelVersionPolicy() const { return _modelVersionPolicy; }
+
+uint32_t Config::__nireq() const {
+    if (!result->count("nireq")) {
+        return 0;
+    }
+    return result->operator[]("nireq").as<uint32_t>();
+}
+
+uint32_t Config::nireq() const { return _nireq; }
+
+const std::string& Config::__targetDevice() const {
+    return result->operator[]("target_device").as<std::string>();
+}
+
+const std::string& Config::targetDevice() const { return _targetDevice; }
+
+const std::string& Config::__pluginConfig() const {
+    if (result->count("plugin_config"))
+        return result->operator[]("plugin_config").as<std::string>();
+    return empty;
+}
+const std::string& Config::Config::pluginConfig() const { return _pluginConfig; }
+
+bool Config::__stateful() const {
+    return result->operator[]("stateful").as<bool>();
+}
+
+bool Config::stateful() const { return _stateful; }
+
+bool Config::__metricsEnabled() const {
+    if (!result->count("metrics_enable")) {
+        return false;
+    }
+    return result->operator[]("metrics_enable").as<bool>();
+}
+
+bool Config::metricsEnabled() const { return _metricsEnabled; }
+
+std::string Config::__metricsList() const {
+    if (!result->count("metrics_list")) {
+        return std::string("");
+    }
+    return result->operator[]("metrics_list").as<std::string>();
+}
+
+std::string Config::metricsList() const { return _metricsList; }
+
+bool Config::__idleSequenceCleanup() const {
+    return result->operator[]("idle_sequence_cleanup").as<bool>();
+}
+
+bool Config::idleSequenceCleanup() const { return _idleSequenceCleanup; }
+
+bool Config::__lowLatencyTransformation() const {
+    return result->operator[]("low_latency_transformation").as<bool>();
+}
+
+bool Config::lowLatencyTransformation() const { return _lowLatencyTransformation; }
+
+const std::string& Config::__logLevel() const {
+    if (result->count("log_level"))
+        return result->operator[]("log_level").as<std::string>();
+    return empty;
+}
+
+const std::string& Config::logLevel() const { return _logLevel; }
+
+const std::string& Config::__logPath() const {
+    if (result->count("log_path"))
+        return result->operator[]("log_path").as<std::string>();
+    return empty;
+}
+
+const std::string& Config::logPath() const { return _logPath; }
+
+#ifdef MTR_ENABLED
+const std::string& Config::__tracePath() const {
+    if (result->count("trace_path"))
+        return result->operator[]("trace_path").as<std::string>();
+    return empty;
+}
+
+const std::string& Config::tracePath() const { return _tracePath; }
+#endif
+
+const std::string& Config::__grpcChannelArguments() const {
+    if (result->count("grpc_channel_arguments"))
+        return result->operator[]("grpc_channel_arguments").as<std::string>();
+    return empty;
+}
+
+const std::string& Config::grpcChannelArguments() const { return _grpcChannelArguments; }
+
+uint Config::__filesystemPollWaitSeconds() const {
+    return result->operator[]("file_system_poll_wait_seconds").as<uint>();
+}
+
+uint Config::filesystemPollWaitSeconds() const { return _filesystemPollWaitSeconds; }
+
+uint32_t Config::__sequenceCleanerPollWaitMinutes() const {
+    return result->operator[]("sequence_cleaner_poll_wait_minutes").as<uint32_t>();
+}
+
+uint32_t Config::sequenceCleanerPollWaitMinutes() const { return _sequenceCleanerPollWaitMinutes; }
+
+uint32_t Config::__resourcesCleanerPollWaitSeconds() const {
+    return result->operator[]("custom_node_resources_cleaner_interval").as<uint32_t>();
+}
+
+uint32_t Config::resourcesCleanerPollWaitSeconds() const { return _resourcesCleanerPollWaitSeconds; }
+
+const std::string Config::__cacheDir() const {
+    if (result != nullptr && result->count("cache_dir")) {
+        return result->operator[]("cache_dir").as<std::string>();
+    }
+    return empty;
+}
+
+const std::string Config::cacheDir() const { return _cacheDir; }
+
 }  // namespace ovms
--- src/config.hpp
+++ src/config.hpp
@@ -130,261 +130,183 @@ public:
          * 
          * @return std::string 
          */
-    const std::string& __configPath() const {
-        if (result->count("config_path"))
-            return result->operator[]("config_path").as<std::string>();
-        return empty;
-    }
-    const std::string& configPath() const { return _configPath; }
+    const std::string& __configPath() const;  // TODO: Move to CLI parser when ready
+    const std::string& configPath() const;
 
     /**
          * @brief Gets the grpc port
          * 
          * @return uint64_t
          */
-    uint64_t __port() const {
-        return result->operator[]("port").as<uint64_t>();
-    }
-    uint64_t port() const { return _port; }
+    uint64_t __port() const;
+    uint64_t port() const;
 
     /**
          * @brief Get the gRPC network interface address to bind to
          * 
          * @return const std::string
          */
-    const std::string __cpuExtensionLibraryPath() const {
-        if (result != nullptr && result->count("cpu_extension")) {
-            return result->operator[]("cpu_extension").as<std::string>();
-        }
-        return "";
-    }
-    const std::string cpuExtensionLibraryPath() const { return _cpuExtensionLibraryPath; }
+    const std::string __cpuExtensionLibraryPath() const;  // TODO: Move to CLI parser when ready
+    const std::string cpuExtensionLibraryPath() const;
 
     /**
          * @brief Get the gRPC network interface address to bind to
          * 
          * @return const std::string&
          */
-    const std::string __grpcBindAddress() const {
-        if (result->count("grpc_bind_address"))
-            return result->operator[]("grpc_bind_address").as<std::string>();
-        return "0.0.0.0";
-    }
-    const std::string grpcBindAddress() const { return _grpcBindAddress; }
+    const std::string __grpcBindAddress() const;  // TODO: Move to CLI parser when ready
+    const std::string grpcBindAddress() const;
 
     /**
          * @brief Gets the REST port
          * 
          * @return uint64_t
          */
-    uint64_t __restPort() const {
-        return result->operator[]("rest_port").as<uint64_t>();
-    }
-    uint64_t restPort() const { return _restPort; }
+    uint64_t __restPort() const;
+    uint64_t restPort() const;
 
     /**
          * @brief Get the rest network interface address to bind to
          * 
          * @return const std::string&
          */
-    const std::string __restBindAddress() const {
-        if (result->count("rest_bind_address"))
-            return result->operator[]("rest_bind_address").as<std::string>();
-        return "0.0.0.0";
-    }
-    const std::string restBindAddress() const { return _restBindAddress; }
+    const std::string __restBindAddress() const;  // TODO: Move to CLI parser when ready
+    const std::string restBindAddress() const;
 
     /**
          * @brief Gets the gRPC workers count
          * 
          * @return uint
          */
-    uint __grpcWorkers() const {
-        return result->operator[]("grpc_workers").as<uint>();
-    }
-    uint grpcWorkers() const { return _grpcWorkers; }
+    uint __grpcWorkers() const;  // TODO: Move to CLI parser when ready
+    uint grpcWorkers() const;
 
     /**
          * @brief Gets the rest workers count
          * 
          * @return uint
          */
-    uint __restWorkers() const {
-        return result->operator[]("rest_workers").as<uint>();
-    }
-    uint restWorkers() const { return _restWorkers; }
+    uint __restWorkers() const;  // TODO: Move to CLI parser when ready
+    uint restWorkers() const;
 
     /**
          * @brief Get the model name
          * 
          * @return const std::string&
          */
-    const std::string& __modelName() const {
-        if (result->count("model_name"))
-            return result->operator[]("model_name").as<std::string>();
-        return empty;
-    }
-    const std::string& modelName() const { return _modelName; }
+    const std::string& __modelName() const;  // TODO: Move to CLI parser when ready
+    const std::string& modelName() const;
 
     /**
          * @brief Gets the model path
          * 
          * @return const std::string&
          */
-    const std::string& __modelPath() const {
-        if (result->count("model_path"))
-            return result->operator[]("model_path").as<std::string>();
-        return empty;
-    }
-    const std::string& modelPath() const { return _modelPath; }
+    const std::string& __modelPath() const;  // TODO: Move to CLI parser when ready
+    const std::string& modelPath() const;
 
     /**
          * @brief Gets the batch size
          * 
          * @return const std::string&
          */
-    const std::string& __batchSize() const {
-        if (!result->count("batch_size")) {
-            static const std::string d = "0";
-            return d;
-        }
-        return result->operator[]("batch_size").as<std::string>();
-    }
-    const std::string& batchSize() const { return _batchSize; }
+    const std::string& __batchSize() const;  // TODO: Move to CLI parser when ready
+    const std::string& batchSize() const;
 
     /**
          * @brief Get the shape
          * 
          * @return const std::string&
          */
-    const std::string& __shape() const {
-        if (result->count("shape"))
-            return result->operator[]("shape").as<std::string>();
-        return empty;
-    }
-    const std::string& shape() const { return _shape; }
+    const std::string& __shape() const;  // TODO: Move to CLI parser when ready
+    const std::string& shape() const;
 
     /**
          * @brief Get the layout
          * 
          * @return const std::string&
          */
-    const std::string& __layout() const {
-        if (result->count("layout"))
-            return result->operator[]("layout").as<std::string>();
-        return empty;
-    }
-    const std::string& layout() const { return _layout; }
+    const std::string& __layout() const;  // TODO: Move to CLI parser when ready
+    const std::string& layout() const;
 
     /**
          * @brief Get the shape
          * 
          * @return const std::string&
          */
-    const std::string& __modelVersionPolicy() const {
-        if (result->count("model_version_policy"))
-            return result->operator[]("model_version_policy").as<std::string>();
-        return empty;
-    }
-    const std::string& modelVersionPolicy() const { return _modelVersionPolicy; }
+    const std::string& __modelVersionPolicy() const;  // TODO: Move to CLI parser when ready
+    const std::string& modelVersionPolicy() const;
 
     /**
          * @brief Get the nireq
          *
          * @return uint 
          */
-    uint32_t __nireq() const {
-        if (!result->count("nireq")) {
-            return 0;
-        }
-        return result->operator[]("nireq").as<uint32_t>();
-    }
-    uint32_t nireq() const { return _nireq; }
+    uint32_t __nireq() const;
+    uint32_t nireq() const;
 
     /**
          * @brief Get the target device
          * 
          * @return const std::string& 
          */
-    const std::string& __targetDevice() const {
-        return result->operator[]("target_device").as<std::string>();
-    }
-    const std::string& targetDevice() const { return _targetDevice; }
+    const std::string& __targetDevice() const;  // TODO: Move to CLI parser when ready
+    const std::string& targetDevice() const;
 
     /**
          * @brief Get the plugin config
          * 
          * @return const std::string& 
          */
-    const std::string& __pluginConfig() const {
-        if (result->count("plugin_config"))
-            return result->operator[]("plugin_config").as<std::string>();
-        return empty;
-    }
-    const std::string& pluginConfig() const { return _pluginConfig; }
+    const std::string& __pluginConfig() const;  // TODO: Move to CLI parser when ready
+    const std::string& pluginConfig() const;
 
     /**
          * @brief Get stateful flag
          *
          * @return bool
          */
-    bool __stateful() const {
-        return result->operator[]("stateful").as<bool>();
-    }
-    bool stateful() const { return _stateful; }
+    bool __stateful() const;  // TODO: Move to CLI parser when ready
+    bool stateful() const;
 
     /**
      * @brief Get metrics enabled flag
      *
      * @return bool
      */
-    bool __metricsEnabled() const {
-        if (!result->count("metrics_enable")) {
-            return false;
-        }
-        return result->operator[]("metrics_enable").as<bool>();
-    }
-    bool metricsEnabled() const { return _metricsEnabled; }
+    bool __metricsEnabled() const;  // TODO: Move to CLI parser when ready
+    bool metricsEnabled() const;
 
     /**
         * @brief Get metrics list
         *
         * @return std::string
         */
-    std::string __metricsList() const {
-        if (!result->count("metrics_list")) {
-            return std::string("");
-        }
-        return result->operator[]("metrics_list").as<std::string>();
-    }
-    std::string metricsList() const { return _metricsList; }
+    std::string __metricsList() const;  // TODO: Move to CLI parser when ready
+    std::string metricsList() const;
 
     /**
      * @brief Get idle sequence cleanup flag
      *
      * @return uint
      */
-    bool __idleSequenceCleanup() const {
-        return result->operator[]("idle_sequence_cleanup").as<bool>();
-    }
-    bool idleSequenceCleanup() const { return _idleSequenceCleanup; }
+    bool __idleSequenceCleanup() const;  // TODO: Move to CLI parser when ready
+    bool idleSequenceCleanup() const;
 
     /**
          * @brief Get low latency transformation flag
          *
          * @return bool
          */
-    bool __lowLatencyTransformation() const {
-        return result->operator[]("low_latency_transformation").as<bool>();
-    }
-    bool lowLatencyTransformation() const { return _lowLatencyTransformation; }
+    bool __lowLatencyTransformation() const;  // TODO: Move to CLI parser when ready
+    bool lowLatencyTransformation() const;
 
     /**
      * @brief Get max number of sequences that can be processed concurrently 
      *
      * @return uint
      */
-    uint32_t __maxSequenceNumber() const;
+    uint32_t __maxSequenceNumber() const;  // TODO: Move to CLI parser when ready
     uint32_t maxSequenceNumber() const;
 
     /**
@@ -392,24 +314,16 @@ public:
         *
         * @return const std::string&
         */
-    const std::string& __logLevel() const {
-        if (result->count("log_level"))
-            return result->operator[]("log_level").as<std::string>();
-        return empty;
-    }
-    const std::string& logLevel() const { return _logLevel; }
+    const std::string& __logLevel() const;  // TODO: Move to CLI parser when ready
+    const std::string& logLevel() const;
 
     /**
         * @brief Get the log path
          *
         * @return const std::string&
         */
-    const std::string& __logPath() const {
-        if (result->count("log_path"))
-            return result->operator[]("log_path").as<std::string>();
-        return empty;
-    }
-    const std::string& logPath() const { return _logPath; }
+    const std::string& __logPath() const;  // TODO: Move to CLI parser when ready
+    const std::string& logPath() const;
 
 #ifdef MTR_ENABLED
     /**
@@ -417,12 +331,8 @@ public:
          *
         * @return const std::string&
         */
-    const std::string& __tracePath() const {
-        if (result->count("trace_path"))
-            return result->operator[]("trace_path").as<std::string>();
-        return empty;
-    }
-    const std::string& tracePath() const { return _tracePath; }
+    const std::string& __tracePath() const;  // TODO: Move to CLI parser when ready
+    const std::string& tracePath() const;
 #endif
 
     /**
@@ -430,54 +340,39 @@ public:
         *
         * @return const std::string&
         */
-    const std::string& __grpcChannelArguments() const {
-        if (result->count("grpc_channel_arguments"))
-            return result->operator[]("grpc_channel_arguments").as<std::string>();
-        return empty;
-    }
-    const std::string& grpcChannelArguments() const { return _grpcChannelArguments; }
+    const std::string& __grpcChannelArguments() const;  // TODO: Move to CLI parser when ready
+    const std::string& grpcChannelArguments() const;
 
     /**
      * @brief Get the filesystem poll wait time in seconds
      * 
      * @return uint 
      */
-    uint __filesystemPollWaitSeconds() const {
-        return result->operator[]("file_system_poll_wait_seconds").as<uint>();
-    }
-    uint filesystemPollWaitSeconds() const { return _filesystemPollWaitSeconds; }
+    uint __filesystemPollWaitSeconds() const;  // TODO: Move to CLI parser when ready
+    uint filesystemPollWaitSeconds() const;
 
     /**
      * @brief Get the sequence cleanup poll wait time in minutes
      * 
      * @return uint32_t
      */
-    uint32_t __sequenceCleanerPollWaitMinutes() const {
-        return result->operator[]("sequence_cleaner_poll_wait_minutes").as<uint32_t>();
-    }
-    uint32_t sequenceCleanerPollWaitMinutes() const { return _sequenceCleanerPollWaitMinutes; }
+    uint32_t __sequenceCleanerPollWaitMinutes() const;  // TODO: Move to CLI parser when ready
+    uint32_t sequenceCleanerPollWaitMinutes() const;
 
     /**
      * @brief Get the resources cleanup poll wait time in seconds
      * 
      * @return uint32_t
      */
-    uint32_t __resourcesCleanerPollWaitSeconds() const {
-        return result->operator[]("custom_node_resources_cleaner_interval").as<uint32_t>();
-    }
-    uint32_t resourcesCleanerPollWaitSeconds() const { return _resourcesCleanerPollWaitSeconds; }
+    uint32_t __resourcesCleanerPollWaitSeconds() const;  // TODO: Move to CLI parser when ready
+    uint32_t resourcesCleanerPollWaitSeconds() const;
 
     /**
          * @brief Model cache directory
          * 
          * @return const std::string& 
          */
-    const std::string __cacheDir() const {
-        if (result != nullptr && result->count("cache_dir")) {
-            return result->operator[]("cache_dir").as<std::string>();
-        }
-        return "";
-    }
-    const std::string cacheDir() const { return _cacheDir; }
+    const std::string __cacheDir() const;  // TODO: Move to CLI parser when ready
+    const std::string cacheDir() const;
 };
 }  // namespace ovms
