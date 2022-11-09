5623e934b94cd4624b05c17b18bd05032c08b3dd
Adrian Tobiszewski <adrian.tobiszewski@intel.com>
2022-11-09 11:09:34 +0100

    C-API Request/response structures (#1511)
    * Cppclean fixes
    
    Fixes for other places in code as the tool has issues with headers
    having pure "using" directives. Since cppclean does not accept headers with using I was forced to either increase threshold for issues count or fix issues not introduced in this MR.
    
    JIRA:CVS-95297

--- src/buffer.hpp
+++ src/buffer.hpp
@@ -1,5 +1,6 @@
+#pragma once
 //*****************************************************************************
-// Copyright 2021 Intel Corporation
+// Copyright 2022 Intel Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,45 +14,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //*****************************************************************************
-#pragma once
+#include <memory>
+#include <optional>
 
-#include <string>
-
-#include <openvino/openvino.hpp>
-
+#include "pocapi.hpp"
 namespace ovms {
 
-enum class Precision {
-    BF16,
-    FP64,
-    FP32,
-    FP16,
-    I64,
-    I32,
-    I16,
-    I8,
-    I4,
-    U64,
-    U32,
-    U16,
-    U8,
-    U4,
-    U1,
-    BOOL,
-    CUSTOM,
-    UNDEFINED,
-    DYNAMIC,
-    MIXED,
-    Q78,
-    BIN  // TODO remove BIN,Q78, DYNAMIC, CUSTOM, MIXED
+class Buffer {
+    const void* ptr;
+    size_t byteSize;
+    BufferType bufferType;
+    std::optional<uint32_t> bufferDeviceId;
+    std::unique_ptr<char[]> ownedCopy = nullptr;
+
+public:
+    Buffer(const void* ptr, size_t byteSize, BufferType bufferType = OVMS_BUFFERTYPE_CPU, std::optional<uint32_t> bufferDeviceId = std::nullopt, bool createCopy = false);
+    ~Buffer();
+    const void* data() const;
+    size_t getByteSize() const;
 };
-
-const std::string& toString(Precision precision);
-
-Precision fromString(const std::string& s);
-
-Precision ovElementTypeToOvmsPrecision(ov::element::Type_t type);
-
-ov::element::Type_t ovmsPrecisionToIE2Precision(Precision precision);
 
 }  // namespace ovms
---
+++ src/inferenceresponse.cpp
@@ -0,0 +1,61 @@
+//*****************************************************************************
+// Copyright 2022 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+#include "inferenceresponse.hpp"
+
+#include <string>
+#include <unordered_map>
+
+#include "inferenceparameter.hpp"
+#include "inferencetensor.hpp"
+#include "modelversion.hpp"
+#include "status.hpp"
+
+namespace ovms {
+InferenceResponse::InferenceResponse(const std::string& servableName, model_version_t servableVersion) :
+    servableName(servableName),
+    servableVersion(servableVersion) {}
+const std::string& InferenceResponse::getServableName() const {
+    return this->servableName;
+}
+model_version_t InferenceResponse::getServableVersion() const {
+    return this->servableVersion;
+}
+Status InferenceResponse::addOutput(const std::string& name, DataType datatype, const size_t* shape, size_t dimCount) {
+    // TODO insert tensor with wrong shape/datatype/name/dimcount
+    // TODO reuse infer response/request
+    auto [it, emplaced] = outputs.emplace(name, InferenceTensor{datatype, shape, dimCount});
+    return emplaced ? StatusCode::OK : StatusCode::DOUBLE_TENSOR_INSERT;
+}
+Status InferenceResponse::getOutput(const char* name, InferenceTensor** tensor) {
+    auto it = outputs.find(name);
+    if (outputs.end() == it) {
+        *tensor = nullptr;
+        return StatusCode::NONEXISTENT_TENSOR;
+    }
+    *tensor = &it->second;
+    return StatusCode::OK;
+}
+Status InferenceResponse::addParameter(const char* parameterName, DataType datatype, const void* data) {
+    auto [it, emplaced] = parameters.emplace(parameterName, InferenceParameter{parameterName, datatype, data});
+    return emplaced ? StatusCode::OK : StatusCode::DOUBLE_PARAMETER_INSERT;
+}
+const InferenceParameter* InferenceResponse::getParameter(const char* name) const {
+    auto it = parameters.find(name);
+    if (it != parameters.end())
+        return &it->second;
+    return nullptr;
+}
+}  // namespace ovms
