697dbfb26b3d3906d692ab8a62751fd644f248dc
Adrian Tobiszewski <adrian.tobiszewski@intel.com>
2022-11-15 14:47:34 +0100

    C-API request deserialization (#1521)
    * Cppclean lower threshold
    * Increase coverage threshold
    
    TODO:
    
    * binary inputs
    
    JIRA:CVS-95304

--- cppclean.sh
+++ cppclean.sh
@@ -45,13 +45,10 @@ fi
 if [ ${NO_WARNINGS} -gt  179 ]; then
     echo "Failed due to higher than allowed number of issues in code: ${NO_WARNINGS}"
     exit 1
-else
-    exit 0;
 fi
-if [ ${NO_WARNINGS_TEST} -gt  131 ]; then
+if [ ${NO_WARNINGS_TEST} -gt  124 ]; then
     echo "Failed due to higher than allowed number of issues in test code: ${NO_WARNINGS_TEST}"
+    cat ${CPPCLEAN_RESULTS_FILE_TEST}
     exit 1
-else
-    exit 0;
 fi
 exit 0
