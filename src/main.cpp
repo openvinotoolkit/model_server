9230106e5cc59a1c55854f6ddb792583cfe2a2c8
rasapala <rafal.a.sapala@intel.com>
2022-11-25 15:08:35 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //*****************************************************************************
-#include "server.hpp"
+#include "pocapi.hpp"
 
 using ovms::Server;
 
