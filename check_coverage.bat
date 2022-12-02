71ee2a58bb69420aebd7b59ce71489745a835fc7
atobisze <adrian.tobiszewski@intel.com>
2022-12-02 14:11:47 +0100

    Change coverage threshold

--- check_coverage.bat
+++ check_coverage.bat
@@ -5,8 +5,8 @@
 #MIN_FUNCTION_COV=87.4
 
 #Rhel
-MIN_LINES_COV=74.2
-MIN_FUNCTION_COV=75.4
+MIN_LINES_COV=74.1
+MIN_FUNCTION_COV=75.6
 
 LINES_COV=`cat genhtml/index.html | grep "headerCovTableEntry.*%" | grep -oP  ">\K(\d*.\d*) " | head -n 1`
 FUNC_COV=`cat genhtml/index.html | grep "headerCovTableEntry.*%" | grep -oP  ">\K(\d*.\d*) " | tail -n 1`
