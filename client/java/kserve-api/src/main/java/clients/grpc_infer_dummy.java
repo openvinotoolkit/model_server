38c8408e90c9bc9c826d75c8cf300aa3dd4afed8
bstrzele <bartosz.strzelecki@intel.com>
2022-12-27 13:10:45 +0100

    style fix

--- client/java/kserve-api/src/main/java/clients/grpc_infer_dummy.java
+++ client/java/kserve-api/src/main/java/clients/grpc_infer_dummy.java
@@ -16,7 +16,6 @@
 
 package clients;
 
-
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.util.Arrays;
@@ -46,30 +45,37 @@ public class grpc_infer_dummy {
 
 	public static void main(String[] args) {
 		Options opt = new Options();
-		opt.addOption("h", "help", false,"Show this help message and exit");
-		opt.addOption(Option.builder("a").longOpt("grpc_address").hasArg().desc("Specify url to grpc service. ").argName("GRPC_ADDRESS").build());
-		opt.addOption(Option.builder("p").longOpt("grpc_port").hasArg().desc("Specify port to grpc service. ").argName("GRPC_PORT").build());
-		opt.addOption(Option.builder("i").longOpt("input_name").hasArg().desc("Specify input tensor name. ").argName("INPUT_NAME").build());
-		opt.addOption(Option.builder("o").longOpt("output_name").hasArg().desc("Specify output tensor name. ").argName("OUTPUT_NAME").build());
-		opt.addOption(Option.builder("n").longOpt("model_name").hasArg().desc("Define model name, must be same as is in service").argName("MODEL_NAME").build());
-		opt.addOption(Option.builder("v").longOpt("model_version").hasArg().desc("Define model version. ").argName("MODEL_VERSION").build());
+		opt.addOption("h", "help", false, "Show this help message and exit");
+		opt.addOption(Option.builder("a").longOpt("grpc_address").hasArg().desc("Specify url to grpc service. ")
+				.argName("GRPC_ADDRESS").build());
+		opt.addOption(Option.builder("p").longOpt("grpc_port").hasArg().desc("Specify port to grpc service. ")
+				.argName("GRPC_PORT").build());
+		opt.addOption(Option.builder("i").longOpt("input_name").hasArg().desc("Specify input tensor name. ")
+				.argName("INPUT_NAME").build());
+		opt.addOption(Option.builder("o").longOpt("output_name").hasArg().desc("Specify output tensor name. ")
+				.argName("OUTPUT_NAME").build());
+		opt.addOption(Option.builder("n").longOpt("model_name").hasArg()
+				.desc("Define model name, must be same as is in service").argName("MODEL_NAME").build());
+		opt.addOption(Option.builder("v").longOpt("model_version").hasArg().desc("Define model version. ")
+				.argName("MODEL_VERSION").build());
 		CommandLineParser parser = new DefaultParser();
 		CommandLine cmd = null;
 		try {
 			cmd = parser.parse(opt, args);
-		}
-		catch (ParseException exp) {
+		} catch (ParseException exp) {
 			System.err.println("Parsing failed.  Reason: " + exp.getMessage());
 			System.exit(1);
 		}
 
-		if(cmd.hasOption("h")){
+		if (cmd.hasOption("h")) {
 			HelpFormatter formatter = new HelpFormatter();
 			formatter.printHelp("grpc_infer_dummy [OPTION]...", opt);
 			System.exit(0);
 		}
 
-		ManagedChannel channel = ManagedChannelBuilder.forAddress(cmd.getOptionValue("a", "localhost"), Integer.parseInt(cmd.getOptionValue("p", "9000"))).usePlaintext().build();
+		ManagedChannel channel = ManagedChannelBuilder
+				.forAddress(cmd.getOptionValue("a", "localhost"), Integer.parseInt(cmd.getOptionValue("p", "9000")))
+				.usePlaintext().build();
 		GRPCInferenceServiceBlockingStub grpc_stub = GRPCInferenceServiceGrpc.newBlockingStub(channel);
 
 		// Generate the request
@@ -78,7 +84,7 @@ public class grpc_infer_dummy {
 		request.setModelVersion(cmd.getOptionValue("v", ""));
 
 		// Input data
-		List<Float> lst = Arrays.asList(0f,1f,2f,3f,4f,5f,6f,7f,8f,9f);
+		List<Float> lst = Arrays.asList(0f, 1f, 2f, 3f, 4f, 5f, 6f, 7f, 8f, 9f);
 		InferTensorContents.Builder input_data = InferTensorContents.newBuilder();
 		input_data.addAllFp32Contents(lst);
 
@@ -91,7 +97,6 @@ public class grpc_infer_dummy {
 		input.addShape(10);
 		input.setContents(input_data);
 
-
 		request.addInputs(0, input);
 
 		// Populate the outputs in the inference request
@@ -99,22 +104,22 @@ public class grpc_infer_dummy {
 				.newBuilder();
 		output.setName(cmd.getOptionValue("o", "a"));
 
-
 		request.addOutputs(0, output);
 
 		ModelInferResponse response = grpc_stub.modelInfer(request.build());
 
 		// Get the response outputs
-		float[] op = toArray(response.getRawOutputContentsList().get(0).asReadOnlyByteBuffer().order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer());
+		float[] op = toArray(response.getRawOutputContentsList().get(0).asReadOnlyByteBuffer()
+				.order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer());
 
 		for (int i = 0; i < op.length; i++) {
 			System.out.println(
-				Float.toString(lst.get(i)) + " => " +
-				Float.toString(op[i]));
+					Float.toString(lst.get(i)) + " => " +
+							Float.toString(op[i]));
 		}
-		
+
 		channel.shutdownNow();
-		
+
 	}
 
 	public static float[] toArray(FloatBuffer b) {
