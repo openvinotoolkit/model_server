5c78606aea03c6be8e664c602696298c489d8dff
bstrzele <bartosz.strzelecki@intel.com>
2022-12-27 15:36:30 +0100

    added resnet example

--- client/java/kserve-api/README.md
+++ client/java/kserve-api/README.md
@@ -187,3 +187,52 @@ java -cp target/grpc-client.jar clients.grpc_infer_dummy --grpc_port 9000 --grpc
 8.0 => 9.0
 9.0 => 10.0
 ```
+
+## Examples with Resnet Model
+
+### Download the Pretrained Model
+Download the model files and store them in the `models` directory
+```Bash
+mkdir -p models/resnet/1
+curl https://storage.openvinotoolkit.org/repositories/open_model_zoo/2022.1/models_bin/2/resnet50-binary-0001/FP32-INT1/resnet50-binary-0001.bin https://storage.openvinotoolkit.org/repositories/open_model_zoo/2022.1/models_bin/2/resnet50-binary-0001/FP32-INT1/resnet50-binary-0001.xml -o models/resnet/1/resnet50-binary-0001.bin -o models/resnet/1/resnet50-binary-0001.xml
+```
+
+### Start the Model Server Container with Resnet Model
+```Bash
+docker run --rm -d -v $(pwd)/models:/models -p 9000:9000 openvino/model_server:latest --model_name resnet --model_path /models/resnet --port 9000 --layout NHWC:NCHW --plugin_config '{"PERFORMANCE_HINT":"LATENCY"}'
+```
+
+Once you finish above steps, you are ready to run the samples.
+
+### Run the Client to perform inference using gRPC API
+```Bash
+java -cp target/grpc-client.jar clients.grpc_infer_resnet --help
+usage: grpc_infer_resnet [OPTION]...
+ -a,--grpc_address <GRPC_ADDRESS>     Specify url to grpc service.
+ -h,--help                            Show this help message and exit
+ -i,--input_name <INPUT_NAME>         Specify input tensor name.
+ -imgs,--image_list <IMAGES>          Path to a file with a list of
+                                      labeled images.
+ -lbs,--labels <LABELS>               Path to a file with a list of
+                                      labels.
+ -n,--model_name <MODEL_NAME>         Define model name, must be same as
+                                      is in service
+ -p,--grpc_port <GRPC_PORT>           Specify port to grpc service.
+ -v,--model_version <MODEL_VERSION>   Define model version.
+```
+
+- Usage Example
+
+```Bash
+java -cp target/grpc-client.jar clients.grpc_infer_resnet -imgs resnet_input_images.txt -lbs resnet_labels.txt --grpc_port 9000
+../../../demos/common/static/images/airliner.jpeg classified as 404 airliner
+../../../demos/common/static/images/zebra.jpeg classified as 340 zebra
+../../../demos/common/static/images/arctic-fox.jpeg classified as 279 Arctic fox, white fox, Alopex lagopus
+../../../demos/common/static/images/bee.jpeg classified as 309 bee
+../../../demos/common/static/images/golden_retriever.jpeg classified as 207 golden retriever
+../../../demos/common/static/images/gorilla.jpeg classified as 366 gorilla, Gorilla gorilla
+../../../demos/common/static/images/magnetic_compass.jpeg classified as 635 magnetic compass
+../../../demos/common/static/images/peacock.jpeg classified as 84 peacock
+../../../demos/common/static/images/pelican.jpeg classified as 144 pelican
+../../../demos/common/static/images/snail.jpeg classified as 113 snail
+```
--- client/java/kserve-api/src/main/java/clients/grpc_infer_resnet.java
+++ client/java/kserve-api/src/main/java/clients/grpc_infer_resnet.java
@@ -16,10 +16,13 @@
 
 package clients;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -29,15 +32,16 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import com.google.protobuf.ByteString;
+
 import inference.GRPCInferenceServiceGrpc;
 import inference.GRPCInferenceServiceGrpc.GRPCInferenceServiceBlockingStub;
-import inference.GrpcPredictV2.InferTensorContents;
 import inference.GrpcPredictV2.ModelInferRequest;
 import inference.GrpcPredictV2.ModelInferResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
-public class grpc_infer_dummy {
+public class grpc_infer_resnet {
 
 	public static void main(String[] args) {
 		Options opt = new Options();
@@ -48,12 +52,15 @@ public class grpc_infer_dummy {
 				.argName("GRPC_PORT").build());
 		opt.addOption(Option.builder("i").longOpt("input_name").hasArg().desc("Specify input tensor name. ")
 				.argName("INPUT_NAME").build());
-		opt.addOption(Option.builder("o").longOpt("output_name").hasArg().desc("Specify output tensor name. ")
-				.argName("OUTPUT_NAME").build());
 		opt.addOption(Option.builder("n").longOpt("model_name").hasArg()
 				.desc("Define model name, must be same as is in service").argName("MODEL_NAME").build());
 		opt.addOption(Option.builder("v").longOpt("model_version").hasArg().desc("Define model version. ")
 				.argName("MODEL_VERSION").build());
+		opt.addOption(Option.builder("imgs").longOpt("image_list").hasArg()
+				.desc("Path to a file with a list of labeled images. ")
+				.argName("IMAGES").build());
+		opt.addOption(Option.builder("lbs").longOpt("labels").hasArg().desc("Path to a file with a list of labels. ")
+				.argName("LABELS").build());
 		CommandLineParser parser = new DefaultParser();
 		CommandLine cmd = null;
 		try {
@@ -65,8 +72,16 @@ public class grpc_infer_dummy {
 
 		if (cmd.hasOption("h")) {
 			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp("grpc_infer_dummy [OPTION]...", opt);
+			formatter.printHelp("grpc_infer_resnet [OPTION]...", opt);
 			System.exit(0);
+		}
+		if (!cmd.hasOption("imgs")) {
+			System.err.println("Missing required option: imgs");
+			System.exit(1);
+		}
+		if (!cmd.hasOption("lbs")) {
+			System.err.println("Missing required option: lbs");
+			System.exit(1);
 		}
 
 		ManagedChannel channel = ManagedChannelBuilder
@@ -79,35 +94,73 @@ public class grpc_infer_dummy {
 		request.setModelName(cmd.getOptionValue("n", "resnet"));
 		request.setModelVersion(cmd.getOptionValue("v", ""));
 
-		// Input data
-		InferTensorContents.Builder input_data = InferTensorContents.newBuilder();
-
 		// Populate the inputs in inference request
 		ModelInferRequest.InferInputTensor.Builder input = ModelInferRequest.InferInputTensor
 				.newBuilder();
-		String defaultInputName = "b";
+		String defaultInputName = "0";
 		input.setName(cmd.getOptionValue("i", defaultInputName));
 		input.setDatatype("BYTES");
 		input.addShape(1);
 
 		request.addInputs(0, input);
-		request.addRawInputContents();
 
-		ModelInferResponse response = grpc_stub.modelInfer(request.build());
-
-		// Get the response outputs
-		FloatBuffer fb = response.getRawOutputContentsList().get(0).asReadOnlyByteBuffer()
-				.order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer();
-		float[] op = new float[fb.remaining()];
-		fb.get(op);
-
-		for (int i = 0; i < op.length; i++) {
-			System.out.println(
-					Float.toString(lst.get(i)) + " => " +
-							Float.toString(op[i]));
+		List<String> labels = new ArrayList<>();
+		try (FileInputStream fis = new FileInputStream(cmd.getOptionValue("lbs"))) {
+			try (Scanner sc = new Scanner(fis)) {
+				while (sc.hasNextLine()) {
+					labels.add(sc.nextLine());
+				}
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 
+		try (FileInputStream fis = new FileInputStream(cmd.getOptionValue("imgs"))) {
+			try (Scanner sc = new Scanner(fis)) {
+				while (sc.hasNext()) {
+
+					String[] line = sc.nextLine().split(" ");
+					String fileName = line[0];
+					int label = Integer.parseInt(line[1]);
+					FileInputStream imageStream = new FileInputStream(fileName);
+					request.clearRawInputContents();
+					request.addRawInputContents(ByteString.readFrom(imageStream));
+
+					ModelInferResponse response = grpc_stub.modelInfer(request.build());
+
+					// Get the response outputs
+					FloatBuffer fb = response.getRawOutputContentsList().get(0).asReadOnlyByteBuffer()
+							.order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer();
+					float[] op = new float[fb.remaining()];
+					fb.get(op);
+					int lb = arrayMax(op);
+
+					if (lb == label)
+						System.out.format("%s classified as %d %s\n", fileName, lb, labels.get(lb));
+					else
+						System.out.format("%s classified as %d %s should be %d %s\n", fileName, lb, labels.get(lb),
+								label, labels.get(label));
+
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		channel.shutdownNow();
 
+	}
+
+	private static int arrayMax(float[] arr) {
+		float max = Float.NEGATIVE_INFINITY;
+		int in = -1;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				in = i;
+				max = arr[i];
+			}
+		}
+
+		return in;
 	}
 }
