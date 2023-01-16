077c01f2aa0fadda5d7a5b03948cb75eb125d28d
Damian Kalinowski <damian.kalinowski@intel.com>
2023-01-16 13:37:59 +0100

    licenses

--- demos/gptj_causal_lm/python/infer_ovms.py
+++ demos/gptj_causal_lm/python/infer_ovms.py
@@ -1,3 +1,18 @@
+#
+# Copyright 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import ovmsclient
 import torch
 import argparse
@@ -15,7 +30,7 @@ input_sentence = "OpenVINO Model Server is"
 inputs = tokenizer(input_sentence, return_tensors="np")
 results = client.predict(inputs=dict(inputs), model_name=args['model_name'])
 
-predicted_token_id = token = torch.argmax(torch.nn.functional.softmax(torch.Tensor(results[0,-1,:]),dim=-1),dim=-1)
+predicted_token_id = torch.argmax(torch.nn.functional.softmax(torch.Tensor(results[0,-1,:]),dim=-1),dim=-1)
 word = tokenizer.decode(predicted_token_id)
 
 print(results)
