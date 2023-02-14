4de284bd3262aba2b8bb476f8f3adf9445663cd8
rasapala <rafal.a.sapala@intel.com>
2023-02-14 19:25:09 +0100

    sdl

--- lib_search.py
+++ lib_search.py
@@ -114,6 +114,9 @@ def check_dir(start_dir):
         'bazel_rules_apple.patch',
         "pom.xml",
         "go.sum",
+        "yarn.lock",
+        "BUILD.bazel",
+        "package.json",
         ]
 
     exclude_directories = ['/dist/', 'extras/ovms-operator', 'extras/openvino-operator-openshift', 'release_files/thirdparty-licenses']
@@ -189,6 +192,9 @@ def check_func(start_dir):
         'tftext.patch',
         'zlib.LICENSE.txt',
         'bazel_rules_apple.patch',
+        'yarn.lock',
+        'BUILD.bazel',
+        'package.json',
     ]
 
     exclude_directories = ['/dist/', 'extras/ovms-operator']
