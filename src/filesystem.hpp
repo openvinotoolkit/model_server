9dad88251df82066bd6496f74bf27b27e8da74f1
bstrzele <bartosz.strzelecki@intel.com>
2023-02-21 09:46:54 +0100

    style fix

--- src/filesystem.hpp
+++ src/filesystem.hpp
@@ -35,81 +35,81 @@ class FileSystem {
 public:
     /**
      * @brief Destroy the File System object
-     *
+     * 
      */
     virtual ~FileSystem() {}
 
     /**
      * @brief Check if given path or file exists
-     *
-     * @param path
-     * @param exists
-     * @return StatusCode
+     * 
+     * @param path 
+     * @param exists 
+     * @return StatusCode 
      */
     virtual StatusCode fileExists(const std::string& path, bool* exists) = 0;
 
     /**
      * @brief Check if given path is a directory
-     *
-     * @param path
-     * @param is_dir
-     * @return StatusCode
+     * 
+     * @param path 
+     * @param is_dir 
+     * @return StatusCode 
      */
     virtual StatusCode isDirectory(const std::string& path, bool* is_dir) = 0;
 
     /**
      * @brief Get the files and directories in given directory
-     *
-     * @param path
-     * @param contents
-     * @return StatusCode
+     * 
+     * @param path 
+     * @param contents 
+     * @return StatusCode 
      */
     virtual StatusCode getDirectoryContents(const std::string& path, files_list_t* contents) = 0;
 
     /**
      * @brief Get only directories in given directory
-     *
-     * @param path
-     * @param subdirs
-     * @return StatusCode
+     * 
+     * @param path 
+     * @param subdirs 
+     * @return StatusCode 
      */
     virtual StatusCode getDirectorySubdirs(const std::string& path, files_list_t* subdirs) = 0;
 
     /**
      * @brief Get only files in given directory
-     *
-     * @param path
-     * @param files
-     * @return StatusCode
+     * 
+     * @param path 
+     * @param files 
+     * @return StatusCode 
      */
     virtual StatusCode getDirectoryFiles(const std::string& path, files_list_t* files) = 0;
 
     /**
      * @brief Read the content of the given file into a string
-     *
-     * @param path
-     * @param contents
-     * @return StatusCode
+     * 
+     * @param path 
+     * @param contents 
+     * @return StatusCode 
      */
     virtual StatusCode readTextFile(const std::string& path, std::string* contents) = 0;
 
     /**
      * @brief Download a remote directory
-     *
-     * @param path
-     * @param local_path
-     * @return StatusCode
+     * 
+     * @param path 
+     * @param local_path 
+     * @return StatusCode 
      */
     virtual StatusCode downloadFileFolder(const std::string& path, const std::string& local_path) = 0;
 
     /**
-     * @brief Download model versions
-     *
-     * @param path
-     * @param local_path
-     * @param versions
-     * @return StatusCode
-     */
+    * @brief Download model versions
+    *
+    * @param path
+    * @param local_path
+    * @param versions
+    * @return StatusCode
+    */
     virtual StatusCode downloadModelVersions(const std::string& path, std::string* local_path, const std::vector<model_version_t>& versions) = 0;
 
     /**
@@ -122,9 +122,9 @@ public:
     virtual StatusCode deleteFileFolder(const std::string& path) = 0;
     /**
      * @brief Create a Temp Path
-     *
-     * @param local_path
-     * @return StatusCode
+     * 
+     * @param local_path 
+     * @return StatusCode 
      */
     static StatusCode createTempPath(std::string* local_path) {
         std::string file_template = "/tmp/fileXXXXXX";
