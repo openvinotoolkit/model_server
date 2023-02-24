ae67887b2d49cddf8bfcecd98e01785af3e349d5
Damian Kalinowski <damian.kalinowski@intel.com>
2023-02-24 12:22:05 +0100

    style

--- src/binaryutils.cpp
+++ src/binaryutils.cpp
@@ -495,7 +495,7 @@ Status convertStringProtoToOVTensor(const tensorflow::TensorProto& src, ov::Tens
         maxStringLength = std::max(maxStringLength, str.size());
     }
     size_t width = maxStringLength + 1;
-    tensor = ov::Tensor(ov::element::Type_t::u8, ov::Shape{static_cast<long unsigned int>(src.string_val_size()), width});
+    tensor = ov::Tensor(ov::element::Type_t::u8, ov::Shape{static_cast<uint64_t>(src.string_val_size()), width});
     size_t i = 0;
     for (const auto& str : src.string_val()) {
         std::memcpy(tensor.data<unsigned char>() + i * width, reinterpret_cast<const unsigned char*>(str.c_str()), str.size());
