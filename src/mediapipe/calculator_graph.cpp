9589433d8f4f6735cbeac1035b2c77bc3321729f
Rafal Sapala <rafal.a.sapala@intel.com>
2023-02-27 17:55:04 +0100

    Dockre builds

--- src/mediapipe/calculator_graph.cpp
+++ src/mediapipe/calculator_graph.cpp
@@ -15,8 +15,8 @@
 //*****************************************************************************
 #include "calculator_graph.hpp"
 
-#include "../status.hpp"
 #include "../logging.hpp"
+#include "../status.hpp"
 #include "mediapipe/framework/calculator_graph.h"
 
 namespace ovms {
@@ -26,41 +26,41 @@ OVMSCalculatorGraph::OVMSCalculatorGraph() {
 
 absl::Status OVMSCalculatorGraph::execute() {
     //google::InitGoogleLogging("--logtostderr=1");
-      mediapipe::CalculatorGraphConfig config =
-      mediapipe::ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig>(R"pb(
-        input_stream: "in"
-        output_stream: "out"
-        node {
-          calculator: "OVMSCalculator"
-          input_stream: "in"
-          output_stream: "out1"
-        }
-        node {
-          calculator: "OVMSCalculator"
-          input_stream: "out1"
-          output_stream: "out"
-        }
-      )pb");
+    mediapipe::CalculatorGraphConfig config =
+        mediapipe::ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig>(R"pb(
+                input_stream: "in"
+                output_stream: "out"
+                node {
+                  calculator: "OVMSCalculator"
+                  input_stream: "in"
+                  output_stream: "out1"
+                }
+                node {
+                  calculator: "OVMSCalculator"
+                  input_stream: "out1"
+                  output_stream: "out"
+                }
+            )pb");
 
     mediapipe::CalculatorGraph graph;
     auto ret = graph.Initialize(config);
     LOG(INFO) << ret;
     SPDLOG_LOGGER_INFO(mediapipe_logger, "Graph initialization");
     ASSIGN_OR_RETURN(mediapipe::OutputStreamPoller poller,
-                   graph.AddOutputStreamPoller("out"));
+        graph.AddOutputStreamPoller("out"));
     MP_RETURN_IF_ERROR(graph.StartRun({}));
     SPDLOG_LOGGER_INFO(mediapipe_logger, "Graph start");
 
     for (int i = 0; i < 10; ++i) {
-    MP_RETURN_IF_ERROR(graph.AddPacketToInputStream(
-                            // TODO covnert float to proper tensor?
-        "in", mediapipe::MakePacket<float>(0.0f).At(mediapipe::Timestamp(i))));
+        MP_RETURN_IF_ERROR(graph.AddPacketToInputStream(
+            // TODO covnert float to proper tensor?
+            "in", mediapipe::MakePacket<float>(0.0f).At(mediapipe::Timestamp(i))));
     }
-    
+
     MP_RETURN_IF_ERROR(graph.CloseInputStream("in"));
 
     mediapipe::Packet packet;
-   // Get the output packets string.
+    // Get the output packets string.
     while (poller.Next(&packet)) {
         SPDLOG_LOGGER_INFO(mediapipe_logger, "Result {}", packet.Get<float>());
     }
