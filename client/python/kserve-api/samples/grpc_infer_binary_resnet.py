ed755f047634080502ef97611400ef6923d4e6e7
michalkulakowski <michal.kulakowski@intel.com>
2023-03-28 13:51:18 +0200

    fix client

--- client/python/kserve-api/samples/grpc_infer_binary_resnet.py
+++ client/python/kserve-api/samples/grpc_infer_binary_resnet.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from re import I
 import sys
 sys.path.append("../../../../demos/common/python")
 
