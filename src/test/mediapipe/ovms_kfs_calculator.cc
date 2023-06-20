ba6af1bb0a3659982af2a489e1c1415ade88c2a6
Rafal Sapala <rafal.a.sapala@intel.com>
2023-06-20 18:00:39 +0200

    Still missing calc.pb.h

--- src/test/mediapipe/ovms_kfs_calculator.cc
+++ src/test/mediapipe/ovms_kfs_calculator.cc
@@ -17,7 +17,6 @@
 #include "mediapipe/framework/calculator_framework.h"
 #include "mediapipe/framework/port/canonical_errors.h"
 #include "src/kfs_frontend/kfs_grpc_inference_service.hpp"
-#include "src/mediapipe_calculators/ovmscalculator.pb.h"
 
 // here we need to decide if we have several calculators (1 for OVMS repository, 1-N inside mediapipe)
 // for the one inside OVMS repo it makes sense to reuse code from ovms lib
