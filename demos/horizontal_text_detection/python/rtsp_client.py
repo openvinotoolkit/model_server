3896f108e3e44bb1d0911c472db75e8e0b0e96d1
bstrzele <bartosz.strzelecki@intel.com>
2023-06-27 14:08:07 +0200

    review fix

--- demos/horizontal_text_detection/python/rtsp_client.py
+++ demos/horizontal_text_detection/python/rtsp_client.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2021 Intel Corporation
+# Copyright (c) 2023 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,20 +23,23 @@ import numpy as np
 import subprocess
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--grpc_address', required=False, default='localhost', help='Specify url to grpc service')
-parser.add_argument('--grpc_port', required=False, default=9000, type=int, help='Specify port to grpc service')
+parser.add_argument('--grpc_address', required=False, default='localhost:9000', help='Specify url to grpc service')
 parser.add_argument('--input_stream', required=False, default="rtsp://localhost:8080/channel1", type=str, help='Url of input rtsp stream')
 parser.add_argument('--output_stream', required=False, default="rtsp://localhost:8080/channel2", type=str, help='Url of output rtsp stream')
-parser.add_argument('--fps', required=False, default=30, type=int, help='Framerate of input stream')
+parser.add_argument('--model_name', required=False, default="detect_text_images", type=str, help='Name of the model')
+parser.add_argument('--width', required=False, default=704, type=int, help='Width of model\'s input image')
+parser.add_argument('--height', required=False, default=704, type=int, help='Height of model\'s input image')
+parser.add_argument('--verbose', required=False, default=False, type=bool, help='Height of model\'s input image')
+parser.add_argument('--input_name', required=False, default="image", type=str, help='Name of the model\'s input')
 args = parser.parse_args()
 output_stream = args.output_stream
 
-address = "{}:{}".format(args.grpc_address, args.grpc_port)
-triton_client = grpcclient.InferenceServerClient( url=address, verbose=False)
+triton_client = grpcclient.InferenceServerClient( url=args.grpc_address, verbose=False)
 
-WIDTH = 704
-HEIGHT = 704
+WIDTH = args.width
+HEIGHT = args.height
 cap = cv2.VideoCapture(args.input_stream, cv2.CAP_FFMPEG)
+fps = cap.get(cv2.CAP_PROP_FPS)
 force_exit = False
 
 def decode(text):
@@ -56,8 +59,8 @@ def get_text(output):
     alphabet = '#1234567890abcdefghijklmnopqrstuvwxyz'
     preds = output.argmax(2)
     word = ''
-    for i in range(preds.shape[0]):
-        word += alphabet[preds[i,0]]
+    for pred in preds:
+        word += alphabet[pred[0]]
     return decode(word)
 
 def finish():
@@ -71,12 +74,9 @@ def grab_frame(cap):
         finish()
         return None
 
-    # crop and resize if original image is too small or too big for the model
     if frame.shape[0] > HEIGHT and frame.shape[1] > WIDTH:
-        frame = frame[0:HEIGHT, 0:WIDTH]
+        frame = cv2.resize(frame, (HEIGHT, WIDTH), interpolation=cv2.INTER_LINEAR)
     else:
-        res = min(frame.shape[0], frame.shape[1])
-        frame = frame[0:res, 0:res]
         frame = cv2.resize(frame, (HEIGHT, WIDTH), interpolation=cv2.INTER_AREA)
     return frame
 
@@ -86,14 +86,16 @@ def postprocess(frame, result):
         text_coordinates = result.as_numpy('text_coordinates')
         for i in range(len(texts)):
             text = get_text(texts[i])
-            x_min = text_coordinates[i][0][0]
-            y_min = text_coordinates[i][0][1]
-            x_max = text_coordinates[i][0][0] + text_coordinates[i][0][2]
-            y_max = text_coordinates[i][0][1] + text_coordinates[i][0][3]
+            coords = text_coordinates[i][0]
+            x_min = coords[0]
+            y_min = coords[1]
+            x_max = coords[0] + coords[2]
+            y_max = coords[1] + coords[3]
             frame = cv2.rectangle(frame, (x_min,y_min), (x_max,y_max), (0,0,255), 1)
             cv2.putText(frame, text, (x_min, y_min-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-            print(text)
-            print((x_min,y_min), (x_max,y_max))
+            if args.verbose:
+                print(text)
+                print((x_min,y_min), (x_max,y_max))
     return frame
 
 
@@ -116,18 +118,19 @@ ffmpeg_process = open_ffmpeg_stream_process()
 
 if grab_frame(cap) is None:
     force_exit = True
-
 while not force_exit:
-    time.sleep(1/args.fps)
+    time.sleep(1/fps)
     frame = grab_frame(cap)
-    inputs=[grpcclient.InferInput( "image", [1,WIDTH,HEIGHT,3], "FP32")]
-    inputs[0].set_data_from_numpy(np.array([frame], dtype=np.float32))
-    triton_client.async_infer(
-        model_name="detect_text_images",
-        callback=partial(callback, frame),
-        inputs=inputs)
+    if frame is not None:
+        inputs=[grpcclient.InferInput( args.input_name, [1,WIDTH,HEIGHT,3], "FP32")]
+        inputs[0].set_data_from_numpy(np.array([frame], dtype=np.float32))
+        triton_client.async_infer(
+            model_name=args.model_name,
+            callback=partial(callback, frame),
+            inputs=inputs)
 
 finish()
 # When everything done, release the capture
 cap.release()
+ffmpeg_process.kill()
 print(f"Finished.")
