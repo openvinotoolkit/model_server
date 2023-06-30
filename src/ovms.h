7bec7c5282bf2aec01052e4a264800f6039c7282
bstrzele <bartosz.strzelecki@intel.com>
2023-06-30 12:48:04 +0200

    minor fix

--- src/ovms.h
+++ src/ovms.h
@@ -14,7 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //*****************************************************************************
-#include <cstdint>
 
 #include <stddef.h>
 #include <stdint.h>  //  For precise data types
