6cf3250850a92fc23214d8e9455819d2ed48eafd
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2023-07-14 19:31:20 +0200

    add create_package script

---
+++ create_package.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This script should be used inside the build image to create a binary package based on the compiled artefacts
+
+
+mkdir -vp /ovms_release/bin && \
+mkdir -vp /ovms_release/deps && \
+mkdir -vp /ovms_release/lib && \
+mkdir -vp /ovms_release/lib/hddl/config && \
+mkdir -vp /ovms_release/lib/custom_nodes
+
+if [ -d /ovms/src/custom_nodes/lib/${BASE_OS} ] ; then cp /ovms/src/custom_nodes/lib/${BASE_OS}/*.so /ovms_release/lib/custom_nodes/ ; fi
+if [ -d /ovms/src/custom_nodes/tokenizer/lib/${BASE_OS} ] ; then cp /ovms/src/custom_nodes/tokenizer/lib/${BASE_OS}/*.so /ovms_release/lib/custom_nodes/ ; fi
+if [ "$sentencepiece" == "1" ]; then cp -v /openvino_contrib/modules/custom_operations/user_ie_extensions/user_ie_extensions/libuser_ov_extensions.so /ovms_release/lib/ ; fi
+if [ "$NVIDIA" == "1" ]; then cp -v /openvino/bin/intel64/Release/libopenvino_nvidia_gpu_plugin.so /ovms_release/lib/ ; fi
+if [ "$NVIDIA" == "1" ]; then echo '<ie><plugins><plugin location="libopenvino_nvidia_gpu_plugin.so" name="NVIDIA"></plugin></plugins></ie>' > /ovms_release/lib/plugins.xml ; fi
+if [ "$ov_use_binary" == "1" ] ; then cp /opt/intel/openvino/install_dependencies/* /ovms_release/deps/ ; fi
+if [ "$ov_use_binary" == "1" ] ; then rm -vrf /ovms_release/deps/*-devel-* ; fi
+
+find /ovms/bazel-out/k8-*/bin -iname '*.so*' -exec cp -v {} /ovms_release/lib/ \;
+cd /ovms_release/lib/ ; rm -f libazurestorage.so.* ; ln -s libazurestorage.so libazurestorage.so.7 ;ln -s libazurestorage.so libazurestorage.so.7.5
+cd /ovms_release/lib/ ; rm -f libcpprest.so.2.10 ; ln -s libcpprest.so libcpprest.so.2.10
+rm -f /ovms_release/lib/libssl.so
+rm -f /ovms_release/lib/libsampleloader*
+rm -f /ovms_release/lib/lib_node*
+rm -f /ovms_release/lib/libcustom_node*
+
+# Remove coverage libaries
+if [ -f /ovms_release/lib/libjava.so ] ; then cd /ovms_release/lib/ \
+    rm -rf  libatk-wrapper.so libattach.so libawt_headless.so libawt.so libawt_xawt.so libdt_socket.so \
+	libextnet.so libfontmanager.so libinstrument.so libj2gss.so libj2pcsc.so libj2pkcs11.so libjaas.so \
+	libjavajpeg.so libjava.so libjawt.so libjdwp.so libjimage.so libjli.so libjsig.so libjsound.so libjvm.so \
+	liblcms.so libmanagement_agent.so libmanagement_ext.so libmanagement.so libmlib_image.so libnet.so libnio.so \
+	libprefs.so librmi.so libsaproc.so libsctp.so libsplashscreen.so libsunec.so libsystemconf.so libunpack.so libverify.so libzip.so ; \
+fi
+
+# Remove capi temp libraries
+if [ -f /ovms_release/lib/libsrc_Slibovms_Ushared.so ] ; then \
+    rm -rf  /ovms_release/lib/libsrc_Slibovms_Ushared.so \
+	/ovms_release/lib/libprediction_service_proto.so-2.params \
+	/ovms_release/lib/libovms_shared.so-2.params ; \
+fi
+
+find /opt/intel/openvino/runtime/lib/intel64/ -iname '*.so*' -exec cp -vP {} /ovms_release/lib/ \;
+find /opt/intel/openvino/runtime/lib/intel64/ -iname '*.mvcmd*' -exec cp -v {} /ovms_release/lib/ \;
+if [ -d /opt/intel/openvino/runtime/3rdparty ] ; then find /opt/intel/openvino/runtime/3rdparty/ -iname '*.so*' -exec cp -vP {} /ovms_release/lib/ \;; fi
+find /opt/opencv/lib/ -iname '*.so*' -exec cp -vP {} /ovms_release/lib/ \;
+cp /opt/opencv/share/licenses/opencv4/* /ovms/release_files/thirdparty-licenses/
+if [ "$ov_use_binary" == "1" ] ; then cp /opt/intel/openvino/docs/licensing/EULA.txt /ovms/release_files/thirdparty-licenses/openvino.LICENSE.txt; fi
+if [ "$ov_use_binary" == "0" ] ; then cp /openvino/LICENSE /ovms/release_files/thirdparty-licenses/openvino.LICENSE.txt; fi
+if [ "$BASE_OS" == "redhat" ] ; then cp /usr/lib64/libpugixml.so* /ovms_release/lib/ ; fi
+
+if [ "$FUZZER_BUILD" == "0" ]; then find /ovms/bazel-bin/src -name 'ovms' -type f -exec cp -v {} /ovms_release/bin \; ; fi;
+cd /ovms_release/bin
+if [ "$FUZZER_BUILD" == "0" ]; then patchelf --remove-rpath ./ovms && patchelf --set-rpath '$ORIGIN/../lib/' ./ovms; fi;
+find /ovms_release/lib/ -iname '*.so*' -exec patchelf --debug --remove-rpath  {}  \;
+find /ovms_release/lib/ -iname '*.so*' -exec patchelf --debug --set-rpath '$ORIGIN/../lib' {} \;
+
+cd /ovms
+cp -v /ovms/release_files/LICENSE /ovms_release/
+cp -rv /ovms/release_files/thirdparty-licenses /ovms_release/
+mkdir -vp /ovms_release/include && cp /ovms/src/ovms.h /ovms_release/include
+ls -lahR /ovms_release/
+
+find /ovms_release/lib/ -iname '*.so*' -type f -exec patchelf --remove-rpath  {}  \;
+find /ovms_release/lib/ -iname '*.so*' -type f -exec patchelf --set-rpath '$ORIGIN/../lib' {} \;
+
+cd /
+tar czf ovms.tar.gz --transform 's/ovms_release/ovms/' /ovms_release/
+sha256sum ovms.tar.gz > ovms.tar.gz.sha256 && \
+tar cJf ovms.tar.xz --transform 's/ovms_release/ovms/' /ovms_release/
+sha256sum ovms.tar.xz > ovms.tar.xz.sha256
+cd /ovms_release
+ls -l
+
+
+
+
+
+
+
+
