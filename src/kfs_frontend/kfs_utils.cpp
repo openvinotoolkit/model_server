7036f098307f2997c079a2dedcb00e1c49921ebc
michalkulakowski <michal.kulakowski@intel.com>
2023-07-31 09:27:03 +0200

    Mediapipe::ImageFrame support

--- src/kfs_frontend/kfs_utils.cpp
+++ src/kfs_frontend/kfs_utils.cpp
@@ -73,6 +73,24 @@ size_t KFSDataTypeSize(const KFSDataType& datatype) {
     return it->second;
 }
 
+Status convertKFSDataTypeToMatFormat(const KFSDataType& datatype, size_t& matFormat) {
+    static std::unordered_map<KFSDataType, size_t> datatypeFormatMap{
+        {"UINT8", CV_8U},
+        {"UINT16", CV_16U},
+        {"INT8", CV_8U},
+        {"INT16", CV_16U},
+        {"INT32", CV_16U},
+        {"FP16", CV_16F},
+        {"FP32", CV_32F},
+        {"FP64", CV_64F}};
+    auto it = datatypeFormatMap.find(datatype);
+    if (it == datatypeFormatMap.end()) {
+        return StatusCode::INTERNAL_ERROR;
+    }
+    matFormat = it->second;
+    return StatusCode::OK;
+}
+
 const KFSDataType& ovmsPrecisionToKFSPrecision(Precision precision) {
     static std::unordered_map<Precision, KFSDataType> precisionMap{
         {Precision::FP64, "FP64"},
--- src/kfs_frontend/kfs_utils.hpp
+++ src/kfs_frontend/kfs_utils.hpp
@@ -27,6 +27,7 @@ Precision KFSPrecisionToOvmsPrecision(const KFSDataType& s);
 const KFSDataType& ovmsPrecisionToKFSPrecision(Precision precision);
 
 size_t KFSDataTypeSize(const KFSDataType& datatype);
+Status convertKFSDataTypeToMatFormat(const KFSDataType& datatype, size_t& matFormat);
 Status prepareConsolidatedTensorImpl(KFSResponse* response, const std::string& name, ov::element::Type_t precision, const ov::Shape& shape, char*& tensorOut, size_t size);
 const std::string& getRequestServableName(const KFSRequest& request);
 Status isNativeFileFormatUsed(const KFSRequest& request, const std::string& name, bool& nativeFileFormatUsed);
