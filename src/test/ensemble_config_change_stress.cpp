79a4fcaeff858947547ccd0cd39c86ff010ebe15
bstrzele <bartosz.strzelecki@intel.com>
2023-08-25 12:49:06 +0200

    minor fix

--- src/test/ensemble_config_change_stress.cpp
+++ src/test/ensemble_config_change_stress.cpp
@@ -13,8 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //*****************************************************************************
-#include <cstddef>
-#include <iterator>
 #include <regex>
 
 #include <gmock/gmock.h>
