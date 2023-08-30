967847f1ff83ff314f0861ef1e35516f0affb87a
Rafal Sapala <rafal.a.sapala@intel.com>
2023-08-30 10:50:21 +0200

    Enable test

--- src/test/mediapipeflow_test.cpp
+++ src/test/mediapipeflow_test.cpp
@@ -122,7 +122,7 @@ TEST_F(MediapipeTFTest, Passthrough) {
     size_t dummysInTheGraph = 0;
     checkDummyResponse("out", requestData, request, response, dummysInTheGraph, 1, modelName);
 }
-TEST_F(MediapipeTFTest, DISABLED_DummyInfer) {
+TEST_F(MediapipeTFTest, DummyInfer) {
     const ovms::Module* grpcModule = server.getModule(ovms::GRPC_SERVER_MODULE_NAME);
     KFSInferenceServiceImpl& impl = dynamic_cast<const ovms::GRPCServerModule*>(grpcModule)->getKFSGrpcImpl();
     ::KFSRequest request;
