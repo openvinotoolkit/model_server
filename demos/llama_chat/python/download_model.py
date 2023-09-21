86b512f89a121e3c8a426f1afffad507617b05de
Damian Kalinowski <damian.kalinowski@intel.com>
2023-09-21 11:21:49 +0200

    v1

---
+++ demos/llama_chat/python/download_model.py
@@ -0,0 +1,29 @@
+import argparse
+
+from optimum.intel.openvino import OVModelForCausalLM
+from huggingface_hub import login, whoami
+
+parser = argparse.ArgumentParser(description='Download script for llama-7b-chat')
+
+parser.add_argument('--token', required=False, help='Huggingfaces login token')
+args = parser.parse_args()
+
+try:
+    whoami()
+    print('Authorization token already provided')
+except OSError:
+    assert args.token is not None, "need to provide HF authorization --token param"
+    login(args.token)
+
+print('Downloading and converting...')
+ov_config = {'PERFORMANCE_HINT': 'LATENCY', 'NUM_STREAMS': '1'}
+ov_model = OVModelForCausalLM.from_pretrained(
+    'meta-llama/Llama-2-7b-hf',
+    export=True,
+    device='CPU',
+    compile=False,
+    ov_config=ov_config)
+
+print('Saving to ./models ...')
+ov_model.save_pretrained('./models/llama-2-7b-hf/1/')
+print('Done.')
