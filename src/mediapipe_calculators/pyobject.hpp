88ba80b94372b09decd5169096fb09490c583e96
Rafal Sapala <rafal.a.sapala@intel.com>
2023-09-27 16:34:40 +0200

    Cleanup

--- src/mediapipe_calculators/pyobject.hpp
+++ src/mediapipe_calculators/pyobject.hpp
@@ -27,42 +27,11 @@
 
 namespace py = pybind11;
 class PYOBJECT {
-    // --- These we get from the request
-
-    void* data;
-    std::vector<py::ssize_t> shape;
-    // Can be one of predefined types (like int8, float32 etc.) or totally custom like numpy (for example "<U83")
-    std::string datatype;
-
-    // ---
-
-    // These we compute if possible
-    py::ssize_t itemsize;
-    py::ssize_t ndim;
-    std::vector<py::ssize_t> strides;
-
-    // ---
-
-    // User interface from Python code
 public:
     // Try to create using other Python object that exposes buffer protocol (like numpy array, pytorch tensor etc.)
     PYOBJECT(py::object object);
 
-    // True if object implements protocol buffer. Example when it can be false:
-    // Irregular (non-padded) string or other binary data - impossible to determine itemsize and compute strides.
-    // Will return True if datatype is predefined type and False for custom type
-    bool implements_buffer();
-
-    // Developer interface
 private:
-    // itemsize deduced from datatype if predefined
-    // ndim deduced from shape
-    // strides computed from shape and itemsize
-    // size might be useful for custom data types handling
-    PYOBJECT(void* data, size_t size, std::vector<py::ssize_t> shape, std::string datatype);
-    // TODO: fix this -> PYOBJECT(&ov::Tensor);
-    ov::Tensor converToOVTensor();
-    // TODO: fix this -> bool packToResponse(T& response);
 };
 
 #endif  // SRC_MEDIAPIPE_CALCULATORS_PYOBJECT_HPP_
--- src/mediapipe_calculators/python_backend_calculator.cc
+++ src/mediapipe_calculators/python_backend_calculator.cc
@@ -28,107 +28,22 @@ using namespace py::literals;
 namespace mediapipe {
 
 class PythonBackendCalculator : public CalculatorBase {
-    py::object pyobjectClass;
-
 public:
     static absl::Status GetContract(CalculatorContract* cc) {
-        LOG(ERROR) << "PythonBackendCalculator::GetContract";
-        cc->Inputs().Index(0).Set<ov::Tensor>();
-        cc->Outputs().Index(0).Set<ov::Tensor>();
-        cc->InputSidePackets().Tag("PYOBJECT").Set<py::object>();
         return absl::OkStatus();
     }
 
     absl::Status Close(CalculatorContext* cc) final {
-        LOG(ERROR) << "PythonBackendCalculator::Close";
         return absl::OkStatus();
     }
     absl::Status Open(CalculatorContext* cc) final {
-        LOG(ERROR) << "PythonBackendCalculator::Open";
-        LOG(ERROR) << "Python node name:" << cc->NodeName();
-        pyobjectClass = cc->InputSidePackets().Tag("PYOBJECT").Get<py::object>();
         return absl::OkStatus();
     }
 
     absl::Status Process(CalculatorContext* cc) final {
-        LOG(ERROR) << "PythonBackendCalculator::Process";
-
-        py::gil_scoped_acquire acquire;
-        py::print("PYTHON: Acquired GIL");
-        // pyobjectClass.attr("execute")();
-        // Read input from the stream
-        PYOBJECT inputs = cc->Inputs().Index(0).Get<PYOBJECT>();
-
-        // py::iterator it = pyobjectClass.attr("execute")(inputs);
-        // while (it != py::iterator::sentinel()) {
-        //    auto result = cast<PYOBJECT>(*it);
-        //    cc->Outputs().Index(0).Add(result, myTimestamp);
-        //    ++it;
-        // }
-
-        ov::Tensor in_tensor = cc->Inputs().Index(0).Get<ov::Tensor>();
-
-        auto* out_tensor = new ov::Tensor(in_tensor.get_element_type(), in_tensor.get_shape());
-        std::memcpy(out_tensor->data(), in_tensor.data(), in_tensor.get_byte_size());
-
-        for (int i = 0; i < 10; i++) {
-            float* pV = ((float*)out_tensor->data()) + i;
-            *pV += 1.0f;
-        }
-
-        py::print("PYTHON: Released GIL");
         return absl::OkStatus();
     }
 };
 
 REGISTER_CALCULATOR(PythonBackendCalculator);
 }  // namespace mediapipe
-
-/*
-class PythonNodeState;
-class PythonSession
-class PYOBJECT;
-
-class PythonExecutorCalculator : public CalculatorBase {
-    PythonNodeState pythonNodeState;
-public:
-    static absl::Status GetContract(CalculatorContract* cc) {
-        ...
-        cc->Inputs().Index(0).Set<PYOBJECT>();
-        cc->Outputs().Index(0).Set<PYOBJECT>();
-        cc->InputSidePackets().Tag("PYSESSION").Set<PythonSession>();
-        ...
-        return absl::OkStatus();
-    }
-
-    absl::Status Close(CalculatorContext* cc) final {
-        ...
-        return absl::OkStatus();
-    }
-    absl::Status Open(CalculatorContext* cc) final {
-        ...
-        auto pythonSession = cc->InputSidePackets().Tag("PYSESSION").Get<PythonSession>();
-        this->pythonNodeState = pythonSession[cc->NodeName()];
-        ...
-        return absl::OkStatus();
-    }
-
-    absl::Status Process(CalculatorContext* cc) final {
-        ...
-        py::gil_scoped_acquire acquire;
-        ...
-        // Read input from the stream
-        PYOBJECT inputs = cc->Inputs().Index(0).Get<PYOBJECT>();
-        // Checks, translations etc.
-        ...
-        // Execute user code
-        outputs_raw = pythonNodeState.attr("execute")(inputs);
-        // Convert results to PYOBJECT
-        PYOBJECT outputs = convertToPYOBJECT(outputs_raw)
-        // Push outputs down the graph
-        cc->Outputs().Index(0).Add(outputs, cc->InputTimestamp());
-        ...
-        return absl::OkStatus();
-    }
-};
-*/
