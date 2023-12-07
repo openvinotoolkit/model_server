27b2255b984169ee89de25bed40b35597cdef888
michalkulakowski <michal.kulakowski@intel.com>
2023-12-07 12:46:33 +0100

    Extend ovms::Config class with information about Python startup

--- src/capi_frontend/capi.cpp
+++ src/capi_frontend/capi.cpp
@@ -273,7 +273,7 @@ DLL_PUBLIC OVMS_Status* OVMS_ServerStartFromConfigurationFile(OVMS_Server* serve
     ovms::Server* srv = reinterpret_cast<ovms::Server*>(server);
     ovms::ServerSettingsImpl* serverSettings = reinterpret_cast<ovms::ServerSettingsImpl*>(server_settings);
     ovms::ModelsSettingsImpl* modelsSettings = reinterpret_cast<ovms::ModelsSettingsImpl*>(models_settings);
-    auto res = srv->start(serverSettings, modelsSettings, false);
+    auto res = srv->start(serverSettings, modelsSettings);
     if (res.ok())
         return nullptr;
     return reinterpret_cast<OVMS_Status*>(new ovms::Status(res));
