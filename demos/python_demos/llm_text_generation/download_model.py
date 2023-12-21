06c4a573c9cb9b4d93f7c46a3643bf7993ab7e82
Trawinski, Dariusz <dariusz.trawinski@intel.com>
2023-12-21 08:01:50 +0100

    llm text generation with a range of models (#2208)
    Co-authored-by: Ryan Loney <ryan.loney@intel.com>
    Co-authored-by: ngrozae <104074686+ngrozae@users.noreply.github.com>
    Co-authored-by: Miłosz Żeglarski <milosz.zeglarski@intel.com>

--- demos/python_demos/llm_text_generation/download_model.py
+++ demos/python_demos/llm_text_generation/download_model.py
@@ -1,33 +1,62 @@
-#
-# Copyright (c) 2023 Intel Corporation
+#*****************************************************************************
+# Copyright 2023 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-from optimum.intel.openvino import OVModelForCausalLM
+#*****************************************************************************
 
-model_name = 'togethercomputer/RedPajama-INCITE-Chat-3B-v1'
-#model_name = 'meta-llama/Llama-2-7b-chat-hf'  # Edit for other models, for example llama
-MODEL_PATH = './model'
+from optimum.intel.openvino import OVModelForCausalLM
+from transformers import AutoTokenizer
+from servable_stream.config import SUPPORTED_LLM_MODELS
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Script to download LLM model based on https://github.com/openvinotoolkit/openvino_notebooks/blob/main/notebooks/254-llm-chatbot')
+
+supported_models_list = []
+for key, _ in SUPPORTED_LLM_MODELS.items() :
+    supported_models_list.append(key)
+
+parser.add_argument('--model',
+                    required=True,
+                    choices=supported_models_list,
+                    help='Select the LLM model out of supported list')
+args = vars(parser.parse_args())
+
+SELECTED_MODEL = args['model']
+
+model_configuration = SUPPORTED_LLM_MODELS[SELECTED_MODEL]
+
+model_id = model_configuration["model_id"]
+
+MODEL_PATH = "./" + SELECTED_MODEL
 OV_CONFIG = {'PERFORMANCE_HINT': 'LATENCY', 'NUM_STREAMS': '1'}
 
 print('Downloading and converting...')
 ov_model = OVModelForCausalLM.from_pretrained(
-    model_name,
+    model_id,
     export=True,
     device='CPU',
     compile=False,
+    trust_remote_code=True,
     ov_config=OV_CONFIG)
 
-print(f'Saving to {MODEL_PATH} ...')
+print(f'Saving model to {MODEL_PATH} ...')
 ov_model.save_pretrained(MODEL_PATH)
 print('Done.')
+
+print(f'Downloading tokenizer to {MODEL_PATH} ...')
+tok = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
+print(f'Saving tokenizer to {MODEL_PATH} ...')
+tok.save_pretrained(MODEL_PATH)
+print('Done.')
+
