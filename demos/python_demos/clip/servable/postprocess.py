2f72ea3e024430ca18ee60ebcf694e58cc0133a8
Rafal Sapala <rafal.a.sapala@intel.com>
2024-01-18 13:56:51 +0100

    Update demos/python_demos/clip/servable/postprocess.py
    Co-authored-by: Miłosz Żeglarski <milosz.zeglarski@intel.com>

--- demos/python_demos/clip/servable/postprocess.py
+++ demos/python_demos/clip/servable/postprocess.py
@@ -24,7 +24,7 @@ class OvmsPythonModel:
         pass
     
     def execute(self, inputs: list):
-        ov_logits_per_image = np.asarray(inputs[0].data)
+        ov_logits_per_image = np.array(inputs[0])
         probs = softmax(ov_logits_per_image, axis=1)[0]
         input_labels = np.frombuffer(inputs[1].data, inputs[1].datatype)
 
