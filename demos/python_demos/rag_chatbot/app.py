705d9e1d7077d3587424df074ec420f72a16e980
Damian Kalinowski <damian.kalinowski@intel.com>
2024-02-14 16:19:36 +0100

    demo

---
+++ demos/python_demos/rag_chatbot/app.py
@@ -0,0 +1,26 @@
+import gradio as gr
+import argparse
+
+from streamer import OvmsStreamer
+
+
+parser = argparse.ArgumentParser(description='Script to download LLM model based on https://github.com/openvinotoolkit/openvino_notebooks/blob/main/notebooks/254-llm-chatbot')
+
+parser.add_argument('--web_url',
+                    required=True,
+                    help='Web server URL')
+parser.add_argument('--ovms_url',
+                    required=True,
+                    help='OVMS server URL')
+args = parser.parse_args()
+
+
+def callback(message, history):
+    streamer = OvmsStreamer(args.ovms_url.split(':')[0], int(args.ovms_url.split(':')[1]))
+    streamer.request_async(message)
+    result = ""
+    for completion in streamer:
+        result += completion
+        yield result
+
+gr.ChatInterface(callback).launch(server_name=args.web_url.split(':')[0], server_port=int(args.web_url.split(':')[1]))
---
+++ demos/python_demos/rag_chatbot/streamer.py
@@ -0,0 +1,58 @@
+import tritonclient.grpc as grpcclient
+from tritonclient.utils import serialize_byte_tensor
+import numpy as np
+import queue
+
+
+def serialize_prompts(prompts):
+    infer_input = grpcclient.InferInput("pre_prompt", [len(prompts)], "BYTES")
+    if len(prompts) == 1:
+        # Single batch serialized directly as bytes
+        infer_input._raw_content = prompts[0].encode()
+        return infer_input
+    # Multi batch serialized in tritonclient 4byte len format
+    infer_input._raw_content = serialize_byte_tensor(
+        np.array(prompts, dtype=np.object_)).item()
+    return infer_input
+
+
+channel_args = [
+    # Do not drop the connection for long workloads
+    ("grpc.http2.max_pings_without_data", 0),
+]
+
+
+class OvmsStreamer:
+    def __init__(self, server_name, server_port):
+        self.server_name = server_name
+        self.server_port = server_port
+        self.client = grpcclient.InferenceServerClient(f"{server_name}:{server_port}", channel_args=channel_args)
+        self.result_queue = queue.Queue()
+
+    def request_async(self, question):
+        def callback(result, error):
+            if error:
+                # raise error
+                self.result_queue.put(0)
+            if result.as_numpy('end_signal') is not None:
+                self.result_queue.put(0)
+            elif result.as_numpy('completion') is not None:
+                self.result_queue.put(result.as_numpy('completion').tobytes().decode())
+            else:
+                assert False, "unexpected output"
+        self.client.start_stream(callback=callback)
+        infer_input = serialize_prompts([question])
+        self.client.async_stream_infer(model_name="python_model", inputs=[infer_input])
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = self.result_queue.get()
+        if isinstance(item, int):
+            self.client.stop_stream()
+            raise StopIteration
+        if not isinstance(item, str):
+            assert False, "must be int or str"
+        return item
