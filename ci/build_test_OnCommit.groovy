44a753e945a212bd0ed6f35b3bed388bfe75ae7d
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2024-03-14 23:50:26 +0100

    remove duplicated scm

--- ci/build_test_OnCommit.groovy
+++ ci/build_test_OnCommit.groovy
@@ -9,19 +9,16 @@ pipeline {
         stage('Configure') {
           steps {
             script {
-              dir ('model_server'){
-                checkout scm
-                shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
-                echo shortCommit
-                echo sh(script: 'env|sort', returnStdout: true)
-                if (env.CHANGE_ID){
-                  sh 'git fetch origin ${CHANGE_TARGET}'
-                  def git_diff = sh (script: "git diff --name-only \$(git merge-base FETCH_HEAD HEAD)", returnStdout: true).trim()
-                  println("git diff:\n${git_diff}")
-                  def matched = (git_diff =~ /src|third_party|(\n|^)Dockerfile|(\n|^)Makefile|\.cpp|\.h|BUILD|WORKSPACE|(\n|^)rununittest\.sh/)
-                  if (matched){
-                    image_build_needed = "true"
-                  }
+              shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+              echo shortCommit
+              echo sh(script: 'env|sort', returnStdout: true)
+              if (env.CHANGE_ID){
+                sh 'git fetch origin ${CHANGE_TARGET}'
+                def git_diff = sh (script: "git diff --name-only \$(git merge-base FETCH_HEAD HEAD)", returnStdout: true).trim()
+                println("git diff:\n${git_diff}")
+                def matched = (git_diff =~ /src|third_party|(\n|^)Dockerfile|(\n|^)Makefile|\.cpp|\.h|BUILD|WORKSPACE|(\n|^)rununittest\.sh/)
+                if (matched){
+                  image_build_needed = "true"
                 }
               }
             }
@@ -68,7 +65,7 @@ pipeline {
             stage("Internal tests") {
               steps {
                 script {
-                  dir ('tests'){ 
+                  dir ('internal_tests'){ 
                     checkout scmGit(
                     branches: [[name: 'develop']],
                     userRemoteConfigs: [[credentialsId: 'workflow-lab',
