700f48ca6fa2e3bf95aac50a290b4be964e7a15e
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2024-03-19 11:29:04 +0100

    review changes

--- rununittest.sh
+++ rununittest.sh
@@ -24,6 +24,7 @@ ${debug_bazel_flags} \
 --test_output=streamed \
 --test_env PYTHONPATH=${PYTHONPATH}"
 test_success_procedure() {
+    grep -a " ms \| ms)" ${TEST_LOG}
     tail -50 ${TEST_LOG}
 }
 generate_coverage_report() {
