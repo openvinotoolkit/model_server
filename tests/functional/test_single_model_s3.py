3d34b77baebb93aa79662e95dc864710db707178
ngrozae <104074686+ngrozae@users.noreply.github.com>
2024-05-17 11:35:39 +0200

    CVS-137992_fix_tests_deadline_exceeded_dg2 (#2440)
    * CVS-137992_fix_deadline_exceeded_dg2
    
    * add retry for get_model_metadata_request
    
    * add get_model_metadata function
    
    * fix test names
    
    * increase timeout for GetModelStatus

--- tests/functional/test_single_model_s3.py
+++ tests/functional/test_single_model_s3.py
@@ -17,10 +17,10 @@
 import pytest
 import numpy as np
 from constants import MODEL_SERVICE, ERROR_SHAPE, TARGET_DEVICE_MYRIAD, TARGET_DEVICE_CUDA, NOT_TO_BE_REPORTED_IF_SKIPPED
-from config import target_device, skip_nginx_test
+from config import skip_nginx_test
 from conftest import devices_not_supported_for_test
 from model.models_information import Resnet
-from utils.grpc import create_channel, infer, get_model_metadata, model_metadata_response, \
+from utils.grpc import create_channel, infer, get_model_metadata_request, get_model_metadata, model_metadata_response, \
     get_model_status
 import logging
 from utils.models_utils import ModelVersionState, ErrorCode, ERROR_MESSAGE
@@ -70,8 +70,8 @@ class TestSingleModelInferenceS3:
 
         expected_input_metadata = {Resnet.input_name: {'dtype': 1, 'shape': list(Resnet.input_shape)}}
         expected_output_metadata = {Resnet.output_name: {'dtype': 1, 'shape': list(Resnet.output_shape)}}
-        request = get_model_metadata(model_name=Resnet.name)
-        response = stub.GetModelMetadata(request, 10)
+        request = get_model_metadata_request(model_name=Resnet.name)
+        response = get_model_metadata(stub, request)
         input_metadata, output_metadata = model_metadata_response(response=response)
         logger.info("Input metadata: {}".format(input_metadata))
         logger.info("Output metadata: {}".format(output_metadata))
@@ -85,7 +85,7 @@ class TestSingleModelInferenceS3:
         _, ports = start_server_single_model_from_minio
         stub = create_channel(port=ports["grpc_port"], service=MODEL_SERVICE)
         request = get_model_status(model_name=Resnet.name)
-        response = stub.GetModelStatus(request, 10)
+        response = stub.GetModelStatus(request, 60)
         versions_statuses = response.model_version_status
         version_status = versions_statuses[0]
         assert version_status.version == 1
