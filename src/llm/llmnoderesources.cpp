96b4ec69f26786a029025f9a0d489b0cdd272d65
mzegla <milosz.zeglarski@intel.com>
2024-06-10 11:08:52 +0200

    init

--- src/llm/llmnoderesources.cpp
+++ src/llm/llmnoderesources.cpp
@@ -38,20 +38,87 @@
 
 namespace ovms {
 
+void loadTextProcessor(std::shared_ptr<LLMNodeResources>& nodeResources) {
+    py::gil_scoped_acquire acquire;
+    try {
+        auto locals = py::dict("models_path"_a = nodeResources->modelsPath);
+        py::exec(R"(
+            # Following the logic from:
+            # https://github.com/huggingface/transformers/blob/25245ec26dc29bcf6102e1b4ddd0dfd02e720cf5/src/transformers/tokenization_utils_base.py#L1837
+
+            global json
+            import json
+            from pathlib import Path
+
+            global jinja2
+            import jinja2
+            from jinja2.sandbox import ImmutableSandboxedEnvironment
+
+            def raise_exception(message):
+                raise jinja2.exceptions.TemplateError(message)
+
+
+            # Default chat template accepts only single message and outputs only it's 'content'
+            # effectively turning it into a regular prompt. 
+            default_chat_template = "{% if messages|length > 1 %} {{ raise_exception('This servable accepts only single message requests') }}{% endif %}{{ messages[0]['content'] }}"
+
+            bos_token = ""
+            eos_token = ""
+            chat_template = default_chat_template
+
+            template = None
+
+            # Try to read template from template.jinja file
+            jinja_file = Path(models_path + "/template.jinja")
+            if jinja_file.is_file():
+                template_loader = jinja2.FileSystemLoader(searchpath=models_path)
+                jinja_env = ImmutableSandboxedEnvironment(trim_blocks=True, lstrip_blocks=True, loader=template_loader)
+                jinja_env.policies["json.dumps_kwargs"]["ensure_ascii"] = False
+                jinja_env.globals["raise_exception"] = raise_exception
+                template = jinja_env.get_template("template.jinja")
+
+            # Try to read data from tokenizer_config.json
+            tokenizer_config_file = Path(models_path + "/tokenizer_config.json")
+            if tokenizer_config_file.is_file():
+                f = open(models_path + "/tokenizer_config.json")
+                data = json.load(f)
+                bos_token = data.get("bos_token", "")
+                eos_token = data.get("eos_token", "")
+                chat_template = data.get("chat_template", default_chat_template)
+
+            if template is None:
+                jinja_env = ImmutableSandboxedEnvironment(trim_blocks=True, lstrip_blocks=True)
+                jinja_env.policies["json.dumps_kwargs"]["ensure_ascii"] = False
+                jinja_env.globals["raise_exception"] = raise_exception
+                template = jinja_env.from_string(chat_template)
+        )", py::globals(), locals);
+
+        nodeResources->textProcessor.bosToken = locals["bos_token"].cast<std::string>();
+        nodeResources->textProcessor.eosToken = locals["eos_token"].cast<std::string>();
+        nodeResources->textProcessor.chatTemplate = std::make_unique<PyObjectWrapper<py::object>>(locals["template"]);
+    } catch (const pybind11::error_already_set& e) {
+        SPDLOG_INFO("Warning: Chat template has not been loaded properly. Servable will not respond to /chat/completions endpoint.");
+        SPDLOG_DEBUG("Chat template loading failed with error: {}", e.what());
+    } catch (...) {
+        SPDLOG_INFO("Warning: Chat template has not been loaded properly. Servable will not respond to /chat/completions endpoint.");
+        SPDLOG_DEBUG("Chat template loading failed with an unexpected error");
+    }
+}
+
 Status LLMNodeResources::createLLMNodeResources(std::shared_ptr<LLMNodeResources>& nodeResources, const ::mediapipe::CalculatorGraphConfig::Node& graphNodeConfig, std::string graphPath) {
     mediapipe::LLMCalculatorOptions nodeOptions;
     graphNodeConfig.node_options(0).UnpackTo(&nodeOptions);
     nodeResources = std::make_shared<LLMNodeResources>();
-    auto fsWorkspacePath = std::filesystem::path(nodeOptions.models_path());
+    auto fsModelsPath = std::filesystem::path(nodeOptions.models_path());
 
     std::string basePath;
-    if (fsWorkspacePath.is_relative()) {
-        basePath = (std::filesystem::path(graphPath) / fsWorkspacePath).string();
+    if (fsModelsPath.is_relative()) {
+        basePath = (std::filesystem::path(graphPath) / fsModelsPath).string();
     } else {
-        basePath = fsWorkspacePath.string();
+        basePath = fsModelsPath.string();
     }
 
-    nodeResources->workspacePath = basePath;
+    nodeResources->modelsPath = basePath;
     if (basePath.empty()) {
         SPDLOG_LOGGER_ERROR(modelmanager_logger, "LLM node models_path: {} is empty. ", basePath);
         return StatusCode::LLM_NODE_DIRECTORY_DOES_NOT_EXIST;
@@ -83,6 +150,8 @@ Status LLMNodeResources::createLLMNodeResources(std::shared_ptr<LLMNodeResources
         SPDLOG_ERROR("Error during llm node initialization for models_path: {}", basePath);
         return StatusCode::LLM_NODE_RESOURCE_STATE_INITIALIZATION_FAILED;
     }
+
+    loadTextProcessor(nodeResources);
 
     nodeResources->initiateGeneration();
 
