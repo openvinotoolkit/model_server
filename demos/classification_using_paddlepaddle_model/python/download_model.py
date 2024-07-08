30751bcde60c57d07430699cb0da6dea86ffd5fd
Damian Kalinowski <damian.kalinowski@intel.com>
2024-07-08 16:11:21 +0200

    bandit fix

--- demos/classification_using_paddlepaddle_model/python/download_model.py
+++ demos/classification_using_paddlepaddle_model/python/download_model.py
@@ -1,3 +1,4 @@
+import hashlib
 import os.path
 import urllib.request
 import tarfile
@@ -11,11 +12,22 @@ else:
     print("Downloading the MobileNetV3_large_x1_0_infer model (20Mb)... May take a while...")
     #make the directory if it is not 
     os.makedirs('model')
-    urllib.request.urlretrieve(mobilenet_url, "model/MobileNetV3_large_x1_0_infer.tar")
+    urllib.request.urlretrieve(mobilenet_url, "model/MobileNetV3_large_x1_0_infer.tar")  # nosec
     print("Model Downloaded")
 
+    sha256_hash = hashlib.sha256()
+    with open("model/MobileNetV3_large_x1_0_infer.tar", "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha256_hash.update(byte_block)
+    calculated_hash = sha256_hash.hexdigest()
+    expected_hash = "ca6c1b685b40c8d9219563f52b76c5716a2fbdc5214ebcdba67b234db8070f72"
+    if calculated_hash != expected_hash:
+        print("Downloaded file integrity check failed")
+        exit(1)
+    print("File integrity verified. Extracting the tar archive...")
+
     file = tarfile.open("model/MobileNetV3_large_x1_0_infer.tar")
-    res = file.extractall('model')
+    res = file.extractall('model')  # nosec
     file.close()
     if (not res):
         print("Model Extracted to \"model/MobileNetV3_large_x1_0_infer\".")
