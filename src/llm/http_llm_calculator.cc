cd9e50f528ea37ecc7dd696fafc956efe4f45c94
rasapala <rafal.a.sapala@intel.com>
2024-07-23 23:27:40 +0200

    Fix

--- src/llm/http_llm_calculator.cc
+++ src/llm/http_llm_calculator.cc
@@ -360,50 +360,6 @@ public:
     }
 };
 
-<<<<<<< HEAD
-=======
-// TODO: To be moved to CB library.
-class TextStreamer {
-    std::shared_ptr<Tokenizer> tokenizer;
-    std::vector<int64_t> tokenCache;
-    size_t printLen{0};
-
-public:
-    TextStreamer(std::shared_ptr<Tokenizer> tokenizer) :
-        tokenizer(std::move(tokenizer)) {}
-
-    std::optional<std::string> put(int64_t token) {
-        tokenCache.push_back(token);
-        std::string text = tokenizer->decode(tokenCache);
-
-        if (!text.empty() && '\n' == text.back() && text.size() > printLen) {
-            // The chunk is ready if the generated text ends with new line.
-            // Also, clear the cache.
-            std::string chunk = std::string{text.data() + printLen, text.size() - printLen};
-            SPDLOG_LOGGER_TRACE(llm_calculator_logger, "Generated tokens: {}", tokenCache);
-            tokenCache.clear();
-            printLen = 0;
-            return chunk;
-        } else if (!isValidUtf8(text)) {
-            return std::nullopt;
-        } else if (text.size() > printLen) {
-            // The chunk is ready if the new text in the cache contains space.
-            // The chunk is constructed from the new text, however only up to the last space character (including it)
-            // Does not clear the cache.
-            auto lastSpacePos = text.rfind(' ');
-            if (lastSpacePos == std::string::npos || lastSpacePos < printLen) {
-                return std::nullopt;
-            }
-            std::string chunk = std::string{text.data() + printLen, lastSpacePos - printLen + 1};
-            printLen = lastSpacePos + 1;
-            SPDLOG_LOGGER_TRACE(llm_calculator_logger, "Generated tokens: {}", tokenCache);
-            return chunk;
-        }
-        return std::nullopt;
-    }
-};
-
->>>>>>> 6260462ab (Fixes)
 using InputDataType = ovms::HttpPayload;
 using OutputDataType = std::string;
 
