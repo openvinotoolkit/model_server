dff266a63a96952dacd239b9eaadbc8d0bcc26d9
Rafal Sapala <rafal.a.sapala@intel.com>
2024-08-02 12:58:04 +0200

    Move

--- ci/lib_search.py
+++ ci/lib_search.py
@@ -225,9 +225,9 @@ def check_func(start_dir):
         "iris_tracking.pbtxt",
         "internal_tests",
         ".bazelversion",
-        "lib_files.txt",
-        "lib_files_python.txt",
-        "lib_custom_nodes_files",
+        "file_lists/lib_files.txt",
+        "file_lists/lib_files_python.txt",
+        "file_lists/lib_custom_nodes_files",
     ]
 
     exclude_directories = ['/dist/']
