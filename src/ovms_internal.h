3a154788c2983ce77fa73cc86b93fff159978b82
Adrian Tobiszewski <adrian.tobiszewski@intel.com>
2024-09-05 09:24:45 +0200

    Fix cpplint

--- src/ovms_internal.h
+++ src/ovms_internal.h
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //*****************************************************************************
-#include "ovms.h"
+#include "ovms.h"  // NOLINT
 
 #ifdef __cplusplus
 extern "C" {
