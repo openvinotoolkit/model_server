82fa72e29d67f3edc2712eee0ab3925b65ec95f9
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2024-09-20 09:01:54 +0200

    update stable diffusion model in python demo

--- demos/python_demos/stable_diffusion/download_model.py
+++ demos/python_demos/stable_diffusion/download_model.py
@@ -17,7 +17,7 @@ from optimum.intel.openvino import OVStableDiffusionPipeline
 
 MODEL_PATH = './model'
 OV_CONFIG = {'PERFORMANCE_HINT': 'LATENCY', 'NUM_STREAMS': '1'}
-model_name = 'runwayml/stable-diffusion-v1-5'
+model_name = 'botp/stable-diffusion-v1-5'
 
 print('Downloading and converting...')
 ov_model = OVStableDiffusionPipeline.from_pretrained(
