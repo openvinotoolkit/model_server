d4134faae4b55a6b006fe0259a126e2a7c18ea6d
Rafal Sapala <rafal.a.sapala@intel.com>
2024-10-11 17:33:02 +0200

    dlopen fix

--- src/dags/custom_node_library_manager.cpp
+++ src/dags/custom_node_library_manager.cpp
@@ -49,7 +49,11 @@ Status CustomNodeLibraryManager::loadLibrary(const std::string& name, const std:
 #ifdef __linux__
     void* handle = dlopen(basePath.c_str(), RTLD_LAZY | RTLD_LOCAL);
     char* error = dlerror();
-
+    if (handle == NULL) {
+        SPDLOG_LOGGER_ERROR(modelmanager_logger, "Library name: {} failed to open base_path: {} with error: {}", name, basePath, error);
+        return StatusCode::NODE_LIBRARY_LOAD_FAILED_OPEN;
+    }
+    
     initialize_fn initialize = reinterpret_cast<initialize_fn>(dlsym(handle, "initialize"));
     error = dlerror();
     if (error || initialize == nullptr) {
