8a1e0ab0f141d0874a3a8c3af83ee550db94b769
michalkulakowski <michal.kulakowski@intel.com>
2024-10-14 15:38:58 +0200

    Add rerank request deserialization

---
+++ src/rerank/rerank.hpp
@@ -0,0 +1,72 @@
+//*****************************************************************************
+// Copyright 2024 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+#pragma once
+
+#include <limits>
+#include <optional>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+
+#include "absl/status/status.h"
+
+using namespace rapidjson;
+
+namespace ovms {
+
+// Class that maps Rerank request content.
+struct RerankRequest {
+    std::string model;
+    std::string query;
+    std::vector<std::string> documentsList;
+    std::unordered_map<std::string, std::string> documentsMap;
+    std::optional<int> topN{std::nullopt};
+    std::optional<std::vector<std::string>> rankFields{std::nullopt};
+    std::optional<bool> returnDocuments{std::nullopt};
+    std::optional<int> maxChunksPerDoc{std::nullopt};
+
+    RerankRequest() = default;
+    ~RerankRequest() = default;
+};
+
+// Class that wraps rerank request, holds and processes raw JSON, provides methods for serialization and keeps track of usage.
+// It is used in the calculator.
+class RerankHandler {
+    Document& doc;
+    RerankRequest request;
+
+public:
+    RerankHandler(Document& doc) :
+        doc(doc) {}
+
+    std::string getModel() const;
+    std::string getQuery() const;
+    std::vector<std::string> getDocumentsList() const;
+    std::unordered_map<std::string, std::string> getDocumentsMap() const;
+
+    std::optional<int> getTopN() const;
+    std::optional<bool> getReturnDocuments() const;
+    std::optional<std::vector<std::string>> getRankFields() const;
+    std::optional<int> getMaxChunksPerDoc() const;
+
+    absl::Status parseRequest();
+};
+}  // namespace ovms
---
+++ src/test/rerank_handler_test.cpp
@@ -0,0 +1,318 @@
+//*****************************************************************************
+// Copyright 2024 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "../rerank/rerank.hpp"
+
+using namespace ovms;
+
+using ::testing::ElementsAre;
+
+class RerankHandlerTest : public ::testing::Test {
+protected:
+    Document doc;
+    std::string json;
+    void SetUp() override {
+    }
+};
+
+TEST_F(RerankHandlerTest, ValidRequestDocumentsMap) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "documents": [
+        {
+        "title": "first document title",
+        "text": "first document text"
+        },
+        {
+        "title": "second document title",
+        "text": "second document text"
+        }
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    ASSERT_EQ(handler.parseRequest(), absl::OkStatus());
+    EXPECT_STREQ(handler.getModel().c_str(), "model");
+    EXPECT_STREQ(handler.getQuery().c_str(), "query");
+    ASSERT_FALSE(handler.getTopN().has_value());
+    ASSERT_FALSE(handler.getReturnDocuments().has_value());
+    ASSERT_FALSE(handler.getRankFields().has_value());
+    ASSERT_FALSE(handler.getMaxChunksPerDoc().has_value());
+    EXPECT_EQ(handler.getDocumentsList().size(), 0);
+    ASSERT_EQ(handler.getDocumentsMap().size(), 2);
+    std::unordered_map<std::string, std::string>::iterator it = handler.getDocumentsMap().find("first document title");
+    EXPECT_NE(it, handler.getDocumentsMap().end());
+    EXPECT_STREQ(handler.getDocumentsMap().at("first document title").c_str(), "first document text");
+    it = handler.getDocumentsMap().find("second document title");
+    EXPECT_NE(it, handler.getDocumentsMap().end());
+    EXPECT_STREQ(handler.getDocumentsMap().at("second document title").c_str(), "second document text");
+}
+
+TEST_F(RerankHandlerTest, ValidRequestDocumentsList) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "documents": [
+        "first document",
+        "second document"
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    ASSERT_EQ(handler.parseRequest(), absl::OkStatus());
+    EXPECT_STREQ(handler.getModel().c_str(), "model");
+    EXPECT_STREQ(handler.getQuery().c_str(), "query");
+    ASSERT_FALSE(handler.getTopN().has_value());
+    ASSERT_FALSE(handler.getReturnDocuments().has_value());
+    ASSERT_FALSE(handler.getRankFields().has_value());
+    ASSERT_FALSE(handler.getMaxChunksPerDoc().has_value());
+    ASSERT_EQ(handler.getDocumentsList().size(), 2);
+    EXPECT_EQ(handler.getDocumentsMap().size(), 0);
+    EXPECT_STREQ(handler.getDocumentsList()[0].c_str(), "first document");
+    EXPECT_STREQ(handler.getDocumentsList()[1].c_str(), "second document");
+}
+
+TEST_F(RerankHandlerTest, DocumentsArrayMixedElementTypes) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "documents": [
+        "first document",
+        {
+            "title": "second document title",
+            "text": "second document text"
+        }
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    auto status = handler.parseRequest();
+    EXPECT_EQ(status, absl::InvalidArgumentError("all documents have to be the same type (string or objects)"));
+}
+
+TEST_F(RerankHandlerTest, InvalidDocuments) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "documents": "INVALID"
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    auto status = handler.parseRequest();
+    EXPECT_EQ(status, absl::InvalidArgumentError("documents is not an array"));
+}
+
+TEST_F(RerankHandlerTest, InvalidDocumentsElement) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "documents": [1,2,3]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    auto status = handler.parseRequest();
+    EXPECT_EQ(status, absl::InvalidArgumentError("documents array element is neither string nor object"));
+}
+
+TEST_F(RerankHandlerTest, ValidTopN) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "top_n": 1,
+    "documents": [
+        "first document"
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    ASSERT_EQ(handler.parseRequest(), absl::OkStatus());
+    EXPECT_STREQ(handler.getModel().c_str(), "model");
+    EXPECT_STREQ(handler.getQuery().c_str(), "query");
+    ASSERT_FALSE(handler.getReturnDocuments().has_value());
+    ASSERT_FALSE(handler.getRankFields().has_value());
+    ASSERT_FALSE(handler.getMaxChunksPerDoc().has_value());
+    EXPECT_EQ(handler.getDocumentsList().size(), 1);
+    ASSERT_EQ(handler.getDocumentsMap().size(), 0);
+    EXPECT_STREQ(handler.getDocumentsList()[0].c_str(), "first document");
+    ASSERT_TRUE(handler.getTopN().has_value());
+    EXPECT_EQ(handler.getTopN().value(), 1);
+}
+
+TEST_F(RerankHandlerTest, InvalidTopN) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "top_n": "INVALID",
+    "documents": [
+        "first document"
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    auto status = handler.parseRequest();
+    EXPECT_EQ(status, absl::InvalidArgumentError("top_n accepts integer values"));
+}
+
+TEST_F(RerankHandlerTest, ValidRankFields) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "rank_fields": ["first", "second"],
+    "documents": [
+        "first document"
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    ASSERT_EQ(handler.parseRequest(), absl::OkStatus());
+    EXPECT_STREQ(handler.getModel().c_str(), "model");
+    EXPECT_STREQ(handler.getQuery().c_str(), "query");
+    ASSERT_FALSE(handler.getReturnDocuments().has_value());
+    ASSERT_FALSE(handler.getTopN().has_value());
+    ASSERT_FALSE(handler.getMaxChunksPerDoc().has_value());
+    EXPECT_EQ(handler.getDocumentsList().size(), 1);
+    ASSERT_EQ(handler.getDocumentsMap().size(), 0);
+    EXPECT_STREQ(handler.getDocumentsList()[0].c_str(), "first document");
+    ASSERT_TRUE(handler.getRankFields().has_value());
+    ASSERT_EQ(handler.getRankFields().value().size(), 2);
+    EXPECT_STREQ(handler.getRankFields().value()[0].c_str(), "first");
+    EXPECT_STREQ(handler.getRankFields().value()[1].c_str(), "second");
+}
+
+TEST_F(RerankHandlerTest, InvalidRankFields) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "rank_fields": "INVALID",
+    "documents": [
+        "first document"
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    auto status = handler.parseRequest();
+    EXPECT_EQ(status, absl::InvalidArgumentError("rank_fields is not an array"));
+}
+
+TEST_F(RerankHandlerTest, InvalidRankFieldsElement) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "rank_fields": [1],
+    "documents": [
+        "first document"
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    auto status = handler.parseRequest();
+    EXPECT_EQ(status, absl::InvalidArgumentError("rank_fields array element is not a string"));
+}
+
+TEST_F(RerankHandlerTest, ValidReturnDocuments) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "return_documents": true,
+    "documents": [
+        "first document"
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    ASSERT_EQ(handler.parseRequest(), absl::OkStatus());
+    EXPECT_STREQ(handler.getModel().c_str(), "model");
+    EXPECT_STREQ(handler.getQuery().c_str(), "query");
+    ASSERT_FALSE(handler.getRankFields().has_value());
+    ASSERT_FALSE(handler.getTopN().has_value());
+    ASSERT_FALSE(handler.getMaxChunksPerDoc().has_value());
+    EXPECT_EQ(handler.getDocumentsList().size(), 1);
+    ASSERT_EQ(handler.getDocumentsMap().size(), 0);
+    EXPECT_STREQ(handler.getDocumentsList()[0].c_str(), "first document");
+    ASSERT_TRUE(handler.getReturnDocuments().has_value());
+    EXPECT_TRUE(handler.getReturnDocuments().value());
+}
+
+TEST_F(RerankHandlerTest, InvalidReturnDocuments) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "return_documents": "INVALID",
+    "documents": [
+        "first document"
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    auto status = handler.parseRequest();
+    EXPECT_EQ(status, absl::InvalidArgumentError("return_documents accepts boolean values"));
+}
+
+TEST_F(RerankHandlerTest, ValidMaxChunksPerDoc) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "max_chunks_per_doc": 2,
+    "documents": [
+        "first document"
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    ASSERT_EQ(handler.parseRequest(), absl::OkStatus());
+    EXPECT_STREQ(handler.getModel().c_str(), "model");
+    EXPECT_STREQ(handler.getQuery().c_str(), "query");
+    ASSERT_FALSE(handler.getReturnDocuments().has_value());
+    ASSERT_FALSE(handler.getRankFields().has_value());
+    ASSERT_FALSE(handler.getTopN().has_value());
+    EXPECT_EQ(handler.getDocumentsList().size(), 1);
+    ASSERT_EQ(handler.getDocumentsMap().size(), 0);
+    EXPECT_STREQ(handler.getDocumentsList()[0].c_str(), "first document");
+    ASSERT_TRUE(handler.getMaxChunksPerDoc().has_value());
+    EXPECT_EQ(handler.getMaxChunksPerDoc().value(), 2);
+}
+
+TEST_F(RerankHandlerTest, InvalidMaxChunksPerDoc) {
+    json = R"({
+    "model": "model",
+    "query": "query",
+    "max_chunks_per_doc": "INVALID",
+    "documents": [
+        "first document"
+    ]
+    })";
+
+    ASSERT_FALSE(doc.Parse(json.c_str()).HasParseError());
+    RerankHandler handler(doc);
+    auto status = handler.parseRequest();
+    EXPECT_EQ(status, absl::InvalidArgumentError("max_chunks_per_doc accepts integer values"));
+}
