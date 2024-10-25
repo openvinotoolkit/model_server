c6aeede30b222e6305feaf0a7b17a37b85a0e448
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2024-10-26 00:20:51 +0200

    style

--- src/modelinstance.cpp
+++ src/modelinstance.cpp
@@ -83,7 +83,7 @@ enum : unsigned int {
 }  // namespace
 
 namespace ov {
-    struct Meta; // pure fwd declaration in getRTInfo
+struct Meta;  // pure fwd declaration in getRTInfo
 }
 
 namespace ovms {
