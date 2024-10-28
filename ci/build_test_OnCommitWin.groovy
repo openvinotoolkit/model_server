9fb4e4d6bd5cc60bcbeac71a6736fa034e8b49b2
rasapala <rafal.a.sapala@intel.com>
2024-10-28 14:23:17 +0100

    Logs2

--- ci/build_test_OnCommitWin.groovy
+++ ci/build_test_OnCommitWin.groovy
@@ -3,35 +3,34 @@ pipeline {
         timeout(time: 2, unit: 'HOURS')
     }
     agent {
-      label 'win_ovms'
+        label 'win_ovms'
     }
     stages {
         // Build windows
         stage("Build windows") {
-          steps {
+            steps {
                 bat "build_windows.bat"
-              }
+            }
         }
+    }
+    //Post build steps
+    post {
+        success {
+            script{}
+        }
+        unstable {
+            script{}
+        }
+        failure {
+            script{}
+        }
+        always {
+            //junit allowEmptyResults: true, testResults: "logs/**/*.xml"
+            archiveArtifacts allowEmptyArchive: true, artifacts: "bazel-bin\\src\\ovms.exe"
+            archiveArtifacts allowEmptyArchive: true, artifacts: "environment.log"
+            archiveArtifacts allowEmptyArchive: true, artifacts: "build.log"
 
-        //Post build steps
-        post {
-          success {
-              script{}
-          }
-          unstable {
-              script{}
-          }
-          failure {
-              script{}
-          }
-          always {
-              //junit allowEmptyResults: true, testResults: "logs/**/*.xml"
-              archiveArtifacts allowEmptyArchive: true, artifacts: "bazel-bin\\src\\ovms.exe"
-              archiveArtifacts allowEmptyArchive: true, artifacts: "environment.log"
-              archiveArtifacts allowEmptyArchive: true, artifacts: "build.log"
-
-              script{}
-          }
-      }
+            script{}
+        }
     }
 }
