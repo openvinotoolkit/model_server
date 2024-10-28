8f4f9ae54aeba8fd8ad92b75ce5370706d8e48ee
rasapala <rafal.a.sapala@intel.com>
2024-10-28 14:25:17 +0100

    Fix2

--- ci/build_test_OnCommitWin.groovy
+++ ci/build_test_OnCommitWin.groovy
@@ -15,22 +15,11 @@ pipeline {
     }
     //Post build steps
     post {
-        success {
-            //script{}
-        }
-        unstable {
-            //script{}
-        }
-        failure {
-            //script{}
-        }
         always {
             //junit allowEmptyResults: true, testResults: "logs/**/*.xml"
             archiveArtifacts allowEmptyArchive: true, artifacts: "bazel-bin\\src\\ovms.exe"
             archiveArtifacts allowEmptyArchive: true, artifacts: "environment.log"
             archiveArtifacts allowEmptyArchive: true, artifacts: "build.log"
-
-            //script{}
         }
     }
 }
