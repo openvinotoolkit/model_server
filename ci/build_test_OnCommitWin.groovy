a606b1009c5b49364ac9e3233bfc104ee7e2548c
rasapala <rafal.a.sapala@intel.com>
2024-10-28 14:21:25 +0100

    Fix

--- ci/build_test_OnCommitWin.groovy
+++ ci/build_test_OnCommitWin.groovy
@@ -6,32 +6,32 @@ pipeline {
       label 'win_ovms'
     }
     stages {
+        // Build windows
         stage("Build windows") {
           steps {
                 bat "build_windows.bat"
               }
         }
+
+        //Post build steps
+        post {
+          success {
+              script{}
+          }
+          unstable {
+              script{}
+          }
+          failure {
+              script{}
+          }
+          always {
+              //junit allowEmptyResults: true, testResults: "logs/**/*.xml"
+              archiveArtifacts allowEmptyArchive: true, artifacts: "bazel-bin\\src\\ovms.exe"
+              archiveArtifacts allowEmptyArchive: true, artifacts: "environment.log"
+              archiveArtifacts allowEmptyArchive: true, artifacts: "build.log"
+
+              script{}
+          }
+      }
     }
 }
-
- post {
-        success {
-            script{}
-        }
-        unstable {
-            script{}
-        }
-        failure {
-            script{}
-        }
-        always {
-            //junit allowEmptyResults: true, testResults: "logs/**/*.xml"
-            archiveArtifacts allowEmptyArchive: true, artifacts: "bazel-bin\\src\\ovms.exe"
-            archiveArtifacts allowEmptyArchive: true, artifacts: "environment.log"
-            archiveArtifacts allowEmptyArchive: true, artifacts: "build.log"
-
-            script{
-            }
-
-        }
-    }
