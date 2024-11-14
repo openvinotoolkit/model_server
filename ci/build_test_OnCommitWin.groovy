090cd1ecd96d1405d955cbd2b16870fb97724b32
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2024-11-14 08:54:00 +0100

    test

--- ci/build_test_OnCommitWin.groovy
+++ ci/build_test_OnCommitWin.groovy
@@ -17,14 +17,10 @@ pipeline {
         stage("Build windows") {
             steps {
                 script {
-                    def status = bat(returnStatus: true, script: 'build_windows.bat ' + env.JOB_BASE_NAME)
-                    status = bat(returnStatus: true, script: 'grep -A 4 bazel-bin/src/ovms.exe build.log | grep "Build completed successfully"')
-                    if (status != 0) {
-                            error "Error: Windows build failed ${status}. Check build.log for details."
-                    } else {
-                        echo "Build successful."
-                    }
+                    powershell dir   
                 }
+                script {
+                    powershell Get-Variable
                 }
             }
         stage("Check tests windows") {
