2313a951421d08445fddb7dda182574d4d6a5128
rasapala <rafal.a.sapala@intel.com>
2024-11-15 12:33:48 +0100

    Cut and store win logs

--- ci/loadWin.groovy
+++ ci/loadWin.groovy
@@ -4,31 +4,31 @@ def clean() {
 
 def build_and_test(){
     def status = bat(returnStatus: true, script: 'build_windows.bat ' + env.JOB_BASE_NAME)
-    status = bat(returnStatus: true, script: 'grep -A 4 bazel-bin/src/ovms.exe build.log | grep "Build completed successfully"')
+    status = bat(returnStatus: true, script: 'grep -A 4 bazel-bin/src/ovms.exe win_build.log | grep "Build completed successfully"')
     if (status != 0) {
-        error "Error: Windows build failed ${status}. Check build.log for details."
+        error "Error: Windows build failed ${status}. Check win_build.log for details."
     } else {
         echo "Build successful."
     }
 }
 
 def check_tests(){
-    def status = bat(returnStatus: true, script: 'grep -A 4 bazel-bin/src/ovms_test.exe build_test.log | grep "Build completed successfully"')
+    def status = bat(returnStatus: true, script: 'grep -A 4 bazel-bin/src/ovms_test.exe win_build_test.log | grep "Build completed successfully"')
     if (status != 0) {
-            error "Error: Windows build test failed ${status}. Check build_test.log for details."
+            error "Error: Windows build test failed ${status}. Check win_build_test.log for details."
     } else {
         echo "Build test successful."
     }
 
-    status = bat(returnStatus: true, script: 'grep "[  PASSED  ]" test.log')
+    status = bat(returnStatus: true, script: 'grep "[  PASSED  ]" win_test.log')
     if (status != 0) {
-            error "Error: Windows run test failed ${status}. Check test.log for details."
+            error "Error: Windows run test failed ${status}. Check win_test.log for details."
     }
 
     // TODO Windows: Currently some tests fail change to no fail when fixed.
-    status = bat(returnStatus: true, script: 'grep "[  FAILED  ]" test.log')
+    status = bat(returnStatus: true, script: 'grep "[  FAILED  ]" win_test.log')
     if (status != 0) {
-            error "Error: Windows run test failed ${status}. Check test.log for details."
+            error "Error: Windows run test failed ${status}. Check win_test.log for details."
     } else {
         echo "Run test successful."
     }
@@ -38,10 +38,10 @@ def check_tests(){
 def archive_artifacts(){
     // Left for tests when enabled - junit allowEmptyResults: true, testResults: "logs/**/*.xml"
     archiveArtifacts allowEmptyArchive: true, artifacts: "bazel-bin\\src\\ovms.exe"
-    archiveArtifacts allowEmptyArchive: true, artifacts: "environment.log"
-    archiveArtifacts allowEmptyArchive: true, artifacts: "build.log"
-    archiveArtifacts allowEmptyArchive: true, artifacts: "build_test.log"
-    archiveArtifacts allowEmptyArchive: true, artifacts: "test.log"
+    archiveArtifacts allowEmptyArchive: true, artifacts: "win_environment.log"
+    archiveArtifacts allowEmptyArchive: true, artifacts: "win_build.log"
+    archiveArtifacts allowEmptyArchive: true, artifacts: "win_build_test.log"
+    archiveArtifacts allowEmptyArchive: true, artifacts: "win_test.log"
 }
 
 return this
