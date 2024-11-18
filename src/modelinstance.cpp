d62b6d9914cea7bbf923e3d6d2adf9688dfe36d8
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2024-11-18 17:56:00 +0100

    style

--- src/modelinstance.cpp
+++ src/modelinstance.cpp
@@ -308,8 +308,8 @@ static Status applyLayoutConfiguration(const ModelConfig& config, std::shared_pt
     for (ov::Output<ov::Node>& output : model->outputs()) {
         try {
             OV_LOGGER("ov::Output<ov::Node> output: {}, output.get_any_name()", reinterpret_cast<const void*>(&output));
-            if (output.get_names().size() == 0){
-                std::unordered_set<std::string> dummy_name{"out_"+std::to_string(output.get_index())};
+            if (output.get_names().size() == 0) {
+                std::unordered_set<std::string> dummy_name{"out_" + std::to_string(output.get_index())};
                 output.add_names(dummy_name);
             }
             std::string name = output.get_any_name();
