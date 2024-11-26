2169085fedf1e2916080333165fdd647aaa11a37
Rafal Sapala <rafal.a.sapala@intel.com>
2024-11-26 14:03:12 +0100

    Style

--- src/test/model_service_test.cpp
+++ src/test/model_service_test.cpp
@@ -385,7 +385,7 @@ TEST_F(TFSModelServiceTest, getAllModelsStatuses_two_models_with_one_versions) {
 
 TEST_F(TFSModelServiceTest, config_reload) {
 #ifdef _WIN32
-        GTEST_SKIP() << "Test disabled on windows [SPORADIC]";
+    GTEST_SKIP() << "Test disabled on windows [SPORADIC]";
 #endif
     std::string port = "9000";
     randomizePort(port);
