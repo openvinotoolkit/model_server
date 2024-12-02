91af7b6dbb9fdb44de84de8a6a6b56020c14599d
Kunda <752038@qq.com>
2024-12-02 21:38:07 +0800

    CVS-154710, CVS-154711, Implement LoadFileInfoReporter and installSignalHandlers (#2769)
    * Windows enabling
    
    * Access error
    
    * Compilation started
    
    * Compile progress
    
    * Link ovms: libevent error
    
    * org_tensorflow problem, serving compile problem
    
    * org_tensorflow framework compiles
    
    * Ovms compile windows errors
    
    * Ocl .h error
    
    * Compilation wip
    
    * Modelconfig
    
    * error LNK1120: 160 unresolved externals
    
    * Mangling errors
    
    * OVMS compiles
    
    * Link cleanup
    
    * Setup
    
    * Quick start working
    
    * Demo
    
    * Batch files
    
    * Clean docs
    
    * IFDEF cleanup
    
    * Access, boost
    
    * Linux fixes
    
    * Fix tests compile
    
    * Speel
    
    * Style
    
    * dlopen fix
    
    * Compiles windows
    
    * Style2
    
    * Python debug
    
    * Fix python segfault
    
    * Fix boringssl
    
    * Fix tests
    
    * Add ssl link
    
    * Config linux Makefile
    
    * Windows works
    
    * Cleanup
    
    * Add manager logs
    
    * Fix exception java:
    
    * --config=linux
    
    * Remove OpenCL
    
    * Fix Ocl
    
    * Style
    
    * Remove vscode
    
    * Code review
    
    * Update opencv to 4.10
    
    * @Review@
    
    * Fix demos
    
    * Demos fix
    
    * Doc update
    
    * Spell
    
    * CVS-154710, Implement LoadFileInfoReporter in modelmanager
    
    * CVS-154711, Implement installSignalHanders()
    
    * Apply clang-format changes
    
    * Apply clang-format changes 2
    
    * Apply clang-format changes 3
    
    * Apply clang-format changes 4
    
    * Apply clang-format changes 5
    
    * Fix compile issue caused by main branch rebase
    
    * Apply clang-format changes 6
    
    ---------
    
    Co-authored-by: rasapala <rafal.a.sapala@intel.com>

--- src/modelmanager.cpp
+++ src/modelmanager.cpp
@@ -30,6 +30,13 @@
 #include <dlfcn.h>
 #include <sysexits.h>
 #endif
+
+#ifdef _WIN32
+#include <iomanip>
+
+#include <windows.h>
+#endif
+
 #include <errno.h>
 #include <rapidjson/document.h>
 #include <rapidjson/error/en.h>
@@ -878,6 +885,40 @@ public:
         ss << "FileInfoReporter: " << filename
            << " time modification [s]: " << statTime.st_ctim.tv_sec
            << " [ns]: " << statTime.st_ctim.tv_nsec << std::endl;
+#elif _WIN32
+        // Windows implementation
+        HANDLE hFile = CreateFileA(
+            filename.c_str(),
+            GENERIC_READ,
+            FILE_SHARE_READ,
+            NULL,
+            OPEN_EXISTING,
+            FILE_ATTRIBUTE_NORMAL,
+            NULL);
+        if (hFile == INVALID_HANDLE_VALUE) {
+            SPDLOG_ERROR("Failed to open file for debug-read on Windows");
+            return;
+        }
+
+        FILETIME creationTime, lastAccessTime, lastWriteTime;
+        if (!GetFileTime(hFile, &creationTime, &lastAccessTime, &lastWriteTime)) {
+            SPDLOG_ERROR("Failed to get file time on Windows");
+            CloseHandle(hFile);
+            return;
+        }
+
+        SYSTEMTIME stUTC;
+        FileTimeToSystemTime(&lastWriteTime, &stUTC);
+
+        ULARGE_INTEGER fileTimeAsUint;  // FILETIME units are 100 nanoseconds
+        fileTimeAsUint.LowPart = lastAccessTime.dwLowDateTime;
+        fileTimeAsUint.HighPart = lastAccessTime.dwHighDateTime;
+        uint64_t timeInNanoseconds = fileTimeAsUint.QuadPart * 100;
+
+        ss << "FileInfoReporter: " << filename
+           << " time modification [s]: " << std::setfill('0') << std::setw(2) << stUTC.wSecond
+           << " [ns]: " << timeInNanoseconds << std::endl;
+#endif
         std::string some;
         file.clear();
         file.seekg(0);
@@ -887,7 +928,6 @@ public:
         }
         file.clear();
         file.seekg(0);
-#endif
     }
     void log() {
         SPDLOG_LOGGER_DEBUG(modelmanager_logger, ss.str());
