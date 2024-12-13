9e9323206296a7de0291635671cb2b2efa6d6335
atobisze <adrian.tobiszewski@intel.com>
2024-12-13 14:24:14 +0100

    Temporarily disable Windows build

--- ci/build_test_OnCommit.groovy
+++ ci/build_test_OnCommit.groovy
@@ -94,19 +94,7 @@ pipeline {
               when { expression { win_image_build_needed == "true" } }
               steps {
                   script {
-                      def windows = load 'ci/loadWin.groovy'
-                      if (windows != null) {
-                        try {
-                          windows.clean()
-                          windows.install_dependencies()
-                          windows.build_and_test()
-                          windows.check_tests()
-                        } finally {
-                          windows.archive_artifacts()
-                        }
-                      } else {
-                          error "Cannot load ci/loadWin.groovy file."
-                      }
+                      return
                   }
               }
             }
