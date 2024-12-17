cc3423d6aa7a4c1cb893185f262bde52bd7226fa
Damian Kalinowski <damian.kalinowski@intel.com>
2024-12-17 14:45:26 +0100

    remove chrono

--- src/test/http_rest_api_handler_test.cpp
+++ src/test/http_rest_api_handler_test.cpp
@@ -13,9 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //*****************************************************************************
-#include <chrono>
-#include <thread>
-
 #include <gtest/gtest.h>
 
 #include "../config.hpp"
