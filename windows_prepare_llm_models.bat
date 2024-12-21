86ebe6b60af291519a84a7cf41ea286f4c48632d
Trawinski <dariusz.trawinski@intel.com>
2024-12-21 11:27:35 +0100

    drop pythonpath

--- windows_prepare_llm_models.bat
+++ windows_prepare_llm_models.bat
@@ -41,6 +41,7 @@ if exist "%~1\%TEXT_GENERATION_MODEL%" if exist "%~1\%EMBEDDING_MODEL%" if exist
 echo Downloading LLM testing models to directory %~1
 set "PIP_EXTRA_INDEX_URL=https://download.pytorch.org/whl/cpu https://storage.openvinotoolkit.org/simple/wheels/nightly"
 set "PYTHONHOME="
+set "PYTHONPATH="
 "C:\Program Files\Python310\python.exe" -m venv .venv
 call .\.venv\Scripts\Activate.bat
 set
