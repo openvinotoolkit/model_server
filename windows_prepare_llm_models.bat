f30e728e91dece0a68f5dc11d2a06298ee8339c4
Trawinski <dariusz.trawinski@intel.com>
2024-12-21 01:17:59 +0100

    test

--- windows_prepare_llm_models.bat
+++ windows_prepare_llm_models.bat
@@ -40,8 +40,9 @@ if exist "%~1\%TEXT_GENERATION_MODEL%" if exist "%~1\%EMBEDDING_MODEL%" if exist
 
 echo Downloading LLM testing models to directory %~1
 set "PIP_EXTRA_INDEX_URL=https://download.pytorch.org/whl/cpu https://storage.openvinotoolkit.org/simple/wheels/nightly"
+set PYTHONHOME=
 "C:\Program Files\Python310\python.exe" -m venv .venv
-.\.venv\Scripts\
+.\.venv\Scripts\activate
 set
 python -m pip install --upgrade pip
 pip install -U -r demos\common\export_models\requirements_win.txt
