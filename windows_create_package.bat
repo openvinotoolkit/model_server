1848d06c7e4d8031e4bf6b8302c452f82edda61b
Zeglarski <milosz.zeglarski@intel.com>
2025-01-08 11:56:29 +0100

    minor fix

--- windows_create_package.bat
+++ windows_create_package.bat
@@ -37,7 +37,7 @@ set "dest_dir=C:\opt"
 set "python_version=3.9.13"
 call %cd%\windows_prepare_python.bat %dest_dir% %python_version%
 :: Copy whole catalog to dist folder and install dependencies required by LLM pipelines
-xcopy C:\opt\python-%python_version%-embed-amd64 dist\windows\ovms\python /E /I /H
+xcopy %dest_dir%\python-%python_version%-embed-amd64 dist\windows\ovms\python /E /I /H
 .\dist\windows\ovms\python\python.exe -m pip install "Jinja2==3.1.4" "MarkupSafe==3.0.2"
 if !errorlevel! neq 0 (
     echo Error copying python into the distribution location. The package will not contain self-contained python.
