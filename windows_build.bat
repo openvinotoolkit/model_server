8b5015ae7e7e97e99e993e782f55b22c0b51a053
rasapala <rafal.a.sapala@intel.com>
2025-01-21 16:06:25 +0100

    Remove pythonhome

--- windows_build.bat
+++ windows_build.bat
@@ -68,7 +68,6 @@ set "BAZEL_SH=C:\opt\msys64\usr\bin\bash.exe"
 :: Set paths with libs for execution - affects PATH
 set "openvinoBatch=call !BAZEL_SHORT_PATH!\openvino\setupvars.bat"
 set "opencvBatch=call C:\opt\opencv\setup_vars_opencv4.cmd"
-set "PYTHONHOME=C:\opt\Python311"
 set "PYTHONPATH=%PYTHONPATH%;%setPythonPath%"
 
 :: Set required libraries paths
