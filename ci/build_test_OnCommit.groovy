664a5d1e03843b3b96afa88f8e4271d583a8cfab
Trawinski, Dariusz <dariusz.trawinski@intel.com>
2025-01-22 15:25:23 +0100

    Adding Ubuntu24 base image (#2892)
    CVS-155483
    CVS-158492
    CVS-159410
    Add support for ubuntu24 base image
    Drop support for ubuntu20 base image
    Support python3.12 in ubuntu24 OS
    Update GPU drivers for ubuntu24
    Change unit tests execution to separate building tests and executing individual groups or tests
    simplify run_unit_tests.sh execution in the build container and improve reporting errors
    add test logs as CI artifact
    Update jinja2 package
    
    ---------
    
    Co-authored-by: Dariusz Trawinski <dariusz.trainski@intel.com>
    Co-authored-by: Miłosz Żeglarski <milosz.zeglarski@intel.com>

--- ci/build_test_OnCommit.groovy
+++ ci/build_test_OnCommit.groovy
@@ -84,7 +84,7 @@ pipeline {
               when { expression { image_build_needed == "true" } }
                 steps {
                       sh "echo build --remote_cache=${env.OVMS_BAZEL_REMOTE_CACHE_URL} > .user.bazelrc"
-                      sh "make ovms_builder_image RUN_TESTS=0 OV_USE_BINARY=1 BASE_OS=redhat OVMS_CPP_IMAGE_TAG=${shortCommit}"
+                      sh "make ovms_builder_image RUN_TESTS=0 OPTIMIZE_BUILDING_TESTS=1 OV_USE_BINARY=1 BASE_OS=redhat OVMS_CPP_IMAGE_TAG=${shortCommit}"
                     }
             }
             stage('Build and test windows') {
@@ -119,6 +119,7 @@ pipeline {
             stage("Run unit tests") {
               steps {
                   sh "make run_unit_tests TEST_LLM_PATH=${HOME}/ovms_models/llm_models_ovms/INT8 BASE_OS=redhat OVMS_CPP_IMAGE_TAG=${shortCommit}"
+                  archiveArtifacts allowEmptyArchive: true, artifacts: "test_logs.tar.gz"
               }
             }
             stage("Internal tests") {
