471517af6c0bd205192af2641de6414933d5c91d
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2025-01-27 23:51:13 +0100

    skip cache on linux

--- ci/build_test_OnCommit.groovy
+++ ci/build_test_OnCommit.groovy
@@ -83,7 +83,7 @@ pipeline {
             stage("Build linux") {
               when { expression { image_build_needed == "true" } }
                 steps {
-                      sh "echo build --remote_cache=${env.OVMS_BAZEL_REMOTE_CACHE_URL} > .user.bazelrc"
+                      // sh "echo build --remote_cache=${env.OVMS_BAZEL_REMOTE_CACHE_URL} > .user.bazelrc"
                       sh "make ovms_builder_image RUN_TESTS=0 OPTIMIZE_BUILDING_TESTS=1 OV_USE_BINARY=1 BASE_OS=redhat OVMS_CPP_IMAGE_TAG=${shortCommit}"
                     }
             }
