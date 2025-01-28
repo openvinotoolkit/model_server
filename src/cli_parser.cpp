cafe6753465c2d0565137839fa9469b0aa8fc64c
Trawinski, Dariusz <dariusz.trawinski@intel.com>
2025-01-28 17:01:00 +0100

    Update src/cli_parser.cpp
    Co-authored-by: Adrian Tobiszewski <adrian.tobiszewski@intel.com>

--- src/cli_parser.cpp
+++ src/cli_parser.cpp
@@ -98,17 +98,16 @@ void CLIParser::parse(int argc, char** argv) {
                 "Time interval between two consecutive resources cleanup scans. Default is 1. Must be greater than 0.",
                 cxxopts::value<uint32_t>()->default_value("1"),
                 "CUSTOM_NODE_RESOURCES_CLEANER_INTERVAL_SECONDS")
+            ("cache_dir",
+                "Overrides model cache directory. By default cache files are saved into"
 #ifdef __linux__
-            ("cache_dir",
-                "Overrides model cache directory. By default cache files are saved into /opt/cache if the directory is present. When enabled, first model load will produce cache files.",
-                cxxopts::value<std::string>(),
-                "CACHE_DIR")
+                 "/opt/cache"
 #elif _WIN32
-            ("cache_dir",
-                "Overrides model cache directory. By default cache files are saved into c:\\Intel\\openvino_cache if the directory is present. When enabled, first model load will produce cache files.",
+                 "c:\\Intel\\openvino_cache"
+#endif
+                 " if the directory is present. When enabled, first model load will produce cache files.",
                 cxxopts::value<std::string>(),
                 "CACHE_DIR")
-#endif
             ("metrics_enable",
                 "Flag enabling metrics endpoint on rest_port.",
                 cxxopts::value<bool>()->default_value("false"),
