22fdea0e49500623bbc2bd2d348d005817dee56c
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2025-02-03 18:55:47 +0100

    fix

--- ci/build_test_OnCommit.groovy
+++ ci/build_test_OnCommit.groovy
@@ -123,9 +123,11 @@ pipeline {
                   sh "make run_unit_tests TEST_LLM_PATH=${HOME}/ovms_models/llm_models_ovms/INT8 BASE_OS=redhat OVMS_CPP_IMAGE_TAG=${shortCommit}"
                 }
               } finally {
+                  stage("Archive logs") {
                   archiveArtifacts allowEmptyArchive: true, artifacts: "test_logs.tar.gz"
                   archiveArtifacts allowEmptyArchive: true, artifacts: "linux_tests.log"
                 }
+              }
             }
             stage("Internal tests") {
               steps {
