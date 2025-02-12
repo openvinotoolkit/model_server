17517e963b5b5fa95d85095d84c62e2b7788007c
Damian Kalinowski <damian.kalinowski@intel.com>
2025-02-12 16:31:55 +0100

    clean

--- src/test/stress_test_utils.hpp
+++ src/test/stress_test_utils.hpp
@@ -1136,10 +1136,8 @@ public:
         it = ovmsConfig.find(pathToReplace);
         while (it != std::string::npos) {
             std::string newDir = getGenericFullPathForSrcTest("/ovms/src/test");
-            SPDLOG_ERROR("CCCCCCCCCCCCCCCCC {}", newDir);
             std::filesystem::path resolved_path = std::filesystem::absolute(newDir).lexically_normal();
             newDir = resolved_path.generic_string();
-            SPDLOG_ERROR("ABBBBBBBBBBBBBBBBBBBBBBBB {}", newDir);
             ovmsConfig.replace(it, pathToReplace.size(), newDir);
             it = ovmsConfig.find(pathToReplace);
         }
