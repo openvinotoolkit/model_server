104416d1e8071afce465bf35689280974d7fea40
michalkulakowski <michal.kulakowski@intel.com>
2025-03-03 18:28:56 +0100

    fix

--- src/llm/apis/openai_completions.cpp
+++ src/llm/apis/openai_completions.cpp
@@ -95,7 +95,7 @@ ov::Tensor load_image_stbi(const std::string& imageBytes) {
     constexpr int desiredChannels = 3;
     unsigned char* image = stbi_load_from_memory(
         (const unsigned char*)imageBytes.data(), imageBytes.size(),
-        &x, &y, channelsInFile, desiredChannels);
+        &x, &y, &channelsInFile, desiredChannels);
     if (!image) {
         std::stringstream errorMessage;
         errorMessage << "Failed to load the image";
