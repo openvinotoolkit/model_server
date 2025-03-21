122c433ca2b62b0640e77211fe4e2cd6904b6e37
michalkulakowski <michal.kulakowski@intel.com>
2025-03-21 09:39:32 +0100

    fix

--- src/llm/visual_language_model/legacy/servable_initializer.cpp
+++ src/llm/visual_language_model/legacy/servable_initializer.cpp
@@ -70,9 +70,8 @@ Status VisualLanguageModelLegacyServableInitializer::initialize(std::shared_ptr<
         return status;
     }
 
-    properties->pluginConfig["ATTENTION_BACKEND"] = "SDPA";
     try {
-        properties->pipeline = std::make_shared<ov::genai::VLMPipeline>(parsedModelsPath, properties->device, properties->pluginConfig);
+        properties->pipeline = std::make_shared<ov::genai::VLMPipeline>(parsedModelsPath, properties->device);
         properties->tokenizer = properties->pipeline->get_tokenizer();
     } catch (const std::exception& e) {
         SPDLOG_ERROR("Error during llm node initialization for models_path: {} exception: {}", parsedModelsPath, e.what());
