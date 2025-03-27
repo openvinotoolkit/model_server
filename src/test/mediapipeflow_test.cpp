8007731c86d6b6163b39f84d31ff6f482a4a6cc1
Rafal Sapala <rafal.a.sapala@intel.com>
2025-03-27 16:02:57 +0100

    Fix relative path (#3162)
    * Fix relative path
    * WR for relative on linux

--- src/test/mediapipeflow_test.cpp
+++ src/test/mediapipeflow_test.cpp
@@ -116,6 +116,21 @@ public:
     }
 };
 
+class MediapipeCliFlowTestDummyRelative : public MediapipeCliFlowTest {
+public:
+    std::filesystem::path originalCwd;
+    void SetUp() {
+        // Workaround for bazel test execution from /root/ or bazel-out directory
+        originalCwd = std::filesystem::current_path();
+#ifdef __linux__
+        std::filesystem::path newCwd = "/ovms";
+        std::filesystem::current_path(newCwd);
+#endif
+        SetUpServer("src/test/mediapipe/cli", "graphkfspass");
+        std::filesystem::current_path(originalCwd);
+    }
+};
+
 TEST_F(MediapipeCliFlowTestNegative, UnsupportedCliParamBatchSize) {
     server.setShutdownRequest(0);
     randomizePort(this->port);
@@ -138,6 +153,25 @@ TEST_F(MediapipeCliFlowTestNegative, UnsupportedCliParamBatchSize) {
 }
 
 TEST_F(MediapipeCliFlowTestDummy, Infer) {
+    const ovms::Module* grpcModule = server.getModule(ovms::GRPC_SERVER_MODULE_NAME);
+    KFSInferenceServiceImpl& impl = dynamic_cast<const ovms::GRPCServerModule*>(grpcModule)->getKFSGrpcImpl();
+    ::KFSRequest request;
+    ::KFSResponse response;
+    const std::string modelName = "graphkfspass";
+    request.Clear();
+    response.Clear();
+    inputs_info_t inputsMeta{
+        {"in", {DUMMY_MODEL_SHAPE, precision}}};
+    std::vector<float> requestData1{1., 1., 1., 1., 1., 1., 1., 1., 1., 1.};
+    std::vector<float> requestData2{0., 0., 0., 0., 0., 0., 0., 0., 0., 0.};
+    preparePredictRequest(request, inputsMeta, requestData1);
+    request.mutable_model_name()->assign(modelName);
+    ASSERT_EQ(impl.ModelInfer(nullptr, &request, &response).error_code(), grpc::StatusCode::OK);
+    // Checking that KFSPASS calculator copies requestData1 to the response so that we expect requestData1 on output
+    checkAddResponse("out", requestData1, requestData2, request, response, 1, 1, modelName);
+}
+
+TEST_F(MediapipeCliFlowTestDummyRelative, Infer) {
     const ovms::Module* grpcModule = server.getModule(ovms::GRPC_SERVER_MODULE_NAME);
     KFSInferenceServiceImpl& impl = dynamic_cast<const ovms::GRPCServerModule*>(grpcModule)->getKFSGrpcImpl();
     ::KFSRequest request;
