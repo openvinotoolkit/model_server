ae2cdb7f76680c199aba27407711742ed425c2e7
michalkulakowski <michal.kulakowski@intel.com>
2025-04-08 08:18:16 +0200

    Fix optical_character_recognition by changing model

--- demos/optical_character_recognition/python/optical_character_recognition.py
+++ demos/optical_character_recognition/python/optical_character_recognition.py
@@ -71,7 +71,7 @@ def decode(text):
         if character == last_character:
             continue
         elif character == '#':
-            last_character = None
+            return word
         else:
             last_character = character
             word += character
@@ -80,11 +80,12 @@ def decode(text):
 def text_recognition_output_to_text(output_nd):
     for i in range(output_nd.shape[0]):
         data = output_nd[i]
-        alphabet = '#1234567890abcdefghijklmnopqrstuvwxyz'
+        alphabet = '#0123456789abcdefghijklmnopqrstuvwxyz'
         preds = data.argmax(2)
+        print(preds)
         word = ''
-        for i in range(preds.shape[0]):
-            word += alphabet[preds[i,0]]
+        for i in range(preds.shape[1]):
+            word += alphabet[preds[0,i]]
         print(decode(word))
 
 
