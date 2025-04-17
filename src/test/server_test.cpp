db34d20ff83d4afc2abfee4d2399fac82e812391
Damian Kalinowski <damian.kalinowski@intel.com>
2025-04-17 11:25:28 +0200

    includes

--- src/test/server_test.cpp
+++ src/test/server_test.cpp
@@ -14,6 +14,7 @@
 // limitations under the License.
 //*****************************************************************************
 #include <chrono>
+#include <cstdlib>
 #include <random>
 #include <thread>
 
--- src/test/stress_test_utils.hpp
+++ src/test/stress_test_utils.hpp
@@ -17,6 +17,7 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <cstdlib>
 #include <memory>
 #include <regex>
 #include <set>
