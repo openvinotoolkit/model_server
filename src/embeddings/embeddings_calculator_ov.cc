4e90737c915abf6b4903c4c1b0b6510d409cba3b
michalkulakowski <michal.kulakowski@intel.com>
2025-05-29 14:45:25 +0200

    embeddings

---
+++ src/embeddings/embeddings_calculator_ov.cc
@@ -0,0 +1,279 @@
+//*****************************************************************************
+// Copyright 2024 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//*****************************************************************************
+#include <string>
+#include <unordered_map>
+
+#pragma warning(push)
+#pragma warning(disable : 6001 6385 6386 6326 6011 4309 6246 4005 4456)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/port/canonical_errors.h"
+#include "mediapipe/framework/port/ret_check.h"
+#pragma GCC diagnostic pop
+#pragma warning(pop)
+
+#include <adapters/inference_adapter.h>
+#pragma warning(push)
+#pragma warning(disable : 6313)
+#include <rapidjson/writer.h>
+#pragma warning(pop)
+
+#include "../http_payload.hpp"
+#include "../logging.hpp"
+#include "../precision.hpp"
+#include "../profiler.hpp"
+#include "../executingstreamidguard.hpp"
+#include "../model_metric_reporter.hpp"
+#include "embeddings_api.hpp"
+#include "src/embeddings/embeddings_calculator_ov.pb.h"
+#include "embeddings_servable.hpp"
+
+
+
+using namespace rapidjson;
+using namespace ovms;
+class EmbeddingsServable;
+
+namespace mediapipe {
+
+const std::string EMBEDDINGS_SESSION_SIDE_PACKET_TAG = "EMBEDDINGS_NODE_RESOURCES";
+
+using InputDataType = ovms::HttpPayload;
+using OutputDataType = std::string;
+
+class EmbeddingsCalculatorOV : public CalculatorBase {
+    static const std::string INPUT_TAG_NAME;
+    static const std::string OUTPUT_TAG_NAME;
+    static const std::string EMBEDDINGS_MODEL_INPUT_IDS_NAME;
+    static const std::string EMBEDDINGS_MODEL_ATTENTION_MASK_NAME;
+    static const std::string EMBEDDINGS_MODEL_TOKEN_TYPE_IDS_NAME;
+
+    mediapipe::Timestamp timestamp{0};
+
+protected:
+    //std::shared_ptr<::InferenceAdapter> tokenizer_session{nullptr};
+    std::shared_ptr<ovms::EmbeddingsServable> embeddings_session{nullptr};
+
+public:
+    static absl::Status GetContract(CalculatorContract* cc) {
+        RET_CHECK(!cc->Inputs().GetTags().empty());
+        RET_CHECK(!cc->Outputs().GetTags().empty());
+        cc->Inputs().Tag(INPUT_TAG_NAME).Set<InputDataType>();
+        cc->Outputs().Tag(OUTPUT_TAG_NAME).Set<OutputDataType>();
+        //cc->InputSidePackets().Tag("TOKENIZER_SESSION").Set<std::shared_ptr<InferenceAdapter>>();
+        cc->InputSidePackets().Tag(EMBEDDINGS_SESSION_SIDE_PACKET_TAG).Set<ovms::EmbeddingsServableMap>();
+        return absl::OkStatus();
+    }
+
+    absl::Status Close(CalculatorContext* cc) final {
+        OVMS_PROFILE_FUNCTION();
+        SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "EmbeddingsCalculatorOV [Node: {} ] Close", cc->NodeName());
+        return absl::OkStatus();
+    }
+
+    absl::Status Open(CalculatorContext* cc) final {
+        OVMS_PROFILE_FUNCTION();
+        auto servableMap = cc->InputSidePackets()
+                               .Tag(EMBEDDINGS_SESSION_SIDE_PACKET_TAG)
+                               .Get<ovms::EmbeddingsServableMap>();
+        auto it = servableMap.find(cc->NodeName());
+        SPDLOG_ERROR("SIZE: {}", servableMap.size());
+        RET_CHECK(it != servableMap.end()) << "Could not find initialized Embeddings node named: " << cc->NodeName();
+        embeddings_session = it->second;
+        SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "EmbeddingsCalculatorOV  [Node: {}] Open start", cc->NodeName());
+
+        SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "EmbeddingsCalculatorOV [Node: {}] Open end", cc->NodeName());
+
+        return absl::OkStatus();
+    }
+
+    absl::Status Process(CalculatorContext* cc) final {
+        OVMS_PROFILE_FUNCTION();
+        // RET_CHECK(tokenizer_session != nullptr);
+        RET_CHECK(embeddings_session != nullptr);
+        if (cc->Inputs().Tag(INPUT_TAG_NAME).IsEmpty()) {
+            return absl::InvalidArgumentError("Input is empty");
+        }
+        InputDataType payload = cc->Inputs().Tag(INPUT_TAG_NAME).Get<InputDataType>();
+        SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "Request body: {}", payload.body);
+        SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "Request uri: {}", payload.uri);
+        ovms::EmbeddingsHandler handler(*payload.parsedJson);
+        auto parseRequestStartTime = std::chrono::high_resolution_clock::now();
+        absl::Status status = handler.parseRequest();
+        if (!status.ok()) {
+            return status;
+        }
+        double time = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - parseRequestStartTime).count();
+        SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "Embeddings request deserialization time: {} ms", time / 1000);
+
+        ov::Tensor embeddingsTensor;
+        try {
+            auto input = handler.getInput();
+            if (auto strings = std::get_if<std::vector<std::string>>(&input)) {
+                auto tokens = embeddings_session->getTokenizer().encode(*strings);
+                RET_CHECK(tokens.input_ids.get_shape().size() == 2);
+                size_t input_ids_size = tokens.input_ids.get_shape()[1];
+                size_t max_context_length = 1024;  // default allowed input length. Otherwise, it will be read from model config.json file
+                if(embeddings_session->getMaxModelLength().has_value()){
+                    max_context_length = embeddings_session->getMaxModelLength().value();
+                }
+                else{
+                    SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "max_position_embeddings nor max_trained_positions included in config.json. Using default value {}", max_context_length);
+                }
+                SPDLOG_ERROR("MAX_CONTENT_LENGTH {} input_ids_size {}", max_context_length, input_ids_size);
+                if (input_ids_size > max_context_length) {
+                    SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "Input size {} exceeds max_context_length {}", input_ids_size, max_context_length);
+                    return absl::InvalidArgumentError(absl::StrCat("Input length ", input_ids_size, " longer than allowed ", max_context_length));
+                }
+                ModelMetricReporter tmp(nullptr, nullptr, "example_pipeline_name", 1);
+                auto executingStreamIdGuard = std::make_shared<ExecutingStreamIdGuard>(embeddings_session->getEmbeddingsInferRequestsQueue(), tmp);
+                ov::InferRequest& inferRequest = executingStreamIdGuard->getInferRequest();
+                inferRequest.set_tensor("input_ids", tokens.input_ids);
+                inferRequest.set_tensor("attention_mask", tokens.attention_mask);
+
+                size_t attendedTokens = 0;
+                if (tokens.attention_mask.get_element_type() == ov::element::Type_t::i64) {
+                    for (int i = 0; i < tokens.attention_mask.get_size(); i++) {
+                        attendedTokens += reinterpret_cast<int64_t*>(tokens.attention_mask.data())[i];
+                    }
+                } else if (tokens.attention_mask.get_element_type() == ov::element::Type_t::i32) {
+                    for (int i = 0; i < tokens.attention_mask.get_size(); i++) {
+                        attendedTokens += reinterpret_cast<int32_t*>(tokens.attention_mask.data())[i];
+                    }
+                } else {
+                    for (int i = 0; i < tokens.attention_mask.get_byte_size(); i++) {
+                        attendedTokens += reinterpret_cast<uint8_t*>(tokens.attention_mask.data())[i];
+                    }
+                }
+                handler.setPromptTokensUsage(attendedTokens);
+                for (auto& input : inferRequest.get_compiled_model().inputs()) {
+                    if (input.get_any_name() == "token_type_ids") {
+                        ov::Tensor token_type_ids{ov::element::i64, tokens.input_ids.get_shape()};
+                        std::fill_n(token_type_ids.data<int64_t>(), tokens.attention_mask.get_size(), 0);
+                        inferRequest.set_tensor("token_type_ids", token_type_ids);
+                        break;
+                    }
+                }
+                inferRequest.start_async();
+                inferRequest.wait();
+                embeddingsTensor = inferRequest.get_tensor("token_embeddings");
+            }
+            else if (auto tokenized_documents = std::get_if<std::vector<std::vector<int64_t>>>(&input)) {
+                ModelMetricReporter tmp(nullptr, nullptr, "example_pipeline_name", 1);
+                auto executingStreamIdGuard = std::make_shared<ExecutingStreamIdGuard>(embeddings_session->getEmbeddingsInferRequestsQueue(), tmp);
+                ov::InferRequest& inferRequest = executingStreamIdGuard->getInferRequest();
+                auto received_batch_size = tokenized_documents->size();
+                size_t tokens = 0;
+                size_t token_count_of_longest_document = 0;
+                for (const auto& document_tokens : *tokenized_documents) {
+                    token_count_of_longest_document = std::max(token_count_of_longest_document, document_tokens.size());
+                    tokens += document_tokens.size();
+                }
+                handler.setPromptTokensUsage(tokens);
+                auto inputsIds = ov::Tensor{
+                    ov::element::i64,
+                    ov::Shape{received_batch_size, token_count_of_longest_document}};
+                auto attentionMask = ov::Tensor{
+                    ov::element::i64,
+                    ov::Shape{received_batch_size, token_count_of_longest_document}};
+                try {
+                    for (size_t i = 0; i < received_batch_size; i++) {
+                        int64_t* input_ids_start = reinterpret_cast<int64_t*>(inputsIds.data()) + i * token_count_of_longest_document;
+                        std::fill(input_ids_start, input_ids_start + token_count_of_longest_document, embeddings_session->getPadToken());
+                        std::copy(tokenized_documents->at(i).data(), tokenized_documents->at(i).data() + tokenized_documents->at(i).size(), input_ids_start);
+
+                        int64_t* attention_mask_start = reinterpret_cast<int64_t*>(attentionMask.data()) + i * token_count_of_longest_document;
+                        std::fill(attention_mask_start, attention_mask_start + token_count_of_longest_document, 0);
+                        std::fill(attention_mask_start, attention_mask_start + tokenized_documents->at(i).size(), 1);
+                    }
+                } catch (std::out_of_range& e) {
+                    SPDLOG_DEBUG("Caught exception from preparing embeddings inputs(): {}", e.what());
+                } catch (std::exception& e) {
+                    SPDLOG_DEBUG("Caught generic exception from preparing embeddings inputs: {}", e.what());
+                }
+                //EMBEDDINGS_MODEL_TOKEN_TYPE_IDS_NAME ??
+                auto typeIds = ov::Tensor{ov::element::i64, ov::Shape{received_batch_size, token_count_of_longest_document}};
+                int64_t* token_type_ids_start = reinterpret_cast<int64_t*>(typeIds.data());
+                std::fill(token_type_ids_start, token_type_ids_start + received_batch_size * token_count_of_longest_document, 1);
+                inferRequest.set_tensor("input_ids", inputsIds);
+                inferRequest.set_tensor("attention_mask", attentionMask);
+                // if(typeIds.get_size() > 0){
+                //     m_request.set_tensor("token_type_ids", typeIds);
+                // }
+                for (auto& input : inferRequest.get_compiled_model().inputs()) {
+                    if (input.get_any_name() == "token_type_ids") {
+                        ov::Tensor token_type_ids{ov::element::i64, inputsIds.get_shape()};
+                        std::fill_n(token_type_ids.data<int64_t>(), attentionMask.get_size(), 0);
+                        inferRequest.set_tensor("token_type_ids", token_type_ids);
+                        break;
+                    }
+                }
+                inferRequest.start_async();
+                inferRequest.wait();
+                embeddingsTensor = inferRequest.get_tensor("token_embeddings");
+            }
+        } catch (const std::exception& e) {
+            SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "Caught exception from session infer(): {}", e.what());
+            LOG(INFO) << e.what();
+            RET_CHECK(false);
+        } catch (...) {
+            SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "Caught unknown exception from session infer()");
+            RET_CHECK(false);
+        }
+        // if (embeddingsOutputMap.size() == 2) {  // GTE
+        //     // Search by number of dimensions, should be 3
+        //     bool found = false;
+        //     for (const auto& [name, tensor] : embeddingsOutputMap) {
+        //         if (tensor.get_shape().size() == 3) {
+        //             embeddingsTensor = tensor;
+        //             SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "Multiple embedding model outputs found, 3-dim output with name {} will be used", name);
+        //             found = true;
+        //             break;
+        //         }
+        //     }
+        //     RET_CHECK(found);
+        // } else {  // BGE
+        //     RET_CHECK(embeddingsOutputMap.size() == 1);
+        //     embeddingsTensor = embeddingsOutputMap.begin()->second;
+        //     SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "Single embedding model output found with name {}", embeddingsOutputMap.begin()->first);
+        // }
+
+        // RET_CHECK(embeddingsTensor.get_shape().size() == 3);
+        // RET_CHECK(embeddingsTensor.get_shape()[0] == received_batch_size);
+        // RET_CHECK(embeddingsTensor.get_element_type() == ov::element::f32);
+
+        auto parseResponseStartTime = std::chrono::high_resolution_clock::now();
+        StringBuffer buffer;
+        status = handler.parseResponse(buffer, embeddingsTensor, cc->Options<EmbeddingsCalculatorOVOptions>().normalize_embeddings());
+        if (!status.ok()) {
+            return status;
+        }
+        time = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - parseResponseStartTime).count();
+        SPDLOG_LOGGER_DEBUG(embeddings_calculator_logger, "Embeddings response deserialization time: {} ms", time / 1000);
+        cc->Outputs().Tag(OUTPUT_TAG_NAME).Add(new std::string(buffer.GetString()), timestamp);
+        return absl::OkStatus();
+    }
+};
+const std::string EmbeddingsCalculatorOV::INPUT_TAG_NAME{"REQUEST_PAYLOAD"};
+const std::string EmbeddingsCalculatorOV::OUTPUT_TAG_NAME{"RESPONSE_PAYLOAD"};
+const std::string EmbeddingsCalculatorOV::EMBEDDINGS_MODEL_INPUT_IDS_NAME{"input_ids"};
+const std::string EmbeddingsCalculatorOV::EMBEDDINGS_MODEL_ATTENTION_MASK_NAME{"attention_mask"};
+const std::string EmbeddingsCalculatorOV::EMBEDDINGS_MODEL_TOKEN_TYPE_IDS_NAME{"token_type_ids"};
+
+REGISTER_CALCULATOR(EmbeddingsCalculatorOV);
+
+}  // namespace mediapipe
