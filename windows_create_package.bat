1f755cd44a3c9896e631a740a9e3dba06cf5327f
michalkulakowski <michal.kulakowski@intel.com>
2025-06-09 17:20:38 +0200

    Upgrade windows libcurl

--- windows_create_package.bat
+++ windows_create_package.bat
@@ -139,25 +139,25 @@ copy %cd%\release_files\thirdparty-licenses\* %license_dest%
 if !errorlevel! neq 0 exit /b !errorlevel!
 
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy C:\opt\curl-8.13.0_1-win64-mingw\COPYING.txt %license_dest%LICENSE-CURL.txt
+copy C:\opt\curl-8.14.1_1-win64-mingw\COPYING.txt %license_dest%LICENSE-CURL.txt
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy C:\opt\curl-8.13.0_1-win64-mingw\dep\brotli\LICENSE.txt %license_dest%LICENSE-BROTIL.txt
+copy C:\opt\curl-8.14.1_1-win64-mingw\dep\brotli\LICENSE.txt %license_dest%LICENSE-BROTIL.txt
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy C:\opt\curl-8.13.0_1-win64-mingw\dep\cacert\LICENSE.url %license_dest%LICENSE-CACERT.url
+copy C:\opt\curl-8.14.1_1-win64-mingw\dep\cacert\LICENSE.url %license_dest%LICENSE-CACERT.url
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy C:\opt\curl-8.13.0_1-win64-mingw\dep\libpsl\COPYING.txt %license_dest%LICENSE-LIBPSL.txt
+copy C:\opt\curl-8.14.1_1-win64-mingw\dep\libpsl\COPYING.txt %license_dest%LICENSE-LIBPSL.txt
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy C:\opt\curl-8.13.0_1-win64-mingw\dep\libressl\COPYING.txt %license_dest%LICENSE-LIBRESSL.txt
+copy C:\opt\curl-8.14.1_1-win64-mingw\dep\libressl\COPYING.txt %license_dest%LICENSE-LIBRESSL.txt
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy C:\opt\curl-8.13.0_1-win64-mingw\dep\libssh2\COPYING.txt %license_dest%LICENSE-LIBSSH2.txt
+copy C:\opt\curl-8.14.1_1-win64-mingw\dep\libssh2\COPYING.txt %license_dest%LICENSE-LIBSSH2.txt
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy C:\opt\curl-8.13.0_1-win64-mingw\dep\nghttp2\COPYING.txt %license_dest%LICENSE-NGHTTP2.txt
+copy C:\opt\curl-8.14.1_1-win64-mingw\dep\nghttp2\COPYING.txt %license_dest%LICENSE-NGHTTP2.txt
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy C:\opt\curl-8.13.0_1-win64-mingw\dep\nghttp3\COPYING.txt %license_dest%LICENSE-NGHTTP3.txt
+copy C:\opt\curl-8.14.1_1-win64-mingw\dep\nghttp3\COPYING.txt %license_dest%LICENSE-NGHTTP3.txt
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy C:\opt\curl-8.13.0_1-win64-mingw\dep\zlib\LICENSE.txt %license_dest%LICENSE-ZLIB.txt
+copy C:\opt\curl-8.14.1_1-win64-mingw\dep\zlib\LICENSE.txt %license_dest%LICENSE-ZLIB.txt
 if !errorlevel! neq 0 exit /b !errorlevel!
-copy C:\opt\curl-8.13.0_1-win64-mingw\dep\zstd\LICENSE.txt %license_dest%LICENSE-ZSTD.txt
+copy C:\opt\curl-8.14.1_1-win64-mingw\dep\zstd\LICENSE.txt %license_dest%LICENSE-ZSTD.txt
 
 :: Add when CAPI enabled and tested
 ::mkdir -vp /ovms_release/include && cp /ovms/src/ovms.h /ovms_release/include
