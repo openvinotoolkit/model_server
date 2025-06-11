7f6afdc2ea8899f9bfbea369724d4e777cfc60cb
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2025-06-11 15:18:41 +0200

    updates

--- demos/continuous_batching/long_context/custom_dataset.py
+++ demos/continuous_batching/long_context/custom_dataset.py
@@ -1,11 +1,28 @@
-# This script generates a dataset of long context examples for training and evaluation.
+#
+# Copyright (c) 2025 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+# This script generates a dataset of long context examples for performance evaluation
 import os
 import json
 import requests
 from transformers import AutoTokenizer
 import argparse
 
-# function to download a file from a URL
+# function to download a file from a URL and convert it to text
 def download_file(url):
     output_path = os.path.basename(url)
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
