07b1619c4a866ec6f595b6e1caa2eead21001b20
pgladkows <patrycja.gladkowska@intel.com>
2025-06-17 13:57:42 +0200

    updates in workspace

--- ci/build_test_OnCommitWin.groovy
+++ ci/build_test_OnCommitWin.groovy
@@ -9,9 +9,8 @@ pipeline {
         stage ("Build and test windows") {
             steps {
                 script {
-                    echo "job base name: ${env.JOB_BASE_NAME}"
-                    echo "tt job name: ${env.TT_USE_JENKINS_JOB_NAME}"
-                    echo "workspace: ${env.WORKSPACE}"
+                    echo "JOB_BASE_NAME: ${env.JOB_BASE_NAME}"
+                    echo "WORKSPACE: ${env.WORKSPACE}"
                     def windows = load 'ci/loadWin.groovy'
                     if (windows != null) {
                         try {
@@ -23,19 +22,12 @@ pipeline {
                           windows.check_tests()
                           def safeBranchName = env.BRANCH_NAME.replaceAll('/', '_')
                           def python_presence = ""
-                          def workspace_name = ""
                           if (env.OVMS_PYTHON_ENABLED) {
                               python_presence = "with_python"
                           } else {
                               python_presence = "without_python"
                           }
-                          if (env.TT_USE_JENKINS_JOB_NAME) {
-                              workspace_name = env.WORKSPACE
-                          } else {
-                              workspace_name = "${env.WORKSPACE}_${safeBranchName}"
-                          }
-                          echo "workspace name: ${workspace_name}"
-                          bat(returnStatus:true, script: "ECHO F | xcopy /Y /E ${workspace_name}\\dist\\windows\\ovms.zip \\\\${env.OV_SHARE_05_IP}\\data\\cv_bench_cache\\OVMS_do_not_remove\\ovms-windows-${python_presence}-${safeBranchName}-latest.zip")
+                          bat(returnStatus:true, script: "ECHO F | xcopy /Y /E ${env.WORKSPACE}\\dist\\windows\\ovms.zip \\\\${env.OV_SHARE_05_IP}\\data\\cv_bench_cache\\OVMS_do_not_remove\\ovms-windows-${python_presence}-${safeBranchName}-latest.zip")
                           } finally {
                           windows.archive_build_artifacts()
                           windows.archive_test_artifacts()
