2bf0766a9cf3121ded401ad26cd5d25c0b9216c6
mzegla <milosz.zeglarski@intel.com>
2025-07-04 10:03:10 +0200

    update

--- windows_install_build_dependencies.bat
+++ windows_install_build_dependencies.bat
@@ -126,7 +126,7 @@ IF /I EXIST %bash_path% (
 ::::::::::::::::::::::: GENAI/OPENVINO - reinstalled per build trigger
 :: Set default GENAI_PACKAGE_URL if not set
 if "%GENAI_PACKAGE_URL%"=="" (
-    set "GENAI_PACKAGE_URL=https://storage.openvinotoolkit.org/repositories/openvino_genai/packages/nightly/2025.3.0.0.dev20250619/openvino_genai_windows_2025.3.0.0.dev20250619_x86_64.zip"
+    set "GENAI_PACKAGE_URL=https://storage.openvinotoolkit.org/repositories/openvino_genai/packages/nightly/2025.3.0.0.dev20250703/openvino_genai_windows_2025.3.0.0.dev20250703_x86_64.zip"
 )
 
 :: Extract genai_ver from GENAI_PACKAGE_URL (filename)
