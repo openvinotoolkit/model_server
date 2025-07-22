f92dd9ca418a2b0c8d7f1c35bfe964ef035693ed
Raghav <take2home241@gmail.com>
2025-07-22 00:42:12 -0400

    Conversion scripts for dino and laion models to openvino IR

---
+++ demos/python_demos/image_embeddings/model_conversion/dino_conversion.py
@@ -0,0 +1,27 @@
+from transformers import AutoImageProcessor, AutoModel
+from PIL import Image
+import openvino as ov
+import os
+
+model_id="facebook/dinov2-base"
+print(f"Downloading pretrained model {model_id}...")
+
+model=AutoModel.from_pretrained(model_id)
+processor=AutoImageProcessor.from_pretrained(model_id)
+
+image=Image.new("RGB",(224,224))
+inputs=processor(images=image,return_tensors="pt")["pixel_values"]
+
+print("Converting models...")
+ov_model=ov.convert_model(model,example_input=inputs)
+ov.save_model(ov_model,"dino_image_encoder.xml")
+print("Model saved!")
+
+mod_path="saved_mod/dino/1"
+os.makedirs(mod_path,exist_ok=True)
+os.replace("dino_image_encoder.xml", f"{mod_path}/dino_image_encoder.xml")
+os.replace("dino_image_encoder.bin", f"{mod_path}/dino_image_encoder.bin")
+print("Model ready for OVMS")
+
+
+
