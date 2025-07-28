24677ee3f70cc72e320a0edf63acab8ee42b1d94
rasapala <rafal.a.sapala@intel.com>
2025-07-28 14:44:38 +0200

    Fixing compilation

--- src/pull_module/libgit2.cpp
+++ src/pull_module/libgit2.cpp
@@ -22,6 +22,7 @@
 #include <assert.h>
 #include <fcntl.h>
 #include <git2.h>
+#include <git2_cli/git2/progress.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -45,62 +46,6 @@
 #endif
 
 namespace ovms {
-
-// C-style callback functions section used in libgt2 library STARTS ********************************
-typedef struct progress_data {
-    git_indexer_progress fetch_progress;
-    size_t completed_steps;
-    size_t total_steps;
-    const char* path;
-} progress_data;
-
-static void print_progress(const progress_data* pd) {
-    int network_percent = pd->fetch_progress.total_objects > 0 ? (100 * pd->fetch_progress.received_objects) / pd->fetch_progress.total_objects : 0;
-    int index_percent = pd->fetch_progress.total_objects > 0 ? (100 * pd->fetch_progress.indexed_objects) / pd->fetch_progress.total_objects : 0;
-
-    int checkout_percent = pd->total_steps > 0
-                               ? (int)((100 * pd->completed_steps) / pd->total_steps)
-                               : 0;
-    size_t kbytes = pd->fetch_progress.received_bytes / 1024;
-
-    if (pd->fetch_progress.total_objects &&
-        pd->fetch_progress.received_objects == pd->fetch_progress.total_objects) {
-        printf("Resolving deltas %u/%u\r",
-            pd->fetch_progress.indexed_deltas,
-            pd->fetch_progress.total_deltas);
-    } else {
-        printf("net %3d%% (%4" PRIuZ " kb, %5u/%5u)  /  idx %3d%% (%5u/%5u)  /  chk %3d%% (%4" PRIuZ "/%4" PRIuZ ")%s\n",
-            network_percent, kbytes,
-            pd->fetch_progress.received_objects, pd->fetch_progress.total_objects,
-            index_percent, pd->fetch_progress.indexed_objects, pd->fetch_progress.total_objects,
-            checkout_percent,
-            pd->completed_steps, pd->total_steps,
-            pd->path);
-    }
-}
-
-static int sideband_progress(const char* str, int len, void* payload) {
-    (void)payload;  //  unused
-
-    printf("remote: %.*s", len, str);
-    fflush(stdout);
-    return 0;
-}
-
-static int fetch_progress(const git_indexer_progress* stats, void* payload) {
-    progress_data* pd = (progress_data*)payload;
-    pd->fetch_progress = *stats;
-    print_progress(pd);
-    return 0;
-}
-
-static void checkout_progress(const char* path, size_t cur, size_t tot, void* payload) {
-    progress_data* pd = (progress_data*)payload;
-    pd->completed_steps = cur;
-    pd->total_steps = tot;
-    pd->path = path;
-    print_progress(pd);
-}
 
 // Callback for clone authentication - will be used when password is not set in repo_url
 // Does not work with LFS download as it requires additional authentication when password is not set in repository url
@@ -318,20 +263,20 @@ Status HfDownloader::cloneRepository() {
     }
 
     SPDLOG_DEBUG("Downloading to path: {}", this->downloadPath);
-    progress_data pd = {{0}};
+    cli_progress progress = CLI_PROGRESS_INIT;
     git_repository* cloned_repo = NULL;
     git_clone_options clone_opts = GIT_CLONE_OPTIONS_INIT;
     git_checkout_options checkout_opts = GIT_CHECKOUT_OPTIONS_INIT;
 
     /* Set up options */
     checkout_opts.checkout_strategy = GIT_CHECKOUT_SAFE;
-    checkout_opts.progress_cb = checkout_progress;
-    checkout_opts.progress_payload = &pd;
+    checkout_opts.progress_cb = cli_progress_checkout;
+    checkout_opts.progress_payload = &progress;
     clone_opts.checkout_opts = checkout_opts;
-    clone_opts.fetch_opts.callbacks.sideband_progress = sideband_progress;
-    clone_opts.fetch_opts.callbacks.transfer_progress = &fetch_progress;
+    clone_opts.fetch_opts.callbacks.sideband_progress = cli_progress_fetch_sideband;
+    clone_opts.fetch_opts.callbacks.transfer_progress = cli_progress_fetch_transfer;
     clone_opts.fetch_opts.callbacks.credentials = cred_acquire_cb;
-    clone_opts.fetch_opts.callbacks.payload = &pd;
+    clone_opts.fetch_opts.callbacks.payload = &progress;
 
     // Use proxy
     if (CheckIfProxySet()) {
