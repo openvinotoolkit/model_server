72f59117885c178a81418e61dcf6f394d06fc5c3
Rafal Sapala <rafal.a.sapala@intel.com>
2025-08-26 15:44:35 +0200

    IFDEF

--- src/llm/servable.cpp
+++ src/llm/servable.cpp
@@ -91,11 +91,13 @@ absl::Status GenAiServable::parseRequest(std::shared_ptr<GenAiServableExecutionC
         executionContext->generationConfigBuilder->unsetStructuredOutputConfig();
     }
 
+#if (PYTHON_DISABLE == 0)
     // Add gguf stop token if ggufEosToken optional has value in the properties
     if (this->getProperties()->ggufEosToken.has_value()) {
         SPDLOG_LOGGER_TRACE(llm_calculator_logger, "Adding GGUF model stop string: [{}]", this->getProperties()->ggufEosToken.value());
         executionContext->generationConfigBuilder->addStopString(this->getProperties()->ggufEosToken.value());
     }
+#endif
     return absl::OkStatus();
 }
 
