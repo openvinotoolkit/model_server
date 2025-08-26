79356dc9646c33ec5b225d735cca69eb7b280cf7
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2025-08-26 12:41:29 +0200

    tab

--- install_ubuntu_gpu_drivers.sh
+++ install_ubuntu_gpu_drivers.sh
@@ -57,7 +57,7 @@ case $INSTALL_DRIVER_VERSION in \
 	curl -L -O https://github.com/intel/compute-runtime/releases/download/25.31.34666.3/libze-intel-gpu1_25.31.34666.3-0_amd64.deb; \
 	curl -L -O https://github.com/intel/compute-runtime/releases/download/25.31.34666.3/intel-opencl-icd_25.31.34666.3-0_amd64.deb; \
 	curl -L -O https://github.com/intel/compute-runtime/releases/download/25.31.34666.3/libigdgmm12_22.8.1_amd64.deb; \
-        curl -L -O https://github.com/intel/compute-runtime/releases/download/25.31.34666.3/intel-ocloc_25.31.34666.3-0_amd64.deb; \
+	curl -L -O https://github.com/intel/compute-runtime/releases/download/25.31.34666.3/intel-ocloc_25.31.34666.3-0_amd64.deb; \
 	curl -L -O https://github.com/intel/intel-graphics-compiler/releases/download/v2.16.0/intel-igc-core-2_2.16.0+19683_amd64.deb; \
 	curl -L -O https://github.com/intel/intel-graphics-compiler/releases/download/v2.16.0/intel-igc-opencl-2_2.16.0+19683_amd64.deb; \
 	dpkg -i *.deb && rm -Rf /tmp/gpu_deps ; \
