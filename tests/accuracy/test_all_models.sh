030c765d3a19e64b640ce58d125a284a60a53c8b
Your Name <you@example.com>
2025-09-02 15:21:39 +0200

    tests automation improvements

--- tests/accuracy/test_all_models.sh
+++ tests/accuracy/test_all_models.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 # install dependencies
-# pip install -r https://raw.githubusercontent.com/openvinotoolkit/model_server/refs/heads/${BRANCH_NAME}/demos/common/export_models/requirements.txt
-# rm -rf gorilla
-# git clone https://github.com/ShishirPatil/gorilla
-# cd gorilla/berkeley-function-call-leaderboard
-# git checkout cd9429ccf3d4d04156affe883c495b3b047e6b64
-# curl -s https://raw.githubusercontent.com/openvinotoolkit/model_server/refs/heads/${BRANCH_NAME}/demos/continuous_batching/accuracy/gorilla.patch | git apply -v
-# pip install -e . 
-# cd ../..
-# curl -L -O https://raw.githubusercontent.com/openvinotoolkit/model_server/refs/heads/${BRANCH_NAME}/demos/common/export_models/export_model.py
-# mkdir -p models
+pip install -r https://raw.githubusercontent.com/openvinotoolkit/model_server/refs/heads/${BRANCH_NAME}/demos/common/export_models/requirements.txt
+rm -rf gorilla
+git clone https://github.com/ShishirPatil/gorilla
+cd gorilla/berkeley-function-call-leaderboard
+git checkout cd9429ccf3d4d04156affe883c495b3b047e6b64
+curl -s https://raw.githubusercontent.com/openvinotoolkit/model_server/refs/heads/${BRANCH_NAME}/demos/continuous_batching/accuracy/gorilla.patch | git apply -v
+pip install -e . 
+cd ../..
+curl -L -O https://raw.githubusercontent.com/openvinotoolkit/model_server/refs/heads/${BRANCH_NAME}/demos/common/export_models/export_model.py
+mkdir -p models
 
 # Qwen/Qwen3-8B
 python export_model.py text_generation --source_model Qwen/Qwen3-8B --model_name Qwen/Qwen3-8B-int4 --weight-format int4 --model_repository_path models
@@ -20,6 +20,14 @@ python export_model.py text_generation --source_model Qwen/Qwen3-8B --model_name
 python export_model.py text_generation --source_model Qwen/Qwen3-4B --model_name Qwen/Qwen3-4B-int4 --weight-format int4 --model_repository_path models
 python export_model.py text_generation --source_model Qwen/Qwen3-4B --model_name Qwen/Qwen3-4B-int8 --weight-format int8 --model_repository_path models
 python export_model.py text_generation --source_model Qwen/Qwen3-4B --model_name Qwen/Qwen3-4B-fp16 --weight-format fp16 --model_repository_path models
+# Qwen/Qwen3-1.7B
+python export_model.py text_generation --source_model Qwen/Qwen3-1.7B --model_name Qwen/Qwen3-1.7B-int4 --weight-format int4 --model_repository_path models
+python export_model.py text_generation --source_model Qwen/Qwen3-1.7B --model_name Qwen/Qwen3-1.7B-int8 --weight-format int8 --model_repository_path models
+python export_model.py text_generation --source_model Qwen/Qwen3-1.7B --model_name Qwen/Qwen3-1.7B-fp16 --weight-format fp16 --model_repository_path models
+# Qwen/Qwen3-0.6B
+python export_model.py text_generation --source_model Qwen/Qwen3-0.6B --model_name Qwen/Qwen3-0.6B-int4 --weight-format int4 --model_repository_path models
+python export_model.py text_generation --source_model Qwen/Qwen3-0.6B --model_name Qwen/Qwen3-0.6B-int8 --weight-format int8 --model_repository_path models
+python export_model.py text_generation --source_model Qwen/Qwen3-0.6B --model_name Qwen/Qwen3-0.6B-fp16 --weight-format fp16 --model_repository_path models
 # meta-llama/Llama-3.1-8B-Instruct
 python export_model.py text_generation --source_model meta-llama/Llama-3.1-8B-Instruct --model_name meta-llama/Llama-3.1-8B-Instruct-int4 --weight-format int4 --model_repository_path models
 curl -L -o models/meta-llama/Llama-3.1-8B-Instruct-int4/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_llama3.1_json.jinja
@@ -50,11 +58,11 @@ python export_model.py text_generation --source_model microsoft/Phi-4-mini-instr
 curl -L -o models/microsoft/Phi-4-mini-instruct-fp16/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_phi4_mini.jinja
 # mistralai/Mistral-7B-Instruct-v0.3
 python export_model.py text_generation --source_model mistralai/Mistral-7B-Instruct-v0.3 --model_name mistralai/Mistral-7B-Instruct-v0.3-int4 --weight-format int4 --config_file_path models/config.json --model_repository_path models --extra_quantization_params "--task text-generation-with-past"
-curl -L -o models/mistralai/Mistral-7B-Instruct-v0.3-int4/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_mistral.jinja
+curl -L -o models/mistralai/Mistral-7B-Instruct-v0.3-int4/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_mistral_parallel.jinja
 python export_model.py text_generation --source_model mistralai/Mistral-7B-Instruct-v0.3 --model_name mistralai/Mistral-7B-Instruct-v0.3-int8 --weight-format int8 --config_file_path models/config.json --model_repository_path models --tool_parser mistral --cache_size 2 --extra_quantization_params "--task text-generation-with-past"
-curl -L -o models/mistralai/Mistral-7B-Instruct-v0.3-int8/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_mistral.jinja
+curl -L -o models/mistralai/Mistral-7B-Instruct-v0.3-int8/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_mistral_parallel.jinja
 python export_model.py text_generation --source_model mistralai/Mistral-7B-Instruct-v0.3 --model_name mistralai/Mistral-7B-Instruct-v0.3-fp16 --weight-format fp16 --config_file_path models/config.json --model_repository_path models --tool_parser mistral --cache_size 2 --extra_quantization_params "--task text-generation-with-past"
-curl -L -o models/mistralai/Mistral-7B-Instruct-v0.3-fp16/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_mistral.jinja
+curl -L -o models/mistralai/Mistral-7B-Instruct-v0.3-fp16/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_mistral_parallel.jinja
 
 run_model_test() {
     local model_name=$1
@@ -77,14 +85,17 @@ run_model_test() {
     sleep 10
     export OPENAI_BASE_URL=http://localhost:8000/v3
     export OPENAI_API_KEY="notused"
-    bfcl generate --model ovms-model --test-category simple,multiple --num-threads 100 --result-dir $result_dir
-    bfcl evaluate --model ovms-model --result-dir $result_dir --score-dir $result_dir
+    export TOOL_CHOICE=auto
+    bfcl generate --model ovms-model --test-category simple,multiple --num-threads 100 --result-dir $result_dir -o
+    bfcl evaluate --model ovms-model --result-dir $result_dir --score-dir ${result_dir}_score
 }
 
 # Model configurations
 declare -A models=(
     ["Qwen/Qwen3-8B"]="hermes3"
     ["Qwen/Qwen3-4B"]="hermes3"
+    ["Qwen/Qwen3-1.7B"]="hermes3"
+    ["Qwen/Qwen3-0.6B"]="hermes3"
     ["meta-llama/Llama-3.1-8B-Instruct"]="llama3"
     ["meta-llama/Llama-3.2-3B-Instruct"]="llama3"
     ["NousResearch/Hermes-3-Llama-3.1-8B"]="hermes3"
@@ -113,6 +124,9 @@ for model in "${!models[@]}"; do
 done
 
 docker stop ovms 2>/dev/null
+
+
+python sumarize_results.py
 
 
 
