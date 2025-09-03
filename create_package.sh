5e91e9d5ca8fae96d19cd67878d2d6f01c723ddd
Miłosz Żeglarski <milosz.zeglarski@intel.com>
2025-09-03 11:15:29 +0200

    Include Tokenizers and GenAI python bindings in Linux packages (#3613)
    This PR enables GenAI and Tokenizers Python bindings in Linux packages by including them directly in OVMS distributions. The main purpose is to make OpenVINO Tokenizers and GenAI functionality available for Python nodes without requiring separate pip installations.

--- create_package.sh
+++ create_package.sh
@@ -64,11 +64,14 @@ if [ -f /ovms_release/lib/libsrc_Slibovms_Ushared.so ] ; then \
 	/ovms_release/lib/libovms_shared.so-2.params ; \
 fi
 
+# Add Python bindings for pyovms, openvino, openvino_tokenizers and openvino_genai, so they are all available for OVMS Python servables
 if ! [[ $debug_bazel_flags == *"_py_off"* ]]; then cp -r /opt/intel/openvino/python /ovms_release/lib/python ; fi
 if ! [[ $debug_bazel_flags == *"_py_off"* ]]; then mv /ovms_release/lib/pyovms.so /ovms_release/lib/python ; fi
 if ! [[ $debug_bazel_flags == *"_py_off"* ]]; then echo $'#!/bin/bash\npython3 -m openvino_tokenizers.cli "$@"' > /ovms_release/bin/convert_tokenizer ; \
    chmod +x /ovms_release/bin/convert_tokenizer ; fi
-if ! [[ $debug_bazel_flags == *"_py_off"* ]]; then cp -r /openvino_tokenizers/python/openvino_tokenizers/ /ovms_release/lib/python/ ; fi
+if ! [[ $debug_bazel_flags == *"_py_off"* ]]; then cp -r /ovms/bazel-out/k8-opt/bin/external/llm_engine/libopenvino_genai/python/* /ovms_release/lib/python/ ; \
+	mkdir -p /ovms_release/lib/python/openvino_genai-2025.4.dist-info ; \
+	echo $'Metadata-Version: 1.0\nName: openvino-genai\nVersion: 2025.4\nRequires-Python: >=3.9\nRequires-Dist: openvino-genai~=2025.4.0' > /ovms_release/lib/python/openvino_genai-2025.4.dist-info/METADATA; fi
 
 if [ -f /opt/intel/openvino/runtime/lib/intel64/plugins.xml ]; then cp /opt/intel/openvino/runtime/lib/intel64/plugins.xml /ovms_release/lib/ ; fi
 find /opt/intel/openvino/runtime/lib/intel64/ -iname '*.mvcmd*' -exec cp -v {} /ovms_release/lib/ \;
