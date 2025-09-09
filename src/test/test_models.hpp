706459425195f0f21826c3ce2ccf94d93b4607ba
atobisze <adrian.tobiszewski@intel.com>
2025-09-09 13:14:58 +0200

    Fix style

--- src/test/test_models.hpp
+++ src/test/test_models.hpp
@@ -1,5 +1,5 @@
 //*****************************************************************************
-// Copyright 2020-2021 Intel Corporation
+// Copyright 2025 Intel Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,6 +14,8 @@
 // limitations under the License.
 //*****************************************************************************
 #pragma once
+
+#include <string>
 
 #include "platform_utils.hpp"
 
