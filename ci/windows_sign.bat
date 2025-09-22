dbcff80c1b2952721d2ce5de2cb1e5b7230f6b7c
Pawel <pawel.rzepecki@intel.com>
2025-09-22 12:46:44 +0200

    added sign and bdba drafts to the windows ci pipeline

---
+++ ci/windows_sign.bat
@@ -0,0 +1,4 @@
+@echo off
+REM
+curl https://raw.githubusercontent.com/intel-innersource/frameworks.ai.openvino.model-server.bdba/refs/heads/main/windows_signing/check_signing.py?token=GHSAT0AAAAAADGTIRKXLRUCX2YYYKZY5PLI2GRGVLA -o check_signing.py
+
