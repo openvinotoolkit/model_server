8167944464899ccc38e2e74c835c1a1d9ecfcba3
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2025-09-24 11:24:36 +0200

    set env variables for oncommit build

--- ci/build_test_OnCommit.groovy
+++ ci/build_test_OnCommit.groovy
@@ -99,6 +99,9 @@ pipeline {
               }
               when { expression { image_build_needed == "true" } }
                 steps {
+                      if (! env.OVMS_BAZEL_REMOTE_CACHE_URL) {
+                        env.OVMS_BAZEL_REMOTE_CACHE_URL = "http://mclx-23.sclab.intel.com:8666"
+                      }
                       sh "echo build --remote_cache=${env.OVMS_BAZEL_REMOTE_CACHE_URL} > .user.bazelrc"
                       sh "echo test:linux --test_env https_proxy=${env.HTTPS_PROXY} >> .user.bazelrc"
                       sh "echo test:linux --test_env http_proxy=${env.HTTP_PROXY} >> .user.bazelrc"
@@ -114,6 +117,9 @@ pipeline {
                   script {
                       agent_name_windows = env.NODE_NAME
                       echo sh(script: 'env|sort', returnStdout: true)
+                      if (! env.OVMS_BAZEL_REMOTE_CACHE_URL) {
+                        env.OVMS_BAZEL_REMOTE_CACHE_URL = "http://mclx-23.sclab.intel.com:8666"
+                      }
                       def windows = load 'ci/loadWin.groovy'
                       if (windows != null) {
                         try {
