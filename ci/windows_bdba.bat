606cf9db395c9c0bf9822225fe459e8287fbb7a8
Pawel <pawel.rzepecki@intel.com>
2025-09-26 12:32:35 +0200

    changes in filenames and cleaning temporal files v2

--- ci/windows_bdba.bat
+++ ci/windows_bdba.bat
@@ -16,16 +16,17 @@ if exist requirements.txt (
 for /f "tokens=2 delims==." %%I in ('wmic os get localdatetime /value') do set datetime=%%I
 set datestamp=%datetime:~0,8%
 set timestamp=%datetime:~8,4%
-set filename=ovms_windows_%datestamp%_%timestamp%.zip
+set filename=ovms_windows_%datestamp%_%timestamp%
+set zipname="%filename%.zip"
 
 copy %OVMS_PATH%\\ovms.zip %OVMS_PATH%\\%filename%
 
 echo "BDBA_KEY=%BDBA_KEY%"
 echo "OVMS_PATH=%OVMS_PATH%"
 
-python binary_scans\ovms_bdba.py --key %BDBA_KEY% --type windows --build_dir %OVMS_PATH% --artifacts %filename% --report_name %filename% 2>&1 | tee ..\win_bdba.log
+python binary_scans\ovms_bdba.py --key %BDBA_KEY% --type windows --build_dir %OVMS_PATH% --artifacts %zipname% --report_name %filename% 2>&1 | tee ..\win_bdba.log
 
 deactivate
 
 tar -cvf %OVMS_PATH%\ovms_windows_bdba_reports.zip -C ovms_windows_*
-rm -rf %OVMS_PATH%\\%filename%
+rm -rf %OVMS_PATH%\\%zipname%
