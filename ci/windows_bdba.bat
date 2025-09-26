4e8485e974087529a96b2a96d99ba575a6e9c126
Pawel <pawel.rzepecki@intel.com>
2025-09-26 10:56:04 +0200

    corrected path to bdba scans v2

--- ci/windows_bdba.bat
+++ ci/windows_bdba.bat
@@ -1,7 +1,6 @@
 @echo off
 set "BDBA_KEY=%1"
 set "OVMS_PATH=%2"
-cd repo_ci_infra
 
 python -m venv venv
 
@@ -9,8 +8,8 @@ call venv\Scripts\activate
 
 python -m pip install --upgrade pip
 
-if exist requirements.txt (
-    pip install -r requirements.txt
+if exist repo_ci_infra\requirements.txt (
+    pip install -r repo_ci_infra\requirements.txt
 )
 
 for /f "tokens=2 delims==." %%I in ('wmic os get localdatetime /value') do set datetime=%%I
@@ -18,13 +17,13 @@ set datestamp=%datetime:~0,8%
 set timestamp=%datetime:~8,4%
 set filename=ovms_%datestamp%_%timestamp%.zip
 
-copy ..\\%OVMS_PATH%\\ovms.zip ..\\%OVMS_PATH%\\%filename%
+copy %OVMS_PATH%\\ovms.zip %OVMS_PATH%\\%filename%
 
 echo "BDBA_KEY=%BDBA_KEY%"
 echo "OVMS_PATH=%OVMS_PATH%"
 
-python binary_scans\ovms_bdba.py --key %BDBA_KEY% --type windows --build_dir %OVMS_PATH% --artifacts %filename% --report_name %filename% 2>&1 | tee ..\win_bdba.log
+python repo_ci_infra\binary_scans\ovms_bdba.py --key %BDBA_KEY% --type windows --build_dir %OVMS_PATH% --artifacts %filename% --report_name %filename% 2>&1 | tee win_bdba.log
 
 deactivate
 
-tar -cvf %2\ovms_windows_bdba_reports.zip -C %2 ovms_windows_*
+tar -cvf %OVMS_PATH%\ovms_windows_bdba_reports.zip -C %OVMS_PATH% ovms_windows_*
