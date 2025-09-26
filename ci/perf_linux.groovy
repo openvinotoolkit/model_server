6bd7571e7d9f085750c1074da4da7ebe41536b15
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2025-09-26 17:21:26 +0200

    latency test

--- ci/perf_linux.groovy
+++ ci/perf_linux.groovy
@@ -79,7 +79,7 @@ pipeline {
                 }
                 sh "echo Start docker container"
                 sh "mkdir -p ${env.MODELS_REPOSITORY_PATH}"
-                sh "docker run --rm -d --user \$(id -u):\$(id -g) -e https_proxy ${env.HTTPS_PROXY} --name model_server_${BUILD_NUMBER} -p 9000:9000 -v ${env.MODELS_REPOSITORY_PATH}:/models ${params.DOCKER_IMAGE_NAME} --source_model ${params.MODEL} --port 9000 --task text_generation --model_repository_path /models --target_device ${params.DEVICE} --log_level INFO"
+                sh "docker run --rm -d --user \$(id -u):\$(id -g) -e https_proxy=${env.HTTPS_PROXY} --name model_server_${BUILD_NUMBER} -p 9000:9000 -v ${env.MODELS_REPOSITORY_PATH}:/models ${params.DOCKER_IMAGE_NAME} --source_model ${params.MODEL} --port 9000 --task text_generation --model_repository_path /models --target_device ${params.DEVICE} --log_level INFO"
                 sh "echo wait for model server to be ready"
                 sh "while [ \"\$(curl -s http://localhost:9000/v3/models | jq -r '.data[0].id')\" != \"${params.MODEL}\" ] ; do echo waiting for LLM model; sleep 1; done"
                 sh "echo Running latency test"
