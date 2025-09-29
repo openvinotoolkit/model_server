2db4779455337e4727a9390b42cc785b1f195860
Pawel <pawel.rzepecki@intel.com>
2025-09-29 10:50:22 +0200

    changed location

--- ci/windows_sign.bat
+++ ci/windows_sign.bat
@@ -5,14 +5,16 @@ set "OVMS_FILES=..\..\%3"
 set "PYTHON=%4"
 set PATH=%PATH%;C:\Jenkins\workspace\ovmsc\signfile;C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64
 
+cd repo_signing\windows_signing
+
 if /I "%PYTHON%"=="1" (
     set "PYTHON_OPT=--python"
 ) else (
     set "PYTHON_OPT="
 )
 
-python repo_signing\windows_signing\check_signing.py --user=%OVMS_USER% --path=%OVMS_FILES% %PYTHON_OPT% --auto --verbose --print_all 2>&1 | tee win_sign.log
-for /f "tokens=* delims=" %%a in ('type win_sign.log ^| tail -n 1') do (
+python check_signing.py --user=%OVMS_USER% --path=%OVMS_FILES% %PYTHON_OPT% --auto --verbose --print_all 2>&1 | tee ..\..\win_sign.log
+for /f "tokens=* delims=" %%a in ('type ..\..\win_sign.log ^| tail -n 1') do (
     echo %%a | findstr /C:"[ OK ]" >nul
     if not errorlevel 1 (
         exit /b 0
