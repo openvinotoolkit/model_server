5f0efc09fc9ee22aace95b37e66b47cb44c3c2cd
Miłosz Żeglarski <milosz.zeglarski@intel.com>
2025-10-07 16:36:46 +0200

    Streaming for Phi4 tool parser (#3673)
    This PR adds streaming functionality to the Phi4 tool parser, enabling real-time processing of tool calls during generation rather than waiting for complete output.

--- src/llm/io_processing/phi4/tool_parser.hpp
+++ src/llm/io_processing/phi4/tool_parser.hpp
@@ -29,6 +29,7 @@
 #pragma warning(pop)
 
 #include "src/llm/io_processing/base_output_parser.hpp"
+#include "src/llm/io_processing/partial_json_builder.hpp"
 
 namespace ovms {
 class Phi4ToolParser : public BaseOutputParser {
@@ -36,6 +37,36 @@ protected:
     // Tools calls are expected to be the last part of the content, so we do not specify an end tag.
     const std::string parsingStartTag = "functools";
     const std::string parsingEndTag = "";
+
+    // Streaming required members
+
+    enum InternalState {
+        AWAITING_START_TAG,
+        AWAITING_TOOL_CALLS_OPENING_BRACKET,
+        AWAITING_TOOL_CALL_OPENING_BRACE,
+        PROCESSING_TOOL_CALL
+    };
+
+    InternalState internalState = AWAITING_START_TAG;
+    rapidjson::Document lastJson;
+    PartialJsonBuilder jsonBuilder;
+    // Index to track the current tool call being processed (-1 means no tool call has been started yet)
+    int toolCallIndex = -1;
+    // Flag to indicate if double quote has been added at the beginning of arguments
+    bool argumentsQuotesOpened = false;
+    std::string unprocessedBuffer;
+
+    // Stack of opened braces to track nested structures while in arguments collection phase
+    // Starting with 1, since we count the tool call opening brace and expect it to be closed as arguments end
+    size_t openBracesCount = 1;
+
+    void movePostColonContentToUnprocessedBuffer(std::string& chunk);
+    void movePostToolCallEndContentToUnprocessedBuffer();
+    void updateOpenBracesCount(const std::string& chunk);
+    void handleGenerationFinish(std::string& chunk) const;
+    void handleEndOfToolCall(std::string& chunk);
+    void openArgumentsString(std::string& chunk) const;
+    void clearState();
 
 public:
     Phi4ToolParser() = delete;
