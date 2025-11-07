15c305d4bd43e5d888145e467832125f8513760b
rasapala <rafal.a.sapala@intel.com>
2025-11-07 11:25:01 +0100

    Skip more tests

--- src/test/stress_test_utils.hpp
+++ src/test/stress_test_utils.hpp
@@ -1144,6 +1144,9 @@ public:
     }
     // we setup the OVMS so that it does not have any models loaded but also prepare the fixture to have ovmsConfig & configFilePath set up
     void SetUpCAPIServerInstance(const std::string& initialConfigContent) {
+        #ifdef _WIN32
+            GTEST_SKIP() << "Skipping test on Windows, sporadic";  // CVS-176244
+        #endif
         TestWithTempDir::SetUp();
         std::string port = "9178";
         std::string restPort = "9178";
