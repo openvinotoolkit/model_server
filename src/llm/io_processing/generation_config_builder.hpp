e9c7060f153d0e333ff0c83f442f2c5af7a20d12
Damian Kalinowski <damian.kalinowski@intel.com>
2025-11-18 11:20:02 +0100

    Remove 0.5kb copy each request

--- src/llm/io_processing/generation_config_builder.hpp
+++ src/llm/io_processing/generation_config_builder.hpp
@@ -34,7 +34,7 @@ class GenerationConfigBuilder {
 public:
     GenerationConfigBuilder() = delete;
     // Using tool parser name to select appropriate builder implementation to avoid introducing additional parameters. Might be insufficient in the future.
-    explicit GenerationConfigBuilder(ov::genai::GenerationConfig baseConfig, std::string toolParserName, bool enableToolGuidedGeneration, DecodingMethod decodingMethod) {
+    explicit GenerationConfigBuilder(ov::genai::GenerationConfig& baseConfig, std::string toolParserName, bool enableToolGuidedGeneration, DecodingMethod decodingMethod) {
         if (toolParserName == "llama3") {
             builder_impl = std::make_unique<Llama3GenerationConfigBuilder>(baseConfig, enableToolGuidedGeneration, decodingMethod);
         } else if (toolParserName == "qwen3") {
