11d62834a4444f69f5d40a9ea6d1ea45f7c72021
przepeck <pawel.rzepecki@intel.com>
2025-12-01 13:07:05 +0100

    changes for multiple model sizes

--- tests/accuracy/sumarize_results.py
+++ tests/accuracy/sumarize_results.py
@@ -17,18 +17,19 @@
 import json
 
 models=[
-	"Hermes-3-Llama-3.1-8B",
-	"Llama-3.1-8B-Instruct",
-	"Mistral-7B-Instruct-v0.3",
-	"Phi-4-mini-instruct",
-	"Llama-3.2-3B-Instruct",
+	# "Hermes-3-Llama-3.1-8B",
+	# "Llama-3.1-8B-Instruct",
+	# "Mistral-7B-Instruct-v0.3",
+	# "Phi-4-mini-instruct",
+	# "Llama-3.2-3B-Instruct",
+    "Qwen3-14B",
 	"Qwen3-8B",
 	"Qwen3-4B",
 	"Qwen3-1.7B",
 	"Qwen3-0.6B"
 ]
 
-precisions=["int4","int8", "fp16"]
+precisions=["int4"] #"int8", "fp16"]
 
 categories=["simple", "multiple"]
 
@@ -55,20 +56,20 @@ def get_result(base_folder, model, precision, category, guided):
             print(score)
     return score
 
-parser = argparse.ArgumentParser(description='Process accuracy results')
-parser.add_argument('--precisions', nargs='+', default=["int4","int8", "fp16"], help='List of precisions')
-parser.add_argument('--categories', nargs='+', default=["simple", "multiple"], help='List of categories')
-parser.add_argument('--guided_generation', nargs='+', default=["true", "false"], help='List of guided generation options')
-parser.add_argument('--models', nargs='+', default=models, help='List of models')
-parser.add_argument('--base_folder', type=str, default="gorilla/berkeley-function-call-leaderboard", help='Base folder path')
+# parser = argparse.ArgumentParser(description='Process accuracy results')
+# parser.add_argument('--precisions', nargs='+', default=["int4","int8", "fp16"], help='List of precisions')
+# parser.add_argument('--categories', nargs='+', default=["simple", "multiple"], help='List of categories')
+# parser.add_argument('--guided_generation', nargs='+', default=["true", "false"], help='List of guided generation options')
+# parser.add_argument('--models', nargs='+', default=models, help='List of models')
+# parser.add_argument('--base_folder', type=str, default="gorilla/berkeley-function-call-leaderboard", help='Base folder path')
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
-precisions = args.precisions
-categories = args.categories
-guided_generation = args.guided_generation
-models = args.models
-base_folder = Path(args.base_folder)
+# precisions = args.precisions
+# categories = args.categories
+# guided_generation = args.guided_generation
+# models = args.models
+base_folder = Path("gorilla/berkeley-function-call-leaderboard")
 
 
 for each_model in models:
@@ -90,8 +91,8 @@ print(json.dumps(results, indent=2))
 
 print("Enabled tool guided generation")
 for each_model in results:
-    print(f"{each_model},{results[each_model]['int4']['simple']['true']},{results[each_model]['int4']['multiple']['true']},{results[each_model]['int8']['simple']['true']},{results[each_model]['int8']['multiple']['true']},{results[each_model]['fp16']['simple']['true']},{results[each_model]['fp16']['multiple']['true']}")
+    print(f"{each_model},{results[each_model]['int4']['simple']['true']},{results[each_model]['int4']['multiple']['true']},") #{results[each_model]['int8']['simple']['true']},{results[each_model]['int8']['multiple']['true']},{results[each_model]['fp16']['simple']['true']},{results[each_model]['fp16']['multiple']['true']}")
 
 print("\nEnabled tool guided generation")
 for each_model in results:
-    print(f"{each_model},{results[each_model]['int4']['simple']['false']},{results[each_model]['int4']['multiple']['false']},{results[each_model]['int8']['simple']['false']},{results[each_model]['int8']['multiple']['false']},{results[each_model]['fp16']['simple']['false']},{results[each_model]['fp16']['multiple']['false']}")
+    print(f"{each_model},{results[each_model]['int4']['simple']['false']},{results[each_model]['int4']['multiple']['false']},") #{results[each_model]['int8']['simple']['false']},{results[each_model]['int8']['multiple']['false']},{results[each_model]['fp16']['simple']['false']},{results[each_model]['fp16']['multiple']['false']}")
--- tests/accuracy/test_all_models.sh
+++ tests/accuracy/test_all_models.sh
@@ -28,57 +28,11 @@ cd ../..
 curl -L -O https://raw.githubusercontent.com/openvinotoolkit/model_server/refs/heads/${BRANCH_NAME}/demos/common/export_models/export_model.py
 mkdir -p models
 
-# Qwen/Qwen3-8B
-python export_model.py text_generation --source_model Qwen/Qwen3-8B --model_name Qwen/Qwen3-8B-int4 --weight-format int4 --model_repository_path models
-python export_model.py text_generation --source_model Qwen/Qwen3-8B --model_name Qwen/Qwen3-8B-int8 --weight-format int8 --model_repository_path models
-python export_model.py text_generation --source_model Qwen/Qwen3-8B --model_name Qwen/Qwen3-8B-fp16 --weight-format fp16 --model_repository_path models
-# Qwen/Qwen3-4B
-python export_model.py text_generation --source_model Qwen/Qwen3-4B --model_name Qwen/Qwen3-4B-int4 --weight-format int4 --model_repository_path models
-python export_model.py text_generation --source_model Qwen/Qwen3-4B --model_name Qwen/Qwen3-4B-int8 --weight-format int8 --model_repository_path models
-python export_model.py text_generation --source_model Qwen/Qwen3-4B --model_name Qwen/Qwen3-4B-fp16 --weight-format fp16 --model_repository_path models
-# Qwen/Qwen3-1.7B
-python export_model.py text_generation --source_model Qwen/Qwen3-1.7B --model_name Qwen/Qwen3-1.7B-int4 --weight-format int4 --model_repository_path models
-python export_model.py text_generation --source_model Qwen/Qwen3-1.7B --model_name Qwen/Qwen3-1.7B-int8 --weight-format int8 --model_repository_path models
-python export_model.py text_generation --source_model Qwen/Qwen3-1.7B --model_name Qwen/Qwen3-1.7B-fp16 --weight-format fp16 --model_repository_path models
-# Qwen/Qwen3-0.6B
-python export_model.py text_generation --source_model Qwen/Qwen3-0.6B --model_name Qwen/Qwen3-0.6B-int4 --weight-format int4 --model_repository_path models
-python export_model.py text_generation --source_model Qwen/Qwen3-0.6B --model_name Qwen/Qwen3-0.6B-int8 --weight-format int8 --model_repository_path models
-python export_model.py text_generation --source_model Qwen/Qwen3-0.6B --model_name Qwen/Qwen3-0.6B-fp16 --weight-format fp16 --model_repository_path models
-# meta-llama/Llama-3.1-8B-Instruct
-python export_model.py text_generation --source_model meta-llama/Llama-3.1-8B-Instruct --model_name meta-llama/Llama-3.1-8B-Instruct-int4 --weight-format int4 --model_repository_path models
-curl -L -o models/meta-llama/Llama-3.1-8B-Instruct-int4/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_llama3.1_json.jinja
-python export_model.py text_generation --source_model meta-llama/Llama-3.1-8B-Instruct --model_name meta-llama/Llama-3.1-8B-Instruct-int8 --weight-format int8 --model_repository_path models
-curl -L -o models/meta-llama/Llama-3.1-8B-Instruct-int8/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_llama3.1_json.jinja
-python export_model.py text_generation --source_model meta-llama/Llama-3.1-8B-Instruct --model_name meta-llama/Llama-3.1-8B-Instruct-fp16 --weight-format fp16 --model_repository_path models
-curl -L -o models/meta-llama/Llama-3.1-8B-Instruct-fp16/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_llama3.1_json.jinja
-# meta-llama/Llama-3.2-3B-Instruct
-python export_model.py text_generation --source_model meta-llama/Llama-3.2-3B-Instruct --model_name meta-llama/Llama-3.2-3B-Instruct-int4 --weight-format int4 --config_file_path models/config.json --model_repository_path models
-curl -L -o models/meta-llama/Llama-3.2-3B-Instruct-int4/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_llama3.2_json.jinja
-python export_model.py text_generation --source_model meta-llama/Llama-3.2-3B-Instruct --model_name meta-llama/Llama-3.2-3B-Instruct-int8 --weight-format int8 --config_file_path models/config.json --model_repository_path models
-    curl -L -o models/meta-llama/Llama-3.2-3B-Instruct-int8/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_llama3.2_json.jinja
-python export_model.py text_generation --source_model meta-llama/Llama-3.2-3B-Instruct --model_name meta-llama/Llama-3.2-3B-Instruct-fp16 --weight-format fp16 --config_file_path models/config.json --model_repository_path models
-curl -L -o models/meta-llama/Llama-3.2-3B-Instruct-fp16/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_llama3.2_json.jinja
-# NousResearch/Hermes-3-Llama-3.1-8B
-python export_model.py text_generation --source_model NousResearch/Hermes-3-Llama-3.1-8B --model_name NousResearch/Hermes-3-Llama-3.1-8B-int4 --weight-format int4 --config_file_path models/config.json --model_repository_path models
-curl -L -o models/NousResearch/Hermes-3-Llama-3.1-8B-int4/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_hermes.jinja
-python export_model.py text_generation --source_model NousResearch/Hermes-3-Llama-3.1-8B --model_name NousResearch/Hermes-3-Llama-3.1-8B-int8 --weight-format int8 --config_file_path models/config.json --model_repository_path models
-curl -L -o models/NousResearch/Hermes-3-Llama-3.1-8B-int8/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_hermes.jinja
-python export_model.py text_generation --source_model NousResearch/Hermes-3-Llama-3.1-8B --model_name NousResearch/Hermes-3-Llama-3.1-8B-fp16 --weight-format fp16 --config_file_path models/config.json --model_repository_path models
-curl -L -o models/NousResearch/Hermes-3-Llama-3.1-8B-fp16/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_hermes.jinja
-# microsoft/Phi-4-mini-instruct
-python export_model.py text_generation --source_model microsoft/Phi-4-mini-instruct --model_name microsoft/Phi-4-mini-instruct-int4 --weight-format int4 --config_file_path models/config.json --model_repository_path models
-curl -L -o models/microsoft/Phi-4-mini-instruct-int4/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_phi4_mini.jinja
-python export_model.py text_generation --source_model microsoft/Phi-4-mini-instruct --model_name microsoft/Phi-4-mini-instruct-int8 --weight-format int8 --config_file_path models/config.json --model_repository_path models
-curl -L -o models/microsoft/Phi-4-mini-instruct-int8/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_phi4_mini.jinja
-python export_model.py text_generation --source_model microsoft/Phi-4-mini-instruct --model_name microsoft/Phi-4-mini-instruct-fp16 --weight-format fp16 --config_file_path models/config.json --model_repository_path models
-curl -L -o models/microsoft/Phi-4-mini-instruct-fp16/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_phi4_mini.jinja
-# mistralai/Mistral-7B-Instruct-v0.3
-python export_model.py text_generation --source_model mistralai/Mistral-7B-Instruct-v0.3 --model_name mistralai/Mistral-7B-Instruct-v0.3-int4 --weight-format int4 --config_file_path models/config.json --model_repository_path models --extra_quantization_params "--task text-generation-with-past"
-curl -L -o models/mistralai/Mistral-7B-Instruct-v0.3-int4/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_mistral_parallel.jinja
-python export_model.py text_generation --source_model mistralai/Mistral-7B-Instruct-v0.3 --model_name mistralai/Mistral-7B-Instruct-v0.3-int8 --weight-format int8 --config_file_path models/config.json --model_repository_path models --tool_parser mistral --cache_size 2 --extra_quantization_params "--task text-generation-with-past"
-curl -L -o models/mistralai/Mistral-7B-Instruct-v0.3-int8/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_mistral_parallel.jinja
-python export_model.py text_generation --source_model mistralai/Mistral-7B-Instruct-v0.3 --model_name mistralai/Mistral-7B-Instruct-v0.3-fp16 --weight-format fp16 --config_file_path models/config.json --model_repository_path models --tool_parser mistral --cache_size 2 --extra_quantization_params "--task text-generation-with-past"
-curl -L -o models/mistralai/Mistral-7B-Instruct-v0.3-fp16/chat_template.jinja https://raw.githubusercontent.com/vllm-project/vllm/refs/tags/v0.9.0/examples/tool_chat_template_mistral_parallel.jinja
+python export_model.py text_generation --source_model Qwen/Qwen3-14B --model_name Qwen/Qwen3-14B --weight-format int4 --model_repository_path models
+python export_model.py text_generation --source_model Qwen/Qwen3-8B --model_name Qwen/Qwen3-8B --weight-format int4 --model_repository_path models
+python export_model.py text_generation --source_model Qwen/Qwen3-4B --model_name Qwen/Qwen3-4B --weight-format int4 --model_repository_path models
+python export_model.py text_generation --source_model Qwen/Qwen3-1.7B --model_name Qwen/Qwen3-1.7B --weight-format int4 --model_repository_path models
+python export_model.py text_generation --source_model Qwen/Qwen3-0.6B --model_name Qwen/Qwen3-0.6B --weight-format int4 --model_repository_path models
 
 run_model_test() {
     local model_name=$1
@@ -88,9 +42,9 @@ run_model_test() {
     set -x
     docker stop ovms 2>/dev/null
     docker run -d --name ovms --user $(id -u):$(id -g) --rm -p 8000:8000 -v $(pwd)/models:/models openvino/model_server:latest \
-    --rest_port 8000 --model_repository_path /models --source_model ${model_name}-${precision} \
+    --rest_port 8000 --model_repository_path /models --source_model ${model_name} \
     --tool_parser ${tool_parser} --model_name ovms-model --enable_tool_guided_generation $enable_tool_guided_generation \
-    --cache_size 20 --task text_generation
+    --cache_size 20 --task text_generation --log_path /models/${model_name//\//_}_ovms.log
     
     sleep 20
 
@@ -109,18 +63,14 @@ run_model_test() {
 
 # Model configurations
 declare -A models=(
+    ["Qwen/Qwen3-14B"]="hermes3"
     ["Qwen/Qwen3-8B"]="hermes3"
     ["Qwen/Qwen3-4B"]="hermes3"
     ["Qwen/Qwen3-1.7B"]="hermes3"
-    ["Qwen/Qwen3-0.6B"]="hermes3"
-    ["meta-llama/Llama-3.1-8B-Instruct"]="llama3"
-    ["meta-llama/Llama-3.2-3B-Instruct"]="llama3"
-    ["NousResearch/Hermes-3-Llama-3.1-8B"]="hermes3"
-    ["microsoft/Phi-4-mini-instruct"]="phi4"
-    ["mistralai/Mistral-7B-Instruct-v0.3"]="mistral"
+    ["Qwen/Qwen3-0.6B"]="hermes3"  
 )
 
-precisions=("int4" "int8" "fp16")
+precisions=("int4") #"int8" "fp16")
 
 # Run tests for each model and precision
 # enable tool guided generation
@@ -132,13 +82,13 @@ for model in "${!models[@]}"; do
 done
 
 
-# disable tool guided generation
-for model in "${!models[@]}"; do
-    tool_parser="${models[$model]}"
-    for precision in "${precisions[@]}"; do
-        run_model_test "$model" "$precision" "$tool_parser" "false"
-    done
-done
+# # disable tool guided generation
+# for model in "${!models[@]}"; do
+#     tool_parser="${models[$model]}"
+#     for precision in "${precisions[@]}"; do
+#         run_model_test "$model" "$precision" "$tool_parser" "false"
+#     done
+# done
 
 docker stop ovms 2>/dev/null
 
