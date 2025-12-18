d905bd6045526937769dee889b382404f5304a0c
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2025-12-19 00:31:34 +0100

    ver fix

--- windows_install_build_dependencies.bat
+++ windows_install_build_dependencies.bat
@@ -217,13 +217,13 @@ if "%TOKENIZER_SOURCE_ORG%"=="" (
     set "TOKENIZER_SOURCE_ORG=openvinotoolkit"
 )
 if "%TOKENIZER_SOURCE_BRANCH%"=="" (
-    set "TOKENIZER_SOURCE_BRANCH=a4d8fdf083e76147a5e8638a788e96f855084176"
+    set "TOKENIZER_SOURCE_BRANCH=releases/2025/4"
 )
 if "%GENAI_SOURCE_ORG%"=="" (
     set "GENAI_SOURCE_ORG=openvinotoolkit"
 )
 if "%GENAI_SOURCE_BRANCH%"=="" (
-    set "GENAI_SOURCE_BRANCH=27ca7fc0c1c05001a2e3f1ebea3d7421392a1d9e"
+    set "GENAI_SOURCE_BRANCH=releases/2025/4"
 )
 
 IF /I NOT EXIST %BAZEL_SHORT_PATH%\openvino_src (
