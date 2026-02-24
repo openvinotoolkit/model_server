774042bd874ae795581cf152923639dcba50d751
mzegla <milosz.zeglarski@intel.com>
2026-02-24 11:46:58 +0100

    no beam search for stateful pipeline

--- src/test/llm/llmnode_test.cpp
+++ src/test/llm/llmnode_test.cpp
@@ -199,7 +199,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJson) {
     auto params = GetParam();
     config.max_new_tokens = 5;
     config.rng_seed = 1;
-    config.num_beams = 16;
     if (params.generateExpectedOutput) {
         ASSERT_EQ(generateExpectedText("What is OpenVINO?"), 0);
         ASSERT_EQ(config.num_return_sequences, expectedMessages.size());
@@ -210,7 +209,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJson) {
                               R"(",
             "stream": false,
             "seed" : 1,
-            "best_of": 16,
             "max_tokens": 5,
             "prompt": "What is OpenVINO?"
         }
@@ -259,7 +257,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJsonEchoWithCompletion) {
 
     config.max_new_tokens = 5;
     config.rng_seed = 1;
-    config.num_beams = 16;
     config.echo = true;
     if (params.generateExpectedOutput) {
         ASSERT_EQ(generateExpectedText("What is OpenVINO?"), 0);
@@ -271,7 +268,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJsonEchoWithCompletion) {
                               R"(",
             "stream": false,
             "seed" : 1,
-            "best_of": 16,
             "max_tokens": 5,
             "prompt": "What is OpenVINO?",
             "echo": true
@@ -647,8 +643,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJsonNFail) {
                               R"(",
             "stream": false,
             "seed" : 1,
-            "best_of": 2,
-            "n": 3,
             "max_tokens": 5,
             "prompt": "What is OpenVINO?"
         }
@@ -656,7 +650,7 @@ TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJsonNFail) {
 
     ASSERT_EQ(
         handler->dispatchToProcessor(endpointCompletions, requestBody, &response, comp, responseComponents, writer, multiPartParser),
-        ovms::StatusCode::MEDIAPIPE_EXECUTION_ERROR);
+        ovms::StatusCode::OK);
 }
 
 TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJsonN) {
@@ -667,8 +661,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJsonN) {
     }
     config.max_new_tokens = 5;
     config.rng_seed = 1;
-    config.num_beams = 16;
-    config.num_return_sequences = 8;
     config.echo = false;
     if (params.generateExpectedOutput) {
         ASSERT_EQ(generateExpectedText("What is OpenVINO?"), 0);
@@ -680,8 +672,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJsonN) {
                               R"(",
             "stream": false,
             "seed" : 1,
-            "best_of": 16,
-            "n": 8,
             "max_tokens": 5,
             "prompt": "What is OpenVINO?"
         }
@@ -692,7 +682,7 @@ TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJsonN) {
         ovms::StatusCode::OK);
     parsedResponse.Parse(response.c_str());
     ASSERT_TRUE(parsedResponse["choices"].IsArray());
-    ASSERT_EQ(parsedResponse["choices"].Capacity(), 8);
+    ASSERT_EQ(parsedResponse["choices"].Capacity(), 1);
     int i = 0;
     for (auto& choice : parsedResponse["choices"].GetArray()) {
         ASSERT_TRUE(choice["finish_reason"].IsString());
@@ -712,7 +702,7 @@ TEST_P(LLMFlowHttpTestParameterized, unaryCompletionsJsonN) {
     ASSERT_TRUE(parsedResponse["usage"].GetObject()["prompt_tokens"].IsInt());
     ASSERT_TRUE(parsedResponse["usage"].GetObject()["completion_tokens"].IsInt());
     ASSERT_TRUE(parsedResponse["usage"].GetObject()["total_tokens"].IsInt());
-    ASSERT_EQ(parsedResponse["usage"].GetObject()["completion_tokens"].GetInt(), 8 * 5 /* n * max_tokens */);
+    ASSERT_EQ(parsedResponse["usage"].GetObject()["completion_tokens"].GetInt(), 5 /* max_tokens */);
     EXPECT_STREQ(parsedResponse["model"].GetString(), params.modelName.c_str());
     EXPECT_STREQ(parsedResponse["object"].GetString(), "text_completion");
 }
@@ -725,8 +715,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonNFail) {
                               R"(",
             "stream": false,
             "seed" : 1,
-            "best_of" : 2,
-            "n" : 3,
             "max_tokens": 5,
             "messages": [
             {
@@ -739,7 +727,7 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonNFail) {
 
     ASSERT_EQ(
         handler->dispatchToProcessor(endpointChatCompletions, requestBody, &response, comp, responseComponents, writer, multiPartParser),
-        ovms::StatusCode::MEDIAPIPE_EXECUTION_ERROR);
+        ovms::StatusCode::OK);
 }
 
 TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonN) {
@@ -747,8 +735,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonN) {
     auto params = GetParam();
     config.max_new_tokens = 5;
     config.rng_seed = 1;
-    config.num_beams = 16;
-    config.num_return_sequences = 8;
     config.echo = false;
     if (params.generateExpectedOutput) {
         ASSERT_EQ(generateExpectedText("What is OpenVINO?", false, true), 0);
@@ -760,8 +746,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonN) {
                               R"(",
             "stream": false,
             "seed" : 1,
-            "best_of" : 16,
-            "n" : 8,
             "max_tokens": 5,
             "messages": [
             {
@@ -777,7 +761,7 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonN) {
         ovms::StatusCode::OK);
     parsedResponse.Parse(response.c_str());
     ASSERT_TRUE(parsedResponse["choices"].IsArray());
-    ASSERT_EQ(parsedResponse["choices"].Capacity(), 8);
+    ASSERT_EQ(parsedResponse["choices"].Capacity(), 1);
     int i = 0;
     for (auto& choice : parsedResponse["choices"].GetArray()) {
         ASSERT_TRUE(choice["finish_reason"].IsString());
@@ -800,7 +784,7 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonN) {
     ASSERT_TRUE(parsedResponse["usage"].GetObject()["prompt_tokens"].IsInt());
     ASSERT_TRUE(parsedResponse["usage"].GetObject()["completion_tokens"].IsInt());
     ASSERT_TRUE(parsedResponse["usage"].GetObject()["total_tokens"].IsInt());
-    ASSERT_EQ(parsedResponse["usage"].GetObject()["completion_tokens"].GetInt(), 8 * 5 /* n * max_tokens */);
+    ASSERT_EQ(parsedResponse["usage"].GetObject()["completion_tokens"].GetInt(), 5 /* max_tokens */);
     EXPECT_STREQ(parsedResponse["model"].GetString(), params.modelName.c_str());
     EXPECT_STREQ(parsedResponse["object"].GetString(), "chat.completion");
 }
@@ -832,7 +816,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJson) {
                               R"(",
             "stream": false,
             "seed" : 1,
-            "best_of" : 16,
             "max_tokens": 5,
             "messages": [
             {
@@ -881,7 +864,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonContentArray) {
                               R"(",
             "stream": false,
             "seed" : 1,
-            "best_of" : 16,
             "max_tokens": 5,
             "messages": [
             {
@@ -930,7 +912,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonContentArrayWithIma
                               R"(",
             "stream": false,
             "seed" : 1,
-            "best_of" : 16,
             "max_tokens": 5,
             "messages": [
             {
@@ -960,8 +941,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonNMultipleStopString
                               R"(",
             "stream": false,
             "seed" : 1,
-            "best_of" : 4,
-            "n": 4,
             "max_tokens": 50,
             "stop": [".", ","],
             "include_stop_str_in_output": true,
@@ -979,7 +958,7 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonNMultipleStopString
         ovms::StatusCode::OK);
     parsedResponse.Parse(response.c_str());
     ASSERT_TRUE(parsedResponse["choices"].IsArray());
-    ASSERT_EQ(parsedResponse["choices"].Capacity(), 4);
+    ASSERT_EQ(parsedResponse["choices"].Capacity(), 1);
     int i = 0;
     for (auto& choice : parsedResponse["choices"].GetArray()) {
         if (params.checkFinishReason) {
