7b9cc7208a25ffc1d2b78002cb839f712ce376bf
Pawel <pawel.rzepecki@intel.com>
2026-03-12 13:49:26 +0100

    save

--- src/llm/io_processing/lfm2/tool_parser.cpp
+++ src/llm/io_processing/lfm2/tool_parser.cpp
@@ -17,7 +17,7 @@
 
 namespace ovms {
 
-Argument Lfm2ToolParser::parseSingleArgument(std::string& argumentStr){
+Argument Lfm2ToolParser::parseSingleArgument(const std::string& argumentStr){
     Argument argument;
 
     size_t equalPos = argumentStr.find('=');
@@ -39,42 +39,71 @@ Argument Lfm2ToolParser::parseSingleArgument(std::string& argumentStr){
         argument.name = argumentStr;
         argument.value = "";
         argument.type = ParameterType::UNKNOWN;
+        SPDLOG_LOGGER_DEBUG(llm_calculator_logger, "Argument string: {} does not contain '=', setting name as entire string and value as empty", argumentStr);
     }
     return argument;
 }
 
-std::vector<Argument> Lfm2ToolParser::parseArguments(std::string& argumentsStr) {
-    const std::string toolArgSeparatorStr = ", ";
+std::vector<Argument> Lfm2ToolParser::parseArguments(const std::string& argumentsStr) {
     std::vector<std::string> args;
     std::vector<Argument> parsedArgs;
 
     size_t argPos = 0;
     while (argPos < argumentsStr.length()) {
-        size_t commaPos = argumentsStr.find(toolArgSeparatorStr, argPos);
+        size_t commaPos = argumentsStr.find(toolSeparatorStr, argPos);
         if (commaPos == std::string::npos) {
             args.push_back(argumentsStr.substr(argPos));
             break;
         }
         args.push_back(argumentsStr.substr(argPos, commaPos - argPos));
-        argPos = commaPos + toolArgSeparatorStr.length();
+        argPos = commaPos + toolSeparatorStr.length();
     }
 
-    for (std::string& arg : args) {
+    for (const std::string& arg : args) {
         parsedArgs.push_back(parseSingleArgument(arg));
     }
     return parsedArgs;
 }
 
+bool Lfm2ToolParser::parseSingleToolCall(const std::string& toolStr, ToolCall& toolCall) {
+    size_t argsPos = toolStr.find(toolArgsStartIndicator);
+    if (argsPos != std::string::npos) {
+        std::string toolName = toolStr.substr(0, argsPos);                
+        SPDLOG_LOGGER_DEBUG(llm_calculator_logger, "Parsed tool name: {}", toolName);
+
+        int argsStrLen = toolStr.length() - argsPos - toolArgsStartIndicator.length() - toolEndIndicator.length();
+        std::string argsStr = toolStr.substr(argsPos + toolArgsStartIndicator.length(), argsStrLen);
+        SPDLOG_LOGGER_DEBUG(llm_calculator_logger, "Parsed args string: {}", argsStr);
+        std::vector<Argument> arguments = parseArguments(argsStr);
+
+        toolCall.name = toolName;
+        rapidjson::Document argsDoc(rapidjson::kObjectType);
+        rapidjson::StringBuffer sb;
+        rapidjson::Writer<rapidjson::StringBuffer> argsWriter(sb);
+        argsWriter.StartObject();
+        for (const Argument& argument : arguments) {
+            argsWriter.Key(argument.name.c_str());
+            argsWriter.StartObject();
+            if (argument.type == ParameterType::STRING) {
+                argsWriter.String(argument.value.c_str());
+            } else if (argument.type == ParameterType::BOOLEAN) {
+                bool boolValue = (argument.value == "true");
+                argsWriter.Bool(boolValue);
+            } else if (argument.type == ParameterType::NUMBER) {
+                argsWriter.RawNumber(argument.value.c_str(), argument.value.length());
+            }
+        }                
+        argsWriter.EndObject();
+        toolCall.arguments = sb.GetString();
+        toolCall.id = generateRandomId();
+        return true;
+    }
+    return false;
+}
+
 void Lfm2ToolParser::parse(ParsedOutput& parsedOutput, const std::vector<int64_t>& generatedTokens) {
     std::vector<std::string> tools;
     size_t pos = 0;
-    
-    const std::string toolListStartIndicator = "[";
-    const std::string toolListEndIndicator = "]";
-    const std::string toolArgsStartIndicator = "(";
-    const std::string toolEndIndicator = ")";
-    const std::string toolSeparatorStr = ", ";
-
     while (true) {
         size_t start = parsedOutput.content.find(toolCallStartTag, pos);
         if (start == std::string::npos) {
@@ -87,8 +116,7 @@ void Lfm2ToolParser::parse(ParsedOutput& parsedOutput, const std::vector<int64_t
         }
         std::string toolListStr = parsedOutput.content.substr(start + toolListStartIndicator.length(), end - start - toolListStartIndicator.length() - toolListEndIndicator.length());
         SPDLOG_LOGGER_DEBUG(llm_calculator_logger, "Parsed tool list string: {}", toolListStr);
-        uint32_t guard = 0;
-        //current state func_name1(arg1=value1), func_name2(arg1=value1) or func_name(arg1=value1, arg2=value2)
+        uint8_t guard = 0;
         while (!toolListStr.empty() && guard < 100) {
             size_t toolEndPos = toolListStr.find(toolEndIndicator);
             std::string singleTool;
@@ -109,41 +137,14 @@ void Lfm2ToolParser::parse(ParsedOutput& parsedOutput, const std::vector<int64_t
             }
         }
         
-
-        for (std::string& tool : tools) {
+        for (const std::string& tool : tools) {
             ToolCall toolCall;
-            size_t argsPos = tool.find(toolArgsStartIndicator);
-            if (argsPos != std::string::npos) {
-                std::string toolName = tool.substr(0, argsPos);                
-                size_t argsStartPos = singleTool.find(toolArgsStartIndicator);
-                std::string toolName = singleTool.substr(0, argsStartPos);
-                SPDLOG_LOGGER_DEBUG(llm_calculator_logger, "Parsed tool name: {}", toolName);
-                std::string argsStr = singleTool.substr(argsStartPos + toolArgsStartIndicator.length(), singleTool.length() - argsStartPos - toolArgsStartIndicator.length() - toolEndIndicator.length());
-                SPDLOG_LOGGER_DEBUG(llm_calculator_logger, "Parsed args string: {}", argsStr);
-                std::vector<Argument> arguments = parseArguments(argsStr);
-                
-                toolCall.name = toolName;
-                rapidjson::Document argsDoc(rapidjson::kObjectType);
-                rapidjson::StringBuffer sb;
-                rapidjson::Writer<rapidjson::StringBuffer> argsWriter(sb);
-                argsWriter.StartObject();
-                for (const Argument& argument : arguments) {
-                    argsWriter.Key(argument.name.c_str());
-                    argsWriter.StartObject();
-                    if (argument.type == ParameterType::STRING) {
-                        argsWriter.String(argument.value.c_str());
-                    } else if (argument.type == ParameterType::BOOLEAN) {
-                        bool boolValue = (argument.value == "true");
-                        argsWriter.Bool(boolValue);
-                    } else if (argument.type == ParameterType::NUMBER) {
-                        argsWriter.RawNumber(argument.value.c_str(), argument.value.length());
-                    }
-                }                
-                argsWriter.EndObject();
-                toolCall.arguments = sb.GetString();
-                toolCall.id = generateRandomId();
+            auto wasToolCallParsed = parseSingleToolCall(tool, toolCall);
+            if (wasToolCallParsed) {
+                SPDLOG_LOGGER_DEBUG(llm_calculator_logger, "Parsed tool call - name: {}, args: {}", toolCall.name, toolCall.arguments);
                 parsedOutput.toolCalls.push_back(toolCall);
-            }
+            } else
+                SPDLOG_LOGGER_DEBUG(llm_calculator_logger, "Failed to parse tool call from string: {}", tool);
         }
     }
 }
