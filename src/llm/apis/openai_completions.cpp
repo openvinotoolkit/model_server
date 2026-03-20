315f1136c9118da34c2430f24ceb15cf935c7dab
Michal Kulakowski <michalkulakowski@intel.com>
2026-03-20 10:09:36 +0100

    style

--- src/llm/apis/openai_completions.cpp
+++ src/llm/apis/openai_completions.cpp
@@ -1974,7 +1974,7 @@ std::string OpenAIChatCompletionsHandler::serializeResponsesStreamingInitEvents(
         writer.String("sequence_number");
         writer.Uint64(responsesStreamingSequenceNumber++);
         writer.String("output_index");
-        writer.Uint64(0); 
+        writer.Uint64(0);
         writer.String("item");
         serializeResponsesOutputItem(writer, outputItemId, "", "in_progress", false);
         writer.EndObject();
@@ -1987,7 +1987,7 @@ std::string OpenAIChatCompletionsHandler::serializeResponsesStreamingInitEvents(
         writer.String("sequence_number");
         writer.Uint64(responsesStreamingSequenceNumber++);
         writer.String("output_index");
-        writer.Uint64(0); 
+        writer.Uint64(0);
         writer.String("content_index");
         writer.Uint64(0);
         writer.String("item_id");
