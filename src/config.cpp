d55e2b3719a43c6abf03b26f173cad10e1a573f9
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2026-04-02 09:18:52 +0200

    style

--- src/config.cpp
+++ src/config.cpp
@@ -14,7 +14,7 @@
 // limitations under the License.
 //*****************************************************************************
 #include "config.hpp"
-
+#include <algorithm>
 #include <filesystem>
 #include <limits>
 #include <regex>
