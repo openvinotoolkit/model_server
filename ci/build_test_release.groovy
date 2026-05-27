4ba532426a4efdd9e15592fc8e0018224267b44b
Pawel Rzepecki <pawel.rzepecki@intel.com>
2026-05-27 13:58:28 +0200

    changing ssh to mounted directory

--- ci/build_test_release.groovy
+++ ci/build_test_release.groovy
@@ -39,11 +39,10 @@ pipeline {
                           } else {
                               python_suffix = "off"
                           }
-                          def destPath = "\\\\${env.OV_SHARE_05_IP}\\data\\cv_bench_cache\\OVMS_do_not_remove\\ovms_artifacts\\${env.PRODUCT_VERSION}\\${env.RELEASE_TAG}\\windows"
+                          def destPath = "w:\\${env.PRODUCT_VERSION}\\${env.RELEASE_TAG}\\windows"
                           def artifactsPath = "${buildstamp}\\ovms_windows_${env.PRODUCT_VERSION}_${env.RELEASE_TAG}_python_${python_suffix}.zip"
                           bat(returnStatus:true, script: "ECHO F | xcopy /Y /E ${env.WORKSPACE}\\dist\\windows\\ovms.zip ${destPath}\\${artifactsPath}")
-                          def remotePath = destPath.replace("\\\\${env.OV_SHARE_05_IP}\\data", "/data").replace("\\", "/")
-                          bat(returnStatus:true, script: "plink -batch -pw \"${env.WORKER_CREDS_PSW}\" ${env.WORKER_CREDS_USR}@${env.OV_SHARE_05_IP} \"rm -f ${remotePath}/latest && ln -sf ${remotePath}/${buildstamp} ${remotePath}/latest\"")
+                          bat(returnStatus:true, script: "rm -f ${destPath}\\latest && ln -sf ${destPath}\\${buildstamp} ${destPath}\\latest")
                         } finally {
                             windows.archive_build_artifacts()
                             windows.archive_test_artifacts()
