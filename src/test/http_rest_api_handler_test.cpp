30aee4153bc447d5235db62621040723d9de0a43
Dariusz Trawinski <Dariusz.Trawinski@intel.com>
2026-06-11 11:42:40 +0200

    update failing test

--- src/test/http_rest_api_handler_test.cpp
+++ src/test/http_rest_api_handler_test.cpp
@@ -470,7 +470,7 @@ TEST_F(ConfigReload, removeModelFromDiskWhenLoaded) {
 
     // On windows, the test expects inability to remove model
 #ifdef _WIN32
-    EXPECT_THROW({ std::filesystem::remove_all(getGenericFullPathForSrcTest("/tmp/dummy")); }, std::filesystem::filesystem_error);
+    EXPECT_THROW({ std::filesystem::remove_all(getGenericFullPathForTmp("/tmp/dummy")); }, std::filesystem::filesystem_error);
 #else
     // On linux, removing the model should be possible even if ENABLE_MMAP=YES
     std::filesystem::remove_all(getGenericFullPathForSrcTest("/tmp/dummy"));
