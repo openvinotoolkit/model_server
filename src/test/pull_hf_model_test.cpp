c735a9c80d4f3b57b735effe3ffee48c835069cd
Rafal Sapala <rafal.a.sapala@intel.com>
2026-06-18 11:14:43 +0200

    Remove static

--- src/test/pull_hf_model_test.cpp
+++ src/test/pull_hf_model_test.cpp
@@ -499,7 +499,7 @@ protected:
         ::SetUpServerForDownloadAndStart(this->t, this->server, sourceModel, downloadPath, task, timeoutSeconds);
     }
 
-    static int RunPullHfModelAndGetCode(const std::string& sourceModel, const std::string& modelRepositoryPath, const std::string& pullTask) {
+    int RunPullHfModelAndGetCode(const std::string& sourceModel, const std::string& modelRepositoryPath, const std::string& pullTask) {
         server.setShutdownRequest(0);
         std::vector<std::string> args = {
             "ovms",
