4a8b862ce7ef6f700d1f39b0d86a34df3e73d4d9
Pawel Rzepecki <pawel.rzepecki@intel.com>
2026-06-25 14:52:22 +0200

    build fixes

--- src/llm/io_processing/lfm2/lfm2_utils.cpp
+++ src/llm/io_processing/lfm2/lfm2_utils.cpp
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //*****************************************************************************
-#pragma once
 #include "lfm2_utils.hpp"
 #include "../utils.hpp"
 #include "../../../logging.hpp"
