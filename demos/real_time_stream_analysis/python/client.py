f2662e884d940b07643b87ae531026a47451e915
Vishwa2684 <115179890+Vishwa2684@users.noreply.github.com>
2026-07-17 18:43:49 +0530

    Update ffmpeg output arguments help text

--- demos/real_time_stream_analysis/python/client.py
+++ demos/real_time_stream_analysis/python/client.py
@@ -23,8 +23,8 @@ from stream_client import StreamClient
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--grpc_address', required=False, default='localhost:9022', help='Specify url to grpc service')
-parser.add_argument('--ffmpeg_output_width', required=False, default=None, type=int, help='Width of the output video')
-parser.add_argument('--ffmpeg_output_height', required=False, default=None, type=int, help='Height of the output video')
+parser.add_argument('--ffmpeg_output_width', required=False, default=None, type=int, help='Width of the rtsp output channel window')
+parser.add_argument('--ffmpeg_output_height', required=False, default=None, type=int, help='Height of the rtsp output channel window')
 parser.add_argument('--input_stream', required=False, default="rtsp://localhost:8080/channel1", type=str, help='Url of input rtsp stream')
 parser.add_argument('--output_stream', required=False, default="rtsp://localhost:8080/channel2", type=str, help='Url of output rtsp stream')
 parser.add_argument('--model_name', required=False, default="holisticTracking", type=str, help='Name of the model')
