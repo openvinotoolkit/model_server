632091c660a31f935e64370b210dbf43c04b3c7e
Rafal Sapala <rafal.a.sapala@intel.com>
2026-08-18 11:34:52 +0200

    Fix no python copy of artifacts (#4457)
    ### 🛠 Summary
    
    JIRA CVS-192913
    Add flags condition to copy.
    
    ### 🧪 Checklist
    
    - [ ] Unit tests added.
    - [ ] The documentation updated.
    - [ ] Change follows security best practices.
    ``

--- create_package.sh
+++ create_package.sh
@@ -27,7 +27,9 @@ if [ "$ov_use_binary" == "0" ] ; then cp -v /openvino_tokenizers/build/src/libop
 
 find /ovms/bazel-out/k8-*/bin -iname '*.so*' ! -type d ! -name "libgtest.so" ! -name "*params" ! -name "*.hana.*" ! -name "py_generate_pipeline.cpython*" !  -name "lib_node_*" ! -name "libazure-*" ! -name "pyovms.so" ! -path "*/_solib_k8/*" ! -path "*test_python_binding*" ! -name "*libpython*" -exec cp -vP {} /ovms_release/lib/ \;
 # Copy pyovms.so directly as a file (not symlink) to avoid broken Bazel cache paths
-find /ovms/bazel-out/k8-*/bin/src/python/binding -name 'pyovms.so' -type f -exec cp -v {} /ovms_release/lib/ \;
+if ! [[ $debug_bazel_flags == *"_py_off"* ]]; then
+    find /ovms/bazel-out/k8-*/bin/src/python/binding -name 'pyovms.so' -type f -exec cp -v {} /ovms_release/lib/ \;
+fi
 
 # Copy Azure SDK libs directly from the CMake install prefix so that the
 # unversioned .so files are local relative symlinks (not absolute Bazel cache
