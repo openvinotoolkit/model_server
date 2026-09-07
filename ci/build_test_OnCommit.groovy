0b483315a5f1fbf77c0e7f2044e878755508a22b
Trawinski <dariusz.trawinski@intel.com>
2026-09-07 14:03:42 +0200

    test

--- ci/build_test_OnCommit.groovy
+++ ci/build_test_OnCommit.groovy
@@ -235,16 +235,21 @@ pipeline {
                     sh "echo build --remote_cache=${env.OVMS_BAZEL_REMOTE_CACHE_URL} > .user.bazelrc"
                     sh "echo test:linux --test_env https_proxy=${env.HTTPS_PROXY} >> .user.bazelrc"
                     sh "echo test:linux --test_env http_proxy=${env.HTTP_PROXY} >> .user.bazelrc"
-                      sh "echo ${TOKEN}" > .github_token
-                    sh "make ovms_builder_image RUN_TESTS=${runTestsFlag} OPTIMIZE_BUILDING_TESTS=1 OVMS_CPP_IMAGE_TAG=${shortCommit} BUILD_IMAGE=openvino/model_server-build:${shortCommit}"
+                    withCredentials([string(credentialsId: 'github-ovms-jenkins', variable: 'TOKEN')]) {
+                      sh 'printf "%s" "$TOKEN" > .github_token'
+                      try {
+                        sh "make ovms_builder_image RUN_TESTS=${runTestsFlag} OPTIMIZE_BUILDING_TESTS=1 OVMS_CPP_IMAGE_TAG=${shortCommit} BUILD_IMAGE=openvino/model_server-build:${shortCommit}"
 
-                    // release_image
-                    sh "make release_image RUN_TESTS=0 GPU=1 NPU=1 OVMS_CPP_IMAGE_TAG=${shortCommit} BUILD_IMAGE=openvino/model_server-build:${shortCommit}"
-                    sh "make run_lib_files_test OVMS_CPP_IMAGE_TAG=${shortCommit}"
-                    if ( test_doc_files_linux ) {
-                        sh "docker save openvino/model_server:${shortCommit} | gzip > ovms_release_image.tar.gz"
-                        stash name: 'ovms-release-image', includes: 'ovms_release_image.tar.gz'
-                        sh "rm -f ovms_release_image.tar.gz"
+                        // release_image
+                        sh "make release_image RUN_TESTS=0 GPU=1 NPU=1 OVMS_CPP_IMAGE_TAG=${shortCommit} BUILD_IMAGE=openvino/model_server-build:${shortCommit}"
+                        sh "make run_lib_files_test OVMS_CPP_IMAGE_TAG=${shortCommit}"
+                        if ( test_doc_files_linux ) {
+                            sh "docker save openvino/model_server:${shortCommit} | gzip > ovms_release_image.tar.gz"
+                            stash name: 'ovms-release-image', includes: 'ovms_release_image.tar.gz'
+                            sh "rm -f ovms_release_image.tar.gz"
+                        }
+                      } finally {
+                        sh 'rm -f .github_token'
                     }
                   }
               }
