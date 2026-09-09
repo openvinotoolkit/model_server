329d954191d6d4da786d55cd54505e9b133c08da
mzegla <milosz.zeglarski@intel.com>
2026-09-09 13:56:13 +0200

    unskip one test

--- src/test/llm/llmnode_test.cpp
+++ src/test/llm/llmnode_test.cpp
@@ -1003,7 +1003,6 @@ TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonNFail) {
 }
 
 TEST_P(LLMFlowHttpTestParameterized, unaryChatCompletionsJsonN) {
-    GTEST_SKIP();  // TODO: Temporary skip to synchronize CI workers
     auto params = GetParam();
     config.max_new_tokens = 5;
     config.rng_seed = 1;
