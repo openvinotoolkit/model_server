ff9249b178411fe4dc225947202b7c59666ffd0d
rasapala <rafal.a.sapala@intel.com>
2026-09-23 09:54:35 +0200

    Fix server start check

--- src/server.cpp
+++ src/server.cpp
@@ -576,8 +576,7 @@ int Server::startServerFromSettings(ServerSettingsImpl& serverSettings, ModelsSe
     try {
         Status ret = startFromSettings(&serverSettings, &modelsSettings);
         if (ret == StatusCode::SERVER_ALREADY_STARTED ||
-            ret == StatusCode::SERVER_ALREADY_STARTING ||
-            ret == StatusCode::OPTIONS_USAGE_ERROR) {
+            ret == StatusCode::SERVER_ALREADY_STARTING) {
             return statusToExitCode(ret);
         }
         ModulesShutdownGuard shutdownGuard(*this);
